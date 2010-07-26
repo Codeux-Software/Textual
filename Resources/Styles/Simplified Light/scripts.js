b5e2b31a1e4563f7e71673939532b3d99c899bd8
Michael Morris <Michael@Macintosh.local>
2010-07-25 20:04:22 -0400

    New, lighter style - Also, the commit prior to this one was an error. It was not the 1.0.0 release.

---
+++ Resources/Styles/Simplified Light/scripts.js
@@ -0,0 +1,7 @@
+
+
+/* The following function calls are required. Add additonal code above it. */
+function on_url() { app.setUrl(event.target.innerHTML); }
+function on_addr() { app.setAddr(event.target.innerHTML); }
+function on_chname() { app.setChan(event.target.innerHTML); }
+function on_nick() { app.setNick(event.target.parentNode.parentNode.getAttribute('nick')); }
