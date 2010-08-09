0a6aa9cc70e01dc311b85ff1596cfd779a5f05bc
Michael Morris <Michael@Macintosh.local>
2010-08-09 04:27:26 -0400

    Plugin wrapper

---
+++ Classes/Helpers/TextualPluginItem.h
@@ -0,0 +1,23 @@
+// Created by Michael Morris <mikey AT codeux DOT com> <http://github.com/mikemac11/Textual>
+// You can redistribute it and/or modify it under the new BSD license.
+
+#import <Cocoa/Cocoa.h>
+#import "PluginProtocol.h"
+
+@interface TextualPluginItem : NSObject {
+	NSBundle *pluginBundle;
+	PluginProtocol* pluginPrimaryClass;
+}
+
+@property (retain) NSBundle *pluginBundle;
+@property (retain) PluginProtocol* pluginPrimaryClass;
+
+- (void)initWithPluginClass:(Class)primaryClass 
+				  andBundle:(NSBundle*)bundle
+				andIRCWorld:(IRCWorld*)world
+		  withUserInputDict:(NSMutableDictionary*)newUserDict
+		withServerInputDict:(NSMutableDictionary*)newServerDict
+	  withUserInputDictRefs:(NSMutableDictionary**)userDict
+	withServerInputDictRefs:(NSMutableDictionary**)serverDict;
+
+@end
---
+++ Classes/Helpers/TextualPluginItem.m
@@ -0,0 +1,90 @@
+// Created by Michael Morris <mikey AT codeux DOT com> <http://github.com/mikemac11/Textual>
+// You can redistribute it and/or modify it under the new BSD license.
+
+#import "TextualPluginItem.h"
+#import "PluginProtocol.h"
+
+@implementation TextualPluginItem
+
+@synthesize pluginBundle;
+@synthesize pluginPrimaryClass;
+
+- (void)initWithPluginClass:(Class)primaryClass 
+				  andBundle:(NSBundle*)bundle
+				andIRCWorld:(IRCWorld*)world
+		  withUserInputDict:(NSMutableDictionary*)newUserDict
+		withServerInputDict:(NSMutableDictionary*)newServerDict
+	  withUserInputDictRefs:(NSMutableDictionary**)userDict
+	withServerInputDictRefs:(NSMutableDictionary**)serverDict
+{
+	pluginPrimaryClass = [[primaryClass alloc] init];
+	
+	if (pluginPrimaryClass) {
+		if (pluginPrimaryClass) {
+			if ([pluginPrimaryClass respondsToSelector:@selector(messageSentByUser:message:command:)]) {
+				if ([pluginPrimaryClass respondsToSelector:@selector(pluginSupportsUserInputCommands)]) {
+					NSArray *spdcmds = [pluginPrimaryClass pluginSupportsUserInputCommands];
+					
+					if ([spdcmds count] >= 1) {
+						for (NSString *cmd in spdcmds) {
+							cmd = [cmd uppercaseString];
+							
+							NSArray *cmdDict = [newUserDict objectForKey:cmd];
+							
+							if (!cmdDict) {
+								[newUserDict setObject:[[NSMutableArray new] autorelease] forKey:cmd];
+							}
+							
+							if (![cmdDict containsObject:bundle]) {
+								[[newUserDict objectForKey:cmd] addObject:self];
+							}
+						}
+					}
+				}
+			}
+			
+			// Server Input
+			if ([pluginPrimaryClass respondsToSelector:@selector(messageReceivedByServer:sender:message:)]) {
+				if ([pluginPrimaryClass respondsToSelector:@selector(pluginSupportsServerInputCommands)]) {
+					NSArray *spdcmds = [pluginPrimaryClass pluginSupportsServerInputCommands];
+					
+					if ([spdcmds count] >= 1) {
+						for (NSString *cmd in spdcmds) {
+							cmd = [cmd uppercaseString];
+							
+							NSArray *cmdDict = [newServerDict objectForKey:cmd];
+							
+							if (!cmdDict) {
+								[newServerDict setObject:[[NSMutableArray new] autorelease] forKey:cmd];
+							}
+							
+							if (![cmdDict containsObject:bundle]) {
+								[[newServerDict objectForKey:cmd] addObject:self];
+							}
+						}
+					}
+				}
+			}
+			
+			if ([pluginPrimaryClass respondsToSelector:@selector(pluginLoadedIntoMemory:)]) {
+				[pluginPrimaryClass pluginLoadedIntoMemory:world];
+			}
+			
+			*userDict = newUserDict;
+			*serverDict = newServerDict;
+		}
+	}
+}
+
+- (void)dealloc
+{
+	if ([pluginPrimaryClass respondsToSelector:@selector(pluginUnloadedFromMemory)]) {
+		[pluginPrimaryClass pluginUnloadedFromMemory];
+	}
+	
+	[pluginPrimaryClass release];
+	[pluginBundle release];
+	[super dealloc];
+}
+
+@end
