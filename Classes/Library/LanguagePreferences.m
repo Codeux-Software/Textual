e465a7043769191b1919280b8f0ce49ce7d89967
Michael Morris <michael@codeux.com>
2010-09-09 02:46:04 -0400

    LanguagePreferences.m

---
+++ Classes/Library/LanguagePreferences.m
@@ -0,0 +1,35 @@
+#import "LanguagePreferences.h"
+
+@implementation LanguagePreferences
+
+static NSDictionary *themeLocalizations = nil;
+
++ (void)setThemeForLocalization:(NSString *)path
+{
+	themeLocalizations = nil;
+	
+	NSString *filepath = [path stringByAppendingPathComponent:@"/BasicLanguage.plist"];
+	
+	if ([[NSFileManager defaultManager] fileExistsAtPath:filepath]) {
+		NSDictionary *localkeys = [[NSDictionary alloc] initWithContentsOfFile:filepath];
+			
+		if (localkeys) {
+			themeLocalizations = localkeys;
+		}
+	}
+}
+
++ (NSString *)localizedStringWithKey:(NSString *)key
+{
+	if (themeLocalizations == nil) {
+		return NSLocalizedStringFromTable(key, @"BasicLanguage", nil);
+	} else {
+		NSString *localstring = [themeLocalizations objectForKey:key];
+		
+		if (localstring) return localstring;
+		
+		return NSLocalizedStringFromTable(key, @"BasicLanguage", nil);;
+	}
+}
+
+@end
