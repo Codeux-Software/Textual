9f8cffee2f5f98660491a3adc37ea09720456d7a
Josh Goebel <dreamer3@gmail.com>
2010-11-16 07:21:25 -0500

    fully support the old api

--- Resources/Javascript/core.js
+++ Resources/Javascript/core.js
@@ -17,9 +17,18 @@ Textual = {
 		document.write('<link href="' + cssFile + '" media="screen" rel="stylesheet" type="text/css" />'); 
 	},
 	
+	// check for the old API functions and call if they exist
+	old_api:function(str)
+	{
+		if (window[str]) {
+			window[str]();
+			return true
+		} else { return false }
+	},
+	
 	/* The following function calls are required. */
-	on_url: function() { app.setUrl(event.target.innerHTML); },
-	on_addr: function() { app.setAddr(event.target.innerHTML); },
-	on_chname: function() { app.setChan(event.target.innerHTML); },
-	on_nick: function() { app.setNick(event.target.parentNode.parentNode.getAttribute('nick')); }
+	on_url: function() { Textual.old_api("on_url") || app.setUrl(event.target.innerHTML); },
+	on_addr: function() { Textual.old_api("on_addr") || app.setAddr(event.target.innerHTML); },
+	on_chname: function() { Textual.old_api("on_chname") || app.setChan(event.target.innerHTML); },
+	on_nick: function() { Textual.old_api("on_nick") || app.setNick(event.target.parentNode.parentNode.getAttribute('nick')); }
 }
