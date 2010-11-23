78799e06ff8183fb4d653cbe913c548f70ca6049
Josh Goebel <dreamer3@gmail.com>
2010-11-23 07:28:50 -0500

    Remove a bunch of boiler plate retain/release code. Use properties instead, that's what they are for.

--- Classes/Preferences/OtherTheme.h
+++ Classes/Preferences/OtherTheme.h
@@ -43,32 +43,32 @@
 }
 
 @property (nonatomic, retain, getter=fileName, setter=setFileName:) NSString* fileName;
-@property (nonatomic, readonly) NSFont* inputTextFont;
-@property (nonatomic, readonly) NSColor* underlyingWindowColor;
-@property (nonatomic, readonly) NSColor* inputTextBgColor;
-@property (nonatomic, readonly) NSColor* inputTextColor;
-@property (nonatomic, readonly) NSFont* treeFont;
-@property (nonatomic, readonly) NSColor* treeBgColor;
-@property (nonatomic, readonly) NSColor* treeHighlightColor;
-@property (nonatomic, readonly) NSColor* treeNewTalkColor;
-@property (nonatomic, readonly) NSColor* treeUnreadColor;
-@property (nonatomic, readonly) NSColor* treeActiveColor;
-@property (nonatomic, readonly) NSColor* treeInactiveColor;
-@property (nonatomic, readonly) NSColor* treeSelActiveColor;
-@property (nonatomic, readonly) NSColor* treeSelInactiveColor;
-@property (nonatomic, readonly) NSColor* treeSelTopLineColor;
-@property (nonatomic, readonly) NSColor* treeSelBottomLineColor;
-@property (nonatomic, readonly) NSColor* treeSelTopColor;
-@property (nonatomic, readonly) NSColor* treeSelBottomColor;
-@property (nonatomic, readonly) NSFont* memberListFont;
-@property (nonatomic, readonly) NSColor* memberListBgColor;
-@property (nonatomic, readonly) NSColor* memberListColor;
-@property (nonatomic, readonly) NSColor* memberListOpColor;
-@property (nonatomic, readonly) NSColor* memberListSelColor;
-@property (nonatomic, readonly) NSColor* memberListSelTopLineColor;
-@property (nonatomic, readonly) NSColor* memberListSelBottomLineColor;
-@property (nonatomic, readonly) NSColor* memberListSelTopColor;
-@property (nonatomic, readonly) NSColor* memberListSelBottomColor;
+@property (nonatomic, retain) NSFont* inputTextFont;
+@property (nonatomic, retain) NSColor* underlyingWindowColor;
+@property (nonatomic, retain) NSColor* inputTextBgColor;
+@property (nonatomic, retain) NSColor* inputTextColor;
+@property (nonatomic, retain) NSFont* treeFont;
+@property (nonatomic, retain) NSColor* treeBgColor;
+@property (nonatomic, retain) NSColor* treeHighlightColor;
+@property (nonatomic, retain) NSColor* treeNewTalkColor;
+@property (nonatomic, retain) NSColor* treeUnreadColor;
+@property (nonatomic, retain) NSColor* treeActiveColor;
+@property (nonatomic, retain) NSColor* treeInactiveColor;
+@property (nonatomic, retain) NSColor* treeSelActiveColor;
+@property (nonatomic, retain) NSColor* treeSelInactiveColor;
+@property (nonatomic, retain) NSColor* treeSelTopLineColor;
+@property (nonatomic, retain) NSColor* treeSelBottomLineColor;
+@property (nonatomic, retain) NSColor* treeSelTopColor;
+@property (nonatomic, retain) NSColor* treeSelBottomColor;
+@property (nonatomic, retain) NSFont* memberListFont;
+@property (nonatomic, retain) NSColor* memberListBgColor;
+@property (nonatomic, retain) NSColor* memberListColor;
+@property (nonatomic, retain) NSColor* memberListOpColor;
+@property (nonatomic, retain) NSColor* memberListSelColor;
+@property (nonatomic, retain) NSColor* memberListSelTopLineColor;
+@property (nonatomic, retain) NSColor* memberListSelBottomLineColor;
+@property (nonatomic, retain) NSColor* memberListSelTopColor;
+@property (nonatomic, retain) NSColor* memberListSelBottomColor;
 
 - (void)reload;
 
