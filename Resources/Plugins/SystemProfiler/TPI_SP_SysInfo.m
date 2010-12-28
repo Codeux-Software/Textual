71369e6d548c28e619e24a6ec5be723a7b6f1915
Michael Morris <mikey@codeux.com>
2010-12-27 23:40:46 -0500

    Some script enhancements and update copyrights related to Codeux to reflect the move to year 2011.

--- Resources/Plugins/SystemProfiler/TPI_SP_SysInfo.m
+++ Resources/Plugins/SystemProfiler/TPI_SP_SysInfo.m
@@ -1,3 +1,6 @@
+// Created by Codeux Software <support AT codeux DOT com> <https://github.com/codeux/Textual>
+// You can redistribute it and/or modify it under the new BSD license.
+
 #import "TPI_SP_SysInfo.h"
 
 #include <mach/mach.h>
