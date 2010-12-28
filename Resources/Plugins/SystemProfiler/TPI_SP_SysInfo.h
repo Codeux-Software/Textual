27f5aae203289c9e3e41c28badd6c885f59bc0ff
Michael Morris <michael@codeux.com>
2010-12-27 23:40:46 -0500

    Some script enhancements and update copyrights related to Codeux to reflect the move to year 2011.

--- Resources/Plugins/SystemProfiler/TPI_SP_SysInfo.h
+++ Resources/Plugins/SystemProfiler/TPI_SP_SysInfo.h
@@ -1,3 +1,6 @@
+// Created by Codeux Software <support AT codeux DOT com> <https://github.com/codeux/Textual>
+// You can redistribute it and/or modify it under the new BSD license.
+
 @interface TPI_SP_SysInfo : NSObject 
 
 + (NSString *)compiledOutput;
