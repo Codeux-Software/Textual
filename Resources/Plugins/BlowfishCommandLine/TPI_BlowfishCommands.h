025b3dd26235f9b0e3fd9ab4e38ea3f4e28c2bef
Michael Morris <mikey@codeux.com>
2011-01-14 10:38:38 -0500

    New plugin for blowfish.

---
+++ Resources/Plugins/BlowfishCommandLine/TPI_BlowfishCommands.h
@@ -0,0 +1,14 @@
+// Created by Codeux Software <support AT codeux DOT com> <https://github.com/codeux/Textual>
+// You can redistribute it and/or modify it under the new BSD license.
+
+#import "TextualApplication.h"
+
+@interface TPI_BlowfishCommands : NSObject 
+
+- (void)messageSentByUser:(IRCClient *)client
+				  message:(NSString *)messageString
+				  command:(NSString *)commandString;
+
+- (NSArray *)pluginSupportsUserInputCommands;
+
+@end
