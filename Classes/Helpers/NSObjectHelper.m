31d461ba42752dd8f5432076c5cb53ca7a579369
Michael Morris <mikey@codeux.com>
2011-01-30 14:23:35 -0500

    File

---
+++ Classes/Helpers/NSObjectHelper.m
@@ -0,0 +1,17 @@
+// Created by Codeux Software <support AT codeux DOT com> <https://github.com/codeux/Textual>
+// You can redistribute it and/or modify it under the new BSD license.
+
+@implementation NSObject (NSOjectHelper)
+
+- (oneway void)drain
+{
+	if (self) {
+		NSUInteger retainTotal = [self retainCount];
+		
+		if (retainTotal >= 1) {
+			[self release];
+		}
+	}
+}
+
+@end
