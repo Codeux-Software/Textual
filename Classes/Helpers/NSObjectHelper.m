e7eebdabd2cd315211e7397212bb7431a12d3ae7
Michael Morris <mikey@codeux.com>
2011-02-27 04:18:48 -0500

    Better lion check

--- Classes/Helpers/NSObjectHelper.m
+++ Classes/Helpers/NSObjectHelper.m
@@ -1,7 +1,7 @@
 // Created by Codeux Software <support AT codeux DOT com> <https://github.com/codeux/Textual>
 // You can redistribute it and/or modify it under the new BSD license.
 
-@implementation NSObject (NSOjectHelper)
+@implementation NSObject (NSObjectHelper)
 
 - (oneway void)drain
 {
