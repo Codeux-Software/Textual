143973157003a4e10c94a4cb407bbfefd0dd69e9
Kyle Fuller <inbox@kylefuller.co.uk>
2012-01-15 07:16:00 +0000

    Welcome to 2012

--- Frameworks/Blowfish/Source/Classes/BlowfishEncryption.h
+++ Frameworks/Blowfish/Source/Classes/BlowfishEncryption.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011 Codeux Software <support at codeux dot com> */
+/* Copyright (c) 2012 Codeux Software <support at codeux dot com> */
 
 #include <BlowfishEncryption/DH1080.h>
 #include <BlowfishEncryption/Encryption.h>
