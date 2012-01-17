5ba6bea1c8ebaf1d628a75476c5be4e2590d8e3d
Kyle Fuller <inbox@kylefuller.co.uk>
2012-01-17 02:16:23 +0000

    App store build should SKIP_INSTALL

--- Classes/Headers/GrowlController.h
+++ Classes/Headers/GrowlController.h
@@ -2,7 +2,7 @@
 // Modifications by Codeux Software <support AT codeux DOT com> <https://github.com/codeux/Textual>
 // You can redistribute it and/or modify it under the new BSD license.
 
-#import "Growl/Growl.h"
+#import <Growl/Growl.h>
 
 @class IRCWorld;
 
