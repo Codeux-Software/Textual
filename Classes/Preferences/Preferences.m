5fa90e2f358d8acc1a2c8224d4cead5038c8b754
Michael Morris <mikey@codeux.com>
2012-02-13 23:19:43 -0500

    Do not check for default IRC client status in trial mode. Also rework some of the language used during trial period.

--- Classes/Preferences/Preferences.m
+++ Classes/Preferences/Preferences.m
@@ -953,9 +953,11 @@ static NSInteger totalRunTime = 0;
 
 	[_NSUserDefaults() setInteger:numberOfRuns forKey:@"TXRunCount"];
 	
+#ifndef IS_TRIAL_BINARY
 	if (numberOfRuns >= 2) {
 		[[self invokeInBackgroundThread] defaultIRCClientPrompt];
 	} 
+#endif
 	
 	startUpTime = [NSDate epochTime];
 	
