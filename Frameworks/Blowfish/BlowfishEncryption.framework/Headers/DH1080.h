6928bd332a5cf9665a3cd003ab2c72dbe8b44904
Michael Morris <mikey@codeux.com>
2012-02-29 16:31:15 -0500

    Always copy over fresh framework.

--- Frameworks/Blowfish/BlowfishEncryption.framework/Headers/DH1080.h
+++
@@ -1,8 +0,0 @@
-/* Copyright (c) 2011 Codeux Software <support at codeux dot com> */
-
-#define requiredPublicKeyLength		135
-
-@interface CFDH1080 : NSObject 
-- (NSString *)generatePublicKey;
-- (NSString *)secretKeyFromPublicKey:(NSString *)publicKey;
-@end
