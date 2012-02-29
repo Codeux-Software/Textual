5437ef2ecd0f177f81ea8fdde71f2d238a50c5f9
Michael Morris <michael@codeux.com>
2012-02-29 16:31:15 -0500

    Always copy over fresh framework.

--- Frameworks/Blowfish/BlowfishEncryption.framework/Versions/A/Headers/Encryption.h
+++
@@ -1,6 +0,0 @@
-/* Copyright (c) 2011 Codeux Software <support at codeux dot com> */
-
-@interface CSFWBlowfish : NSObject
-+ (NSString *)encodeData:(NSString *)input key:(NSString *)phrase encoding:(NSStringEncoding)local;
-+ (NSString *)decodeData:(NSString *)input key:(NSString *)phrase encoding:(NSStringEncoding)local;
-@end
