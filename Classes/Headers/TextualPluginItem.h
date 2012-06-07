5114af868e6d0a5a36de4e33b1081192ee2a777b
Jonathan Willing <jwilling@me.com>
2012-06-07 16:43:47 -0500

    fix some IB warnings, other misc.
    - missing import on the TextualPluginItem.h
    - not using properties correcty in aforementioned class, fixed

--- Classes/Headers/TextualPluginItem.h
+++ Classes/Headers/TextualPluginItem.h
@@ -1,11 +1,7 @@
 // Created by Codeux Software <support AT codeux DOT com> <https://github.com/codeux/Textual>
 // You can redistribute it and/or modify it under the new BSD license.
 
-@interface TextualPluginItem : NSObject {
-	NSBundle *pluginBundle;
-	
-	PluginProtocol *pluginPrimaryClass;
-}
+@interface TextualPluginItem : NSObject
 
 @property (nonatomic, strong) NSBundle *pluginBundle;
 @property (nonatomic, strong) PluginProtocol *pluginPrimaryClass;
