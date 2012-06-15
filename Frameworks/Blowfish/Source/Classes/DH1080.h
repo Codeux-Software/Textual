ee7d66b5662196a1601b57f58de21d60417e8d2c
emsquared <mikey@codeux.com>
2012-06-15 01:53:04 -0400

    Misc. changes

--- Frameworks/Blowfish/Source/Classes/DH1080.h
+++ Frameworks/Blowfish/Source/Classes/DH1080.h
@@ -1,7 +1,5 @@
 /* Copyright (c) 2012 Codeux Software <support at codeux dot com> */
 
-#define requiredPublicKeyLength		135
-
 @interface CFDH1080 : NSObject 
 - (NSString *)generatePublicKey;
 - (NSString *)secretKeyFromPublicKey:(NSString *)publicKey;
