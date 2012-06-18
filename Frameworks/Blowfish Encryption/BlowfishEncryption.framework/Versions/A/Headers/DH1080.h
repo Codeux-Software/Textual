c9ebe4c624d4e5814c976a9c6672ca8f43cfb70c
emsquared <mikey@codeux.com>
2012-06-18 01:24:20 -0400

    Update our ignore file.

--- Frameworks/Blowfish Encryption/BlowfishEncryption.framework/Versions/A/Headers/DH1080.h
+++
@@ -1,6 +0,0 @@
-/* Copyright (c) 2012 Codeux Software <support at codeux dot com> */
-
-@interface CFDH1080 : NSObject 
-- (NSString *)generatePublicKey;
-- (NSString *)secretKeyFromPublicKey:(NSString *)publicKey;
-@end
