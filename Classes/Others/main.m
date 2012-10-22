43e44dc303adfb8cd126cd309bd22d5d93416793
Michael Morris <michael@codeux.com>
2012-10-22 16:00:27 -0400

    Access preferences for other IDs.

--- Classes/Others/main.m
+++ Classes/Others/main.m
@@ -3,6 +3,10 @@
 
 int main(int argc, const char *argv[])
 {
+	[_NSUserDefaults() addSuiteNamed:@"com.codeux.textual"];
+	[_NSUserDefaults() addSuiteNamed:@"com.codeux.irc.textual"];
+	[_NSUserDefaults() addSuiteNamed:@"com.codeux.irc.textual.trial"];
+
 	@autoreleasepool {
 		NSApplicationMain(argc, argv);
 	}
