e3892af093b31524896d32dac5b43f5a4f78cf94
emsquared <mikey@codeux.com>
2012-10-22 15:36:36 -0400

    Big changes.
    Revert old project ID and update our sandbox to be appliecd to all
    executables.
    
    To use old settings move the folder
    ~/Library/Containers/com.codeux.textual to
    ~/Library/Containers/com.codeux.irc.textual

--- Classes/Others/main.m
+++ Classes/Others/main.m
@@ -4,11 +4,6 @@
 int main(int argc, const char *argv[])
 {
 	@autoreleasepool {
-		/* Backwards compatibility for 2.1.0 and earlier 
-		 which used a different bundle identifier. */
-		
-		[_NSUserDefaults() addSuiteNamed:TPCPreferencesMigrationAssistantOldBundleIdentifier];
-		
 		NSApplicationMain(argc, argv);
 	}
 	
