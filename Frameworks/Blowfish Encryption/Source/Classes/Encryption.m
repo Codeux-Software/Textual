ee4cd5b0a267427357eeebf997eaffbc273cda4c
emsquared <mikey@codeux.com>
2013-02-04 17:14:24 -0500

    Refactor Blowfish Encryption Framework.
    Removed DH1080 implementation. That is being rewritten
    from scratch and is removed until that is finished.
    
    The basic low level Blowfish encryption and decryption
    has been merged into an Objective-C++ class. The class
    is private to the framework, but is designed to make future
    changes easier.

--- Frameworks/Blowfish Encryption/Source/Classes/Encryption.m
+++ Frameworks/Blowfish Encryption/Source/Classes/Encryption.m
@@ -36,42 +36,34 @@
  *********************************************************************** */
 
 #import "Encryption.h"
-#import "blowfish_be.h"
+#import "BlowfishBase.h"
 
 @implementation CSFWBlowfish
 
 + (NSString *)encodeData:(NSString *)input key:(NSString *)phrase encoding:(NSStringEncoding)local
 {
-	const char *message = [input cStringUsingEncoding:local];
-	const char *key = [phrase cStringUsingEncoding:local];
-	size_t keylen = [phrase length];
-	
-	char *resultString = fish_encrypt(key, keylen, message);
-	
-	NSString *cypher = [NSString stringWithCString:resultString encoding:local];
-	
-	free(resultString);
-	
-	return [@"+OK " stringByAppendingString:cypher];
+	NSString *result = [BlowfishBase encrypt:input key:phrase encoding:local];
+
+	if (result.length <= 0) {
+		return nil;
+	}
+
+	return [@"+OK " stringByAppendingString:result];
 }
 
 + (NSString *)decodeData:(NSString *)input key:(NSString *)phrase encoding:(NSStringEncoding)local
 {
-	if ([input hasPrefix:@"+OK "] && [input length] > 4) {
+	if ([input hasPrefix:@"+OK "] && [input length] >= 5) {
 		input = [input substringFromIndex:4];
 	}
-	
-	const char *message = [input cStringUsingEncoding:local];
-	const char *key = [phrase cStringUsingEncoding:local];
-	size_t keylen = [phrase length];
-	
-	char *resultString = fish_decrypt(key, keylen, message);
-	
-	NSString *cypher = [NSString stringWithCString:resultString encoding:local];
-	
-	free(resultString);
-	
-	return cypher;
+
+	NSString *result = [BlowfishBase decrypt:input key:phrase encoding:local];
+
+	if (result.length <= 0) {
+		return nil;
+	}
+
+	return result;
 }
 
 @end
