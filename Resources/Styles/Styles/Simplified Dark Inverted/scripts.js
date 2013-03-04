fa5836fe9d05e88b2cb76c26f87831eb68a388c8
Alex Sørlie Glomsaas <alex@delexious.com>
2013-03-04 23:17:58 +0100

    Added new high contrast style "Lucidity"

---
+++ Resources/Styles/Styles/Simplified Dark Inverted/scripts.js
@@ -0,0 +1,15 @@
+/* Defined in: "Textual.app -> Contents -> Resources -> JavaScript -> API -> core.js" */
+
+Textual.viewFinishedLoading = function()
+{
+	Textual.fadeInLoadingScreen(1.00, 0.95);
+
+	setTimeout(function() {
+		Textual.scrollToBottomOfView()
+	}, 500);
+}
+
+Textual.viewFinishedReload = function()
+{
+	Textual.viewFinishedLoading();
+}
