419d21920ef80b29f7f8b03377b7e4ce087704da
Michael Morris <michael@codeux.com>
2013-03-08 10:30:58 -0500

    Use the loading screen for the trial period introduction instead of an alert. More user friendly.

--- Classes/Headers/TVCMemberListUserInfoPopover.h
+++ Classes/Headers/TVCMemberListUserInfoPopover.h
@@ -38,8 +38,8 @@
 #import "TextualApplication.h"
 
 @interface TVCMemberListUserInfoPopover : NSPopover
-@property (nonatomic, nweak) IBOutlet NSTextField *nicknameField;
-@property (nonatomic, nweak) IBOutlet NSTextField *usernameField;
-@property (nonatomic, nweak) IBOutlet NSTextField *addressField;
-@property (nonatomic, nweak) IBOutlet NSTextField *privilegesField;
+@property (nonatomic, nweak) NSTextField *nicknameField;
+@property (nonatomic, nweak) NSTextField *usernameField;
+@property (nonatomic, nweak) NSTextField *addressField;
+@property (nonatomic, nweak) NSTextField *privilegesField;
 @end
