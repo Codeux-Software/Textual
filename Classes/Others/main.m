9fd2c543ea485e2f6313b887cbb87f6d4c2082f9
emsquared <mikey@codeux.com>
2013-03-23 12:58:42 -0400

    Bump max scrollback size to 15,000.

--- Classes/Others/main.m
+++ Classes/Others/main.m
@@ -3,13 +3,11 @@
 
 int main(int argc, const char *argv[])
 {
-	@autoreleasepool {
-		[RZUserDefaults() addSuiteNamed:@"com.codeux.textual"];
-		[RZUserDefaults() addSuiteNamed:@"com.codeux.irc.textual"];
-		[RZUserDefaults() addSuiteNamed:@"com.codeux.irc.textual.trial"];
+	[RZUserDefaults() addSuiteNamed:@"com.codeux.textual"];
+	[RZUserDefaults() addSuiteNamed:@"com.codeux.irc.textual"];
+	[RZUserDefaults() addSuiteNamed:@"com.codeux.irc.textual.trial"];
 
-		NSApplicationMain(argc, argv);
-	}
-	
+	NSApplicationMain(argc, argv);
+
     return 0;
 }
