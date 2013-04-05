Codeux-Software/Textual

commit ed5a397895c8c6ff359a2eb1dc899ff764a67359

NSMutable* properties are by definition not very thread safe so let's replace them. Hopefully this fixes some crashes.

Classes/IRC/IRCChannel.m
@@ -263,7 +263,8 @@
 
 - (void)sortedMemberListReload
 {
-	[self.memberList sortUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+	/* Do not call this unless needed. */
+	self.memberList = [self.memberList sortedArrayUsingComparator:^NSComparisonResult(id obj1, id obj2) {
 		return [obj1 compare:obj2];
 	}];
 
@@ -278,6 +279,8 @@
 	
 	NSInteger left = 0;
 	NSInteger right = self.memberList.count;
+
+	NSMutableArray *userList = [self.memberList mutableCopy];
 	
 	while ((right - left) > LINEAR_SEARCH_THRESHOLD) {
 		NSInteger i = ((left + right) / 2);
@@ -295,13 +298,16 @@
 		IRCUser *t = [self memberAtIndex:i];
 		
 		if ([t compare:item] == NSOrderedDescending) {
-			[self.memberList safeInsertObject:item atIndex:i];
+			[userList safeInsertObject:item atIndex:i];
 			
 			return;
 		}
 	}
 	
-	[self.memberList safeAddObject:item];
+	[userList safeAddObject:item];
+
+	self.memberList = nil;
+	self.memberList = userList;
 }
 
 #pragma mark -
@@ -342,9 +348,14 @@
 	NSObjectIsEmptyAssert(nick);
 	
 	NSInteger n = [self indexOfMember:nick];
+
+	NSMutableArray *userList = [self.memberList mutableCopy];
 	
 	if (n >= 0) {
-		[self.memberList safeRemoveObjectAtIndex:n];
+		[userList safeRemoveObjectAtIndex:n];
+
+		self.memberList = nil;
+		self.memberList = userList;
 
         [self.client postEventToViewController:@"channelMemberRemoved" forChannel:self];
 	}
@@ -414,7 +425,8 @@
 
 - (void)clearMembers
 {
-	[self.memberList removeAllObjects];
+	self.memberList = nil;
+	self.memberList = @[];
 	
 	[self reloadMemberList];
 }

Classes/Headers/IRCChannel.h
@@ -51,9 +51,10 @@ typedef enum IRCChannelStatus : NSInteger {
 @property (nonatomic, strong) IRCChannelMode *modeInfo;
 @property (nonatomic, strong) IRCChannelConfig *config;
 @property (nonatomic, assign) IRCChannelStatus status;
-@property (nonatomic, strong) NSMutableArray *memberList;
 @property (nonatomic, assign) BOOL errorOnLastJoinAttempt;
 @property (nonatomic, assign) BOOL isGatheringModeInfo;
+
+@property (strong) NSArray *memberList;
 
 - (void)setup:(IRCChannelConfig *)seed;
 - (void)updateConfig:(IRCChannelConfig *)seed;

Classes/Views/Channel View/Message Queue/TVCLogControllerOperationQueue.m
@@ -48,7 +48,7 @@
 @end
 
 @interface TVCLogControllerOperationQueue ()
-@property (strong) NSMutableDictionary *cachedOperations;
+@property (strong) NSDictionary *cachedOperations;
 @end
 
 #pragma mark -
@@ -163,7 +163,12 @@
 	[mutableOperations addObject:@[callbackBlock, context]];
 
 	/* Save array. */
-	[self.cachedOperations setObject:mutableOperations forKey:controllerKey];
+	NSMutableDictionary *mutops = [self.cachedOperations mutableCopy];
+	
+	[mutops setObject:mutableOperations forKey:controllerKey];
+
+	self.cachedOperations = nil;
+	self.cachedOperations = mutops;
 }
 
 - (NSArray *)cachedOperationsFor:(TVCLogController *)controller
@@ -185,7 +190,12 @@
 	NSString *controllerKey = [controller operationQueueHash];
 
 	/* Destroy cache. */
-	[self.cachedOperations removeObjectForKey:controllerKey];
+	NSMutableDictionary *mutops = [self.cachedOperations mutableCopy];
+
+	[mutops removeObjectForKey:controllerKey];
+
+	self.cachedOperations = nil;
+	self.cachedOperations = mutops;
 }
 
 #pragma mark -

Classes/Views/Channel View/TVCLogRenderer.m
@@ -694,14 +694,7 @@ static NSInteger getNextAttributeRange(attr_t *attrBuf, NSInteger start, NSInteg
 			if (log && isNormalMsg) {
 				IRCChannel *logChannel = log.channel;
 
-				NSArray *rawMemberList = [logChannel.memberList copy];
-				NSArray *sortedMembers = [rawMemberList sortedArrayWithOptions:NSSortStable usingComparator:^NSComparisonResult(id obj1, id obj2) {
-					PointerIsEmptyAssertReturn(obj1, NO);
-					PointerIsEmptyAssertReturn(obj1, NO);
-
-					NSObjectIsKindOfClassAssertReturn(obj1, IRCUser, NO);
-					NSObjectIsKindOfClassAssertReturn(obj2, IRCUser, NO);
-
+				NSArray *sortedMembers = [logChannel.memberList sortedArrayWithOptions:NSSortStable usingComparator:^NSComparisonResult(id obj1, id obj2) {
 					IRCUser *s1 = obj1;
 					IRCUser *s2 = obj2;
 

commit 871f371562c1f3adb65ca2c4ae9e93e53e91da0f

Fix user list sorted insert. ed5a397895c8 broke it.

Classes/IRC/IRCChannel.m
@@ -299,6 +299,9 @@
 		
 		if ([t compare:item] == NSOrderedDescending) {
 			[userList safeInsertObject:item atIndex:i];
+
+			self.memberList = nil;
+			self.memberList = userList;
 			
 			return;
 		}