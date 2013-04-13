e052ffe95323b750cb5e0e599a484f855444eed0
Jiang Jiang <jiangj@opera.com>
2013-04-13 21:55:16 +0200

    Fix a warning for trial build

--- Classes/Preferences/TPCPreferences.m
+++ Classes/Preferences/TPCPreferences.m
@@ -1294,9 +1294,9 @@ static NSMutableArray *excludeKeywords = nil;
 {
 	[self updateApplicationRunCount];
 
-	NSInteger numberOfRuns = [self applicationRunCount];
-	
 #ifndef TEXTUAL_TRIAL_BINARY
+	NSInteger numberOfRuns = [self applicationRunCount];
+
 	if (numberOfRuns >= 2) {
 		[self.invokeInBackgroundThread defaultIRCClientPrompt:NO];
 	}
