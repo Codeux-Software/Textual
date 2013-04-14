a320625b268ce02cdaf1f012b4782dc84fa37ca7
emsquared <mikey@codeux.com>
2013-04-13 22:45:51 -0400

    Revert "Fix mode output being duplicated during autojoin."
    This reverts commit 1552411980f21e89d82df26d1527e7621e458bc6.

--- Classes/IRC/IRCClient.m
+++ Classes/IRC/IRCClient.m
@@ -4445,16 +4445,14 @@
 
 			PointerIsEmptyAssertLoopBreak(c);
 
-			if (NSObjectIsEmpty(c.modeInfo.modeInformation)) {
+			if (c.isGatheringModeInfo == NO || NSObjectIsEmpty(c.modeInfo.modeInformation)) {
 				if (c.isActive) {
 					[c.modeInfo clear];
 					[c.modeInfo update:modestr];
 
 					c.isGatheringModeInfo = YES;
 				}
-			}
 
-			if (c.isGatheringModeInfo == NO) {
 				[self print:c type:TVCLogLineModeType nick:nil text:TXTFLS(@"IRCChannelHasModes", modestr) receivedAt:m.receivedAt];
 			}
 
