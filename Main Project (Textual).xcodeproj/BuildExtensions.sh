e7c75adfe034033fcbe316d11eeadb4c4a7bdb9c
Michael Morris <michael@codeux.com>
2013-04-24 12:03:11 -0400

    Frameworks and extensions need to inherit CODE_SIGN_IDENTITY from Textual so that they can be signed using the Developer ID certificate when needed.

--- Main Project (Textual).xcodeproj/BuildExtensions.sh
+++ Main Project (Textual).xcodeproj/BuildExtensions.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 cd "${PROJECT_DIR}/Resources/Plugins/Blowfish Key Control"
-xcodebuild -alltargets -configuration "Release" BUNDLE_LOADER="${CODESIGNING_FOLDER_PATH}/Contents/MacOS/${EXECUTABLE_NAME}"
+xcodebuild -alltargets -configuration "Release" BUNDLE_LOADER="${CODESIGNING_FOLDER_PATH}/Contents/MacOS/${EXECUTABLE_NAME}" CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}"
 
 cd "${PROJECT_DIR}/Resources/Plugins/Brag Spam"
-xcodebuild -alltargets -configuration "Release" BUNDLE_LOADER="${CODESIGNING_FOLDER_PATH}/Contents/MacOS/${EXECUTABLE_NAME}"
+xcodebuild -alltargets -configuration "Release" BUNDLE_LOADER="${CODESIGNING_FOLDER_PATH}/Contents/MacOS/${EXECUTABLE_NAME}" CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}"
 
 cd "${PROJECT_DIR}/Resources/Plugins/System Profiler"
-xcodebuild -alltargets -configuration "Release" BUNDLE_LOADER="${CODESIGNING_FOLDER_PATH}/Contents/MacOS/${EXECUTABLE_NAME}"
+xcodebuild -alltargets -configuration "Release" BUNDLE_LOADER="${CODESIGNING_FOLDER_PATH}/Contents/MacOS/${EXECUTABLE_NAME}" CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}"
 
 cd "${PROJECT_DIR}/Resources/Plugins/Spammer Paradise"
-xcodebuild -alltargets -configuration "Release" BUNDLE_LOADER="${CODESIGNING_FOLDER_PATH}/Contents/MacOS/${EXECUTABLE_NAME}"
+xcodebuild -alltargets -configuration "Release" BUNDLE_LOADER="${CODESIGNING_FOLDER_PATH}/Contents/MacOS/${EXECUTABLE_NAME}" CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}"
 
 cd "${PROJECT_DIR}/Resources/Plugins/ZNC Additions"
-xcodebuild -alltargets -configuration "Release" BUNDLE_LOADER="${CODESIGNING_FOLDER_PATH}/Contents/MacOS/${EXECUTABLE_NAME}"
+xcodebuild -alltargets -configuration "Release" BUNDLE_LOADER="${CODESIGNING_FOLDER_PATH}/Contents/MacOS/${EXECUTABLE_NAME}" CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}"
