ca61b58b1eae155d2b3b721fd11437be22a9ea31
Michael Morris <mikey@codeux.com>
2013-04-24 12:03:11 -0400

    Frameworks and extensions need to inherit CODE_SIGN_IDENTITY from Textual so that they can be signed using the Developer ID certificate when needed.

--- Main Project (Textual).xcodeproj/BuildFrameworks.sh
+++ Main Project (Textual).xcodeproj/BuildFrameworks.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 cd "${PROJECT_DIR}/Frameworks/Auto Hyperlinks/Source/"
-xcodebuild -alltargets -configuration "Release"
+xcodebuild -alltargets -configuration "Release" CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}"
 
 cd "${PROJECT_DIR}/Frameworks/Blowfish Encryption/Source/"
-xcodebuild -alltargets -configuration "Release"
+xcodebuild -alltargets -configuration "Release" CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}"
 
 cd "${PROJECT_DIR}/Frameworks/System Information/Source/"
-xcodebuild -alltargets -configuration "Release"
+xcodebuild -alltargets -configuration "Release" CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}"
