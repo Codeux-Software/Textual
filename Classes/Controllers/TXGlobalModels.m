Codeux-Software/Textual

commit 1feba02a21ba876a7736c85240380a043d82cf84

Use strftime for this so it matches the actual link in preferences for the symbol reference.

Classes/Controllers/TXGlobalModels.m
@@ -38,6 +38,9 @@
 #import "TextualApplication.h"
 
 #import <objc/objc-runtime.h>
+#import <time.h>
+
+#define _timeBufferSize		256 /* Let's hope to God no one tries to overflow this… */
 
 #pragma mark -
 #pragma mark Validity.
@@ -73,7 +76,16 @@ NSString *TXFormattedTimestampWithOverride(NSDate *date, NSString *format, NSStr
 		format = override;
 	}
 
-	return [NSString stringWithFormat:@"%@", [date dateWithCalendarFormat:format timeZone:nil]];
+	time_t global = time(NULL);
+	
+	struct tm *local = localtime(&global);
+	char buf[(_timeBufferSize + 1)];
+	
+	strftime(buf, _timeBufferSize, [format UTF8String], local);
+
+	buf[_timeBufferSize] = 0;
+
+	return [NSString stringWithBytes:buf length:strlen(buf) encoding:NSUTF8StringEncoding];
 }
 
 NSString *TXFormattedTimestamp(NSDate *date, NSString *format) 

commit 45ac54322ceb6a0bcabedd3b45bdcb2a3fe831fe

Fix displaying real timestamp from znc buffer, 1feba02a21ba broke it.

Classes/Controllers/TXGlobalModels.m
@@ -76,7 +76,7 @@ NSString *TXFormattedTimestampWithOverride(NSDate *date, NSString *format, NSStr
 		format = override;
 	}
 
-	time_t global = time(NULL);
+	time_t global = (time_t) [date timeIntervalSince1970];
 	
 	struct tm *local = localtime(&global);
 	char buf[(_timeBufferSize + 1)];