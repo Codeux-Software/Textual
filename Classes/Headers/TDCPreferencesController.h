0306ed220ee3b4c2e2bc9d27a2b6734abf754e15
winny <winny@lavabit.com>
2013-06-15 23:30:14 +0200

    Textual alert preferences now manage behavior of the dock icon's bounce.

--- Classes/Headers/TDCPreferencesController.h
+++ Classes/Headers/TDCPreferencesController.h
@@ -59,6 +59,7 @@
 @property (nonatomic, nweak) NSButton *addExcludeKeywordButton;
 @property (nonatomic, nweak) NSButton *alertSpeakEventButton;
 @property (nonatomic, nweak) NSButton *alertDisableWhileAwayButton;
+@property (nonatomic, nweak) NSButton *alertBounceDockIconButton;
 @property (nonatomic, nweak) NSButton *alertPushNotificationButton;
 @property (nonatomic, nweak) NSButton *highlightNicknameButton;
 @property (nonatomic, nweak) NSButton *setAsDefaultIRCClientButton;
@@ -86,6 +87,7 @@
 - (void)onChangedAlertSpoken:(id)sender;
 - (void)onChangedAlertSound:(id)sender;
 - (void)onChangedAlertDisableWhileAway:(id)sender;
+- (void)onChangedAlertBounceDockIcon:(id)sender;
 - (void)onChangedAlertNotification:(id)sender;
 - (void)onChangedAlertType:(id)sender;
 
