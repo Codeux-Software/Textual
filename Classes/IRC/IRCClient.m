104fa9ce3bb7ff65b3504105c9b18353fcf1b15e
Michael Morris <michael@codeux.com>
2013-07-02 09:42:21 -0400

    Include reference to Colloquy's chat core license in source. In reference to #399

--- Classes/IRC/IRCClient.m
+++ Classes/IRC/IRCClient.m
@@ -36,6 +36,45 @@
 
  *********************************************************************** */
 
+/* This source file contains work that originated from the Chat Core 
+ framework of the Colloquy project. The source in question is in relation
+ to the handling of SASL authentication requests. The license of the 
+ Chat Core project is as follows: 
+ 
+ This document can be found mirrored at the author's website:
+ <http://colloquy.info/project/browser/trunk/Resources/BSD%20License.txt>
+ 
+ No actual copyright is presented in the license file or the actual 
+ source file in which this work was obtained so the work is assumed to
+ be Copyright © 2000 - 2012 the Colloquy IRC Client
+ 
+ ------- License -------
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are
+ met:
+
+ 1. Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ 2. Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ 3. The name of the author may not be used to endorse or promote
+ products derived from this software without specific prior written
+ permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+ NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #import "TextualApplication.h"
 
 #define _isonCheckInterval			30
