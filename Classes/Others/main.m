d029da9e2144c3196177d5e9e11665f1671cdc8f
Michael Morris <mikey@codeux.com>
2013-08-01 17:25:42 -0400

    Okay

--- Classes/Others/main.m
+++ Classes/Others/main.m
@@ -4,10 +4,6 @@
 int main(int argc, const char *argv[])
 {
 	@autoreleasepool {
-		[RZUserDefaults() addSuiteNamed:@"com.codeux.textual"];
-		[RZUserDefaults() addSuiteNamed:@"com.codeux.irc.textual"];
-		[RZUserDefaults() addSuiteNamed:@"com.codeux.irc.textual.trial"];
-
 		NSApplicationMain(argc, argv);
 	}
 
