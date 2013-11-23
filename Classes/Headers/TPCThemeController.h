788d9d26454890fa43c2d74d4e934f0b8c26aadf
Michael Morris <mikey@codeux.com>
2013-11-23 06:43:08 -0500

    Cache the associated theme name for the current theme controller object

--- Classes/Headers/TPCThemeController.h
+++ Classes/Headers/TPCThemeController.h
@@ -53,7 +53,7 @@ typedef enum TPCThemeControllerStorageLocation : NSInteger {
 
 @interface TPCThemeController : NSObject
 @property (nonatomic, strong) NSURL *baseURL;
-
+@property (nonatomic, strong) NSString *associatedThemeName;
 @property (nonatomic, strong) TPCThemeSettings *customSettings;
 
 /* Calls for the active theme. */
