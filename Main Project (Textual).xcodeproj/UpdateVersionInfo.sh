f6fa2f8adecd1c5e3b7cba0e18379be8c8ce73e1
Michael Morris <michael@codeux.com>
2013-12-11 06:53:18 -0500

    Update profile

--- Main Project (Textual).xcodeproj/UpdateVersionInfo.sh
+++ Main Project (Textual).xcodeproj/UpdateVersionInfo.sh
@@ -32,25 +32,4 @@ else
 echo "#define TXBundleBuildReference	@\"${buildRef},nocdsign\"" >> BuildConfig.h
 echo "#define TXBundleBuiltWithoutCodeSigning                   1" >> BuildConfig.h
 fi
-
-echo "                                                                                        "
-echo " /!\ ***************************************************************************** /!\  "
-echo "                  WARNING: BUILDING UNSTABLE & UNSUPPORTED SOFTWARE.                    "
-echo " /!\ ***************************************************************************** /!\  "
-echo "                                                                                        "
-echo "   You are building a custom copy of Textual. Custom copies are not supported by        "
-echo "   Codeux Software or any members of the #textual IRC channel community. It is not      "
-echo "   recommended to build your own copy of Textual unless absolutely necessary.           "
-echo "                                                                                        "
-echo "   Textual is open source for various reasons, but we do not encourage many users       "
-echo "   to actually build and run it on their own. The source code, unless in the Mac        "
-echo "   App Store, is considered in a constant state of instability so use of it can         "
-echo "   lead to undefined behavior.                                                          "
-echo "                                                                                        "
-echo "   It is highly recommended that you use the Mac App Store version of Textual.          "
-echo "   The only reason you should be building it on your own is if you were asked to        "
-echo "   by one of the developers to test a fix specific to you. Wait for the official        "
-echo "   update or at least the one posted to our mailing list.                               "
-echo "                                                                                        "
-echo " /!\ ***************************************************************************** /!\  "
-echo "                                                                                        "
+                                                                                "
