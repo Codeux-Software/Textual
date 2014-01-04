b9b28383d66f9d9abc8bc8fb99e4c5ae53c06293
Michael Morris <michael@codeux.com>
2014-01-03 23:45:08 -0500

    Fixes crash when closing view

--- Classes/Dialogs/TDCServerSheet.m
+++ Classes/Dialogs/TDCServerSheet.m
@@ -618,6 +618,17 @@
 	/* Save changes. */
 	[self save];
 	
+	/* Break bonds. */
+	self.tabView.delegate = nil;
+	self.ignoreTable.delegate = nil;
+	self.channelTable.delegate = nil;
+	self.highlightsTable.delegate = nil;
+	
+	self.tabView.dataSource = nil;
+	self.ignoreTable.dataSource = nil;
+	self.channelTable.dataSource = nil;
+	self.highlightsTable.dataSource = nil;
+	
 	/* Inform delegate. */
 	if ([self.delegate respondsToSelector:@selector(serverSheetOnOK:)]) {
 		[self.delegate serverSheetOnOK:self];
@@ -639,6 +650,17 @@
 
 - (void)cancel:(id)sender
 {
+	/* Break bonds. */
+	self.tabView.delegate = nil;
+	self.ignoreTable.delegate = nil;
+	self.channelTable.delegate = nil;
+	self.highlightsTable.delegate = nil;
+	
+	self.tabView.dataSource = nil;
+	self.ignoreTable.dataSource = nil;
+	self.channelTable.dataSource = nil;
+	self.highlightsTable.dataSource = nil;
+
 	[self.sheet makeFirstResponder:nil];
 
 	[super cancel:nil]; 
