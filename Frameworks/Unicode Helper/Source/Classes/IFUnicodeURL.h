9a00772bdc8a9fdcc28e649ae29abfd10886bd59
Michael Morris <michael@codeux.com>
2014-01-14 17:50:11 -0500

    Committed files by mistake

--- Frameworks/Unicode Helper/Source/Classes/IFUnicodeURL.h
+++
@@ -1,9 +0,0 @@
-
-// Created by Sean Heber on 4/22/10.
-
-@interface NSURL (IFUnicodeURL)
-+ (NSURL *)URLWithUnicodeString:(NSString *)str;
-
-- (NSString *)unicodeAbsoluteString;
-- (NSString *)unicodeHost;
-@end
