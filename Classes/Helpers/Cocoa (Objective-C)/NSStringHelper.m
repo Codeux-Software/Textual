cabd61b94a64837f64d8ee5bff49f2a45c88b4b1
Michael Morris <mikey@codeux.com>
2014-02-20 09:53:41 -0500

    Block spaces

--- Classes/Helpers/Cocoa (Objective-C)/NSStringHelper.m
+++ Classes/Helpers/Cocoa (Objective-C)/NSStringHelper.m
@@ -375,7 +375,7 @@
 	NSAssertReturnR([usernameInt isUsername], NO);
 
 	/* Further compare values. */
-	if ([addressInt containsCharacters:@"!@"]) {
+	if ([addressInt containsCharacters:@"!@ "]) {
 		/* Host sections contain redundant characters. */
 		/* The host is not valid. */
 
