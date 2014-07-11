0e771b84ce982ea53f6aaa9ef7de640133f3341d
emsquared <mikey@codeux.com>
2014-07-11 14:41:51 -0400

    Rename class

--- Classes/Views/TVCBasicTableViewSeparatorCell.m
+++ Classes/Views/TVCBasicTableViewSeparatorCell.m
@@ -37,7 +37,40 @@
 
 #import "TextualApplication.h"
 
-#define TVCListSeparatorCellItemIndex		@"<TVCListSeparatorCell>"
+@implementation TVCBasicTableViewSeparatorCell
 
-@interface TVCListSeparatorCell : NSTextFieldCell
+- (void)drawWithFrame:(NSRect)cellFrame inView:(NSView *)controlView
+{
+	NSString *value = [self stringValue];
+	
+	if ([value isEqualToString:TVCBasicTableViewSeparatorCellItemIndex]) {
+		NSColor *fillColor = [NSColor darkGrayColor];
+		
+		CGFloat lineW = 0;
+		CGFloat lineX = 0;
+		CGFloat lineY = 0;
+		
+		lineY  = cellFrame.size.height;
+		lineY -= 2;
+		lineY /= 2;
+		lineY += 1.0;
+		
+		lineW = cellFrame.size.width;
+		
+		NSRect lineRect = NSMakeRect((cellFrame.origin.x + lineX), 
+									 (cellFrame.origin.y + lineY), lineW, 0.5);
+		
+		[fillColor set];
+		
+		NSRectFill(lineRect);
+	} else {
+		[super drawWithFrame:cellFrame inView:controlView];
+	}
+}
+
+- (NSRect)expansionFrameWithFrame:(NSRect)cellFrame inView:(NSView *)view
+{
+	return NSZeroRect;
+}
+
 @end
