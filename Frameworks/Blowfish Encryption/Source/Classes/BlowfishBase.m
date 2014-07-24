4052cdbd68903d328c998b9f688d6bbe945bc025
emsquared <mikey@codeux.com>
2014-07-23 23:56:27 -0400

    Add CBC cipher support for blowfish

--- Frameworks/Blowfish Encryption/Source/Classes/BlowfishBase.m
+++ Frameworks/Blowfish Encryption/Source/Classes/BlowfishBase.m
@@ -18,12 +18,20 @@
 	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 	THE SOFTWARE.
-*/
+ */
+
+/* This source file contains modified work of the original Dirt IRC proxy
+ project hosted at: https://github.com/flakes/mirc_fish_10/tree/master/fish_10
+ The author of this project has opted to not license their software and 
+ instead release it into the Public Domain. */
 
 #import "BlowfishBase.h"
 #import "NSDataHelper.h"
+#import "Base64Encoding.h"
 
 #include <openssl/blowfish.h>
+#include <openssl/evp.h>
+#include <openssl/rand.h>
 
 /* =============================================== */
 
@@ -56,10 +64,243 @@ static const signed char fish_unbase64[256] = {
 
 @implementation BlowfishBase
 
-#pragma mark -
-#pragma mark Encryption.
++ (NSString *)encrypt:(NSString *)rawInput key:(NSString *)secretKey algorithm:(CSFWBlowfishEncryptionAlgorithm)algorithm encoding:(NSStringEncoding)dataEncoding
+{
+	if (algorithm == CSFWBlowfishEncryptionDefaultAlgorithm || algorithm == CSFWBlowfishEncryptionECBAlgorithm) {
+		return [self ecb_encrypt:rawInput key:secretKey encoding:dataEncoding];
+	} else {
+		return [self cbc_encrypt:rawInput key:secretKey encoding:dataEncoding];
+	}
+}
 
-+ (NSString *)encrypt:(NSString *)rawInput key:(NSString *)secretKey encoding:(NSStringEncoding)dataEncoding
++ (NSString *)decrypt:(NSString *)rawInput key:(NSString *)secretKey algorithm:(CSFWBlowfishEncryptionAlgorithm)algorithm encoding:(NSStringEncoding)dataEncoding badBytes:(NSInteger *)badByteCount
+{
+	if (algorithm == CSFWBlowfishEncryptionDefaultAlgorithm || algorithm == CSFWBlowfishEncryptionECBAlgorithm) {
+		return [self ecb_decrypt:rawInput key:secretKey encoding:dataEncoding badBytes:badByteCount];
+	} else {
+		return [self cbc_decrypt:rawInput key:secretKey encoding:dataEncoding badBytes:badByteCount];
+	}
+}
+
+#pragma mark -
+#pragma mark CBC Encryption
+
++ (BOOL)walkBlowfishCypherForCBC:(EVP_CIPHER_CTX *)context inputStream:(unsigned char *)inputStream inputSize:(size_t)inputSize outputHandler:(NSMutableData **)outputHandler
+{
+	/* Define basic context. */
+	size_t _bytesLeft = inputSize;
+	
+	const unsigned char *_inputPointer = inputStream;
+	
+	unsigned char _temporaryBuffer[256];
+	
+	int _outLength;
+	
+	/* Begin looping blocks. */
+	while (_bytesLeft > 0) {
+		int _inSize = (int)_bytesLeft;
+		
+		if (_inSize > 256) {
+			_inSize = 256;
+		}
+		
+		if (EVP_CipherUpdate(context, _temporaryBuffer, &_outLength, _inputPointer, _inSize) == 0) {
+			return NO;
+		}
+		
+		[*outputHandler appendBytes:_temporaryBuffer length:_outLength];
+		
+		_bytesLeft -= _inSize;
+		
+		_inputPointer += _inSize;
+	}
+	
+	/* Finish looping. */
+	BOOL success = (EVP_CipherFinal_ex(context, _temporaryBuffer, &_outLength) == 1);
+	
+	if (success) {
+		[*outputHandler appendBytes:_temporaryBuffer length:_outLength];
+	}
+	
+	return success;
+}
+
++ (NSString *)cbc_encrypt:(NSString *)rawInput key:(NSString *)secretKey encoding:(NSStringEncoding)dataEncoding
+{
+	if ([secretKey length] <= 0 || [rawInput length] <= 0) {
+		return nil;
+	}
+	
+	const char *message	= [rawInput	 cStringUsingEncoding:dataEncoding];
+	const char *secrkey	= [secretKey cStringUsingEncoding:dataEncoding];
+	
+	size_t keylen = strlen(secrkey);
+	size_t msglen = strlen(message);
+
+	/* =============================================== */
+	
+	EVP_CIPHER_CTX context;
+	
+	const unsigned char iv[8] = {0};
+	
+	if (keylen > 56) {
+		keylen = 56;
+	} else {
+		keylen = keylen;
+	}
+	
+	/* =============================================== */
+	
+	/* Create structure for encryption. */
+	EVP_CIPHER_CTX_init(&context);
+	
+	EVP_CipherInit_ex(&context, EVP_bf_cbc(), NULL, NULL, NULL, 1);
+	
+	/* Set options for encryption. */
+	EVP_CIPHER_CTX_set_key_length(&context, (int)keylen);
+	
+	EVP_CIPHER_CTX_set_padding(&context, 0);
+	
+	/* Build session context. */
+	EVP_CipherInit_ex(&context, NULL, NULL, (const unsigned char *)secrkey, iv, 1);
+	
+	/* Prepare buffer. */
+	size_t bufferSize = msglen;
+	
+	if (bufferSize % 8) {
+		bufferSize += (8 - (bufferSize % 8));
+	}
+	
+	bufferSize += 8;
+	
+	/* =============================================== */
+	
+	/* Create buffer. */
+	unsigned char *inputStream = malloc(bufferSize);
+	
+	memset(inputStream, 0, bufferSize);
+	
+	/* General IV. */
+	unsigned char realIv[8];
+	
+	if (RAND_bytes(realIv, 8) == 0) {
+		RAND_pseudo_bytes(realIv, 8);
+	}
+	
+	/* Copy seed information. */
+	memcpy( inputStream, realIv, 8);
+	memcpy((inputStream + 8), message, msglen);
+	
+	/* =============================================== */
+	
+	/* Out output stream. */
+	NSMutableData *outputHandler = [NSMutableData data];
+	
+	/* Perform encryption. */
+	BOOL result = [BlowfishBase walkBlowfishCypherForCBC:&context inputStream:inputStream inputSize:bufferSize outputHandler:&outputHandler];
+	
+	EVP_CIPHER_CTX_cleanup(&context);
+	
+	free(inputStream);
+
+	if (result) {
+		return [CSFWBase64Encoding encodeData:outputHandler];
+	} else {
+		return nil;
+	}
+}
+
+
+#pragma mark -
+#pragma mark CBC Encryption
+
++ (NSString *)cbc_decrypt:(NSString *)rawInput key:(NSString *)secretKey encoding:(NSStringEncoding)dataEncoding badBytes:(NSInteger *)badByteCount
+{
+	if ([secretKey length] <= 0 || [rawInput length] <= 0) {
+		*badByteCount = 0;
+		
+		return nil;
+	}
+	
+	/* Decode encoded data. */
+	NSData *decodedMessage = [CSFWBase64Encoding decodeData:rawInput];
+	
+	/* Check if message is fragment. */
+	if ((([decodedMessage length] % 8) == 0 && [decodedMessage length] > 8) == NO) {
+		*badByteCount = 0;
+		
+		return nil; // Cancel operation.
+	}
+	
+	/* Begin decoding message. */
+	const char *message	= [decodedMessage bytes];
+	
+	const char *secrkey	= [secretKey cStringUsingEncoding:dataEncoding];
+	
+	size_t keylen = strlen(secrkey);
+	size_t msglen = strlen(message);
+	
+	/* =============================================== */
+	
+	EVP_CIPHER_CTX context;
+	
+	const unsigned char iv[8] = {0};
+	
+	if (keylen > 56) {
+		keylen = 56;
+	} else {
+		keylen = keylen;
+	}
+	
+	/* Create structure for encryption. */
+	EVP_CIPHER_CTX_init(&context);
+	
+	EVP_CipherInit_ex(&context, EVP_bf_cbc(), NULL, NULL, NULL, 0);
+	
+	/* Set options for encryption. */
+	EVP_CIPHER_CTX_set_key_length(&context, (int)keylen);
+	
+	EVP_CIPHER_CTX_set_padding(&context, 0);
+	
+	/* Build session context. */
+	EVP_CipherInit_ex(&context, NULL, NULL, (const unsigned char *)secrkey, iv, 0);
+	
+	/* =============================================== */
+	
+	/* Out output stream. */
+	NSMutableData *outputHandler = [NSMutableData data];
+	
+	/* Perform encryption. */
+	BOOL result = [BlowfishBase walkBlowfishCypherForCBC:&context inputStream:(unsigned char *)message inputSize:msglen outputHandler:&outputHandler];
+	
+	EVP_CIPHER_CTX_cleanup(&context);
+	
+	/* Return result. */
+	if (result) {
+		[outputHandler replaceBytesInRange:NSMakeRange(0, 8) withBytes:NULL length:0];
+		
+		NSData *finalData = nil;
+		
+		if (dataEncoding == NSUTF8StringEncoding) {
+			finalData = [outputHandler repairedCharacterBufferForUTF8Encoding:badByteCount];
+		} else {
+			finalData =  outputHandler;
+			
+			*badByteCount = 0;
+		}
+		
+		NSString *cypher = [[NSString alloc] initWithData:outputHandler encoding:dataEncoding];
+		
+		return cypher;
+	} else {
+		return nil;
+	}
+}
+
+#pragma mark -
+#pragma mark ECB Decryption
+
++ (NSString *)ecb_encrypt:(NSString *)rawInput key:(NSString *)secretKey encoding:(NSStringEncoding)dataEncoding
 {
 	if ([secretKey length] <= 0 || [rawInput length] <= 0) {
 		return nil;
@@ -74,6 +315,7 @@ static const signed char fish_unbase64[256] = {
 	/* =============================================== */
 
 	BF_KEY bfkey;
+	
     BF_set_key(&bfkey, (int)keylen, (const unsigned char *)secrkey);
 
 	NSInteger mallocSize = msglen;
@@ -149,11 +391,13 @@ static const signed char fish_unbase64[256] = {
 }
 
 #pragma mark -
-#pragma mark Decryption.
+#pragma mark ECB Decryption
 
-+ (NSString *)decrypt:(NSString *)rawInput key:(NSString *)secretKey encoding:(NSStringEncoding)dataEncoding badBytes:(NSInteger *)badByteCount
++ (NSString *)ecb_decrypt:(NSString *)rawInput key:(NSString *)secretKey encoding:(NSStringEncoding)dataEncoding badBytes:(NSInteger *)badByteCount
 {
 	if ([secretKey length] <= 0 || [rawInput length] <= 0) {
+		*badByteCount = 0;
+		
 		return nil;
 	}
 
@@ -166,6 +410,7 @@ static const signed char fish_unbase64[256] = {
 	/* =============================================== */
 
 	BF_KEY bfkey;
+	
     BF_set_key(&bfkey, (int)keylen, (const unsigned char *)secrkey);
 
     char *decrypted = malloc((msglen + 1));
@@ -236,6 +481,5 @@ static const signed char fish_unbase64[256] = {
 	
     return cypher;
 }
-
 
 @end
