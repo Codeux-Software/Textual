57c03b0c50711b4e2986693f09439b4fd2b06ee0
emsquared <mikey@codeux.com>
2014-08-08 16:47:45 -0400

    Always use custom user agent

--- Classes/Views/Channel View/TVCImageURLoader.m
+++ Classes/Views/Channel View/TVCImageURLoader.m
@@ -37,8 +37,6 @@
 
 #import "TextualApplication.h"
 
-#define _requestUserAgent					@"Textual/1.0 (+http://www.codeux.com/textual/wiki/Inline-Media-Scanner-User-Agent.wiki)"
-
 #define _imageLoaderMaxRequestTime			30
 
 /* We shouldn't want to load anything larger than this. */
@@ -91,7 +89,7 @@
 															   cachePolicy:NSURLRequestReloadIgnoringCacheData
 														   timeoutInterval:_imageLoaderMaxRequestTime];
 
-	[baseRequest setValue:_requestUserAgent forHTTPHeaderField:@"User-Agent"];
+	[baseRequest setValue:TVCLogViewCommonUserAgentString forHTTPHeaderField:@"User-Agent"];
 
 	/* This is stored in a local variable so that a user changing something during a load in
 	 progess, it does not fuck up any of the already existing requests. */
