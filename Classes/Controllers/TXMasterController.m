dd2744ee9d25fb924454a80f67ab067fe01d19f8
Michael Morris <michael@codeux.com>
2014-08-28 14:54:26 -0400

    Correct result type

--- Classes/Controllers/TXMasterController.m
+++ Classes/Controllers/TXMasterController.m
@@ -266,13 +266,13 @@
 	}
 	
 	if ([TPCPreferences confirmQuit]) {
-		NSInteger result = [TLOPopupPrompts dialogWindowWithQuestion:TXTLS(@"BasicLanguage[1000][1]")
-															   title:TXTLS(@"BasicLanguage[1000][2]") 
-													   defaultButton:TXTLS(@"BasicLanguage[1000][3]") 
-													 alternateButton:BLS(1009)
-													  suppressionKey:nil
-													 suppressionText:nil];
-		
+		BOOL result = [TLOPopupPrompts dialogWindowWithQuestion:TXTLS(@"BasicLanguage[1000][1]")
+														  title:TXTLS(@"BasicLanguage[1000][2]")
+												  defaultButton:TXTLS(@"BasicLanguage[1000][3]")
+												alternateButton:BLS(1009)
+												 suppressionKey:nil
+												suppressionText:nil];
+
 		return result;
 	}
 	
