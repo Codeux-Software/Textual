1892704f7d1d2c0beb88142908c6ce6d8dae0ba0
Michael Morris <mikey@codeux.com>
2014-09-06 07:35:52 -0400

    app.networkName() API

--- Resources/Javascript/API/core.js
+++ Resources/Javascript/API/core.js
@@ -196,6 +196,10 @@ Textual.styleSettingDidChange                       = function(changedKey) {};
 // app.channelName()				— Channel name of associated channel. Can be an actual channel name,
 //									  a nickname for a private message, or blank for the console.
 
+// C				— Returns the name of the client related to this view. If the actual network
+									  name of the connected server is known (e.g. "freenode IRC network"), then
+									  that is returned. Otherwise, the user configured name is returned.
+
 // app.serverAddress()				— Actual server address (e.g. verne.freenode.net) of the associated
 //									  server. This value is not available until raw numeric 005 is posted.
 
