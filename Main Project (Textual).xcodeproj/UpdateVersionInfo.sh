20c374fe389d7500e88a80f58e0183e2d7fa8f11
Michael Morris <michael@codeux.com>
2014-09-06 15:57:19 -0400

    New provisioning profile logic
    I got tired of creating a commit each time my provisioning profiles
    change. Therefore, I wrote a script which handles provisioning for me.
    
    Each time the App Store version of Textual is built, a pre-build script
    is ran which scans my Mac for provisioning profiles which match a
    specific name. If it finds one that matches, then it creates an
    automatically generated xcconfig file which Xcode can read for the
    provisioning profile UUID value.
    
    If no provisioning profile is found, then it creates a blank file so
    that the file at least exists.

--- Main Project (Textual).xcodeproj/UpdateVersionInfo.sh
+++ Main Project (Textual).xcodeproj/UpdateVersionInfo.sh
@@ -2,7 +2,6 @@
 
 cd "${PROJECT_DIR}/Resources/"
 
-bundleIdentifier=$(/usr/libexec/PlistBuddy -c "Print \"CFBundleIdentifier\"" Info.plist)
 bundleVersionForComparisons=$(/usr/libexec/PlistBuddy -c "Print \"TXBundleVersionForComparisons\"" Info.plist)
 bundleVersionShort=$(/usr/libexec/PlistBuddy -c "Print \"CFBundleShortVersionString\"" Info.plist)
 bundleVersion=$(/usr/libexec/PlistBuddy -c "Print \"CFBundleVersion\"" Info.plist)
@@ -17,9 +16,6 @@ buildRef="${bundleVersionShort}-${gitRefInfo}-${TEXTUAL_GITREF_BUILD_ID}"
 
 echo "Building Textual (Build Reference: ${gitRefInfo})"
 
-test "${bundleIdentifier}" != "${TEXTUAL_BUNDLE_ID}" && \
-	/usr/libexec/PlistBuddy -c "Set :\"CFBundleIdentifier\" ${TEXTUAL_BUNDLE_ID}" Info.plist
-
 mkdir -p "${PROJECT_DIR}/.tmp/"
 
 cd "${PROJECT_DIR}/.tmp/"
@@ -28,16 +24,16 @@ buildDate=`date +%s`
 
 echo "/* ANY CHANGES TO THIS FILE WILL NOT BE SAVED AND WILL NOT BE COMMITTED */" > BuildConfig.h
 echo "" >> BuildConfig.h
-echo "#define TXBundleBuildCommitCount				@\"${gitCommitCount}\"" >> BuildConfig.h
-echo "#define TXBundleBuildGroupIdentifier			@\"${TEXTUAL_GROUP_ID}\"" >> BuildConfig.h
-echo "#define TXBundleBuildDate						@\"${buildDate}\"" >> BuildConfig.h
-echo "#define TXBundleBuildVersion					@\"${bundleVersion}\"" >> BuildConfig.h
-echo "#define TXBundleBuildVersionShort				@\"${bundleVersionShort}\"" >> BuildConfig.h
-echo "#define TXBundleBuildVersionForComparisons	@\"${bundleVersionForComparisons}\"" >> BuildConfig.h
+echo "#define TXBundleBuildCommitCount					@\"${gitCommitCount}\"" >> BuildConfig.h
+echo "#define TXBundleBuildGroupIdentifier				@\"${TEXTUAL_GROUP_ID}\"" >> BuildConfig.h
+echo "#define TXBundleBuildDate							@\"${buildDate}\"" >> BuildConfig.h
+echo "#define TXBundleBuildVersion						@\"${bundleVersion}\"" >> BuildConfig.h
+echo "#define TXBundleBuildVersionShort					@\"${bundleVersionShort}\"" >> BuildConfig.h
+echo "#define TXBundleBuildVersionForComparisons		@\"${bundleVersionForComparisons}\"" >> BuildConfig.h
 
 if [ -n "$CODE_SIGN_IDENTITY" ]; then
-echo "#define TXBundleBuildReference	@\"${buildRef}\"" >> BuildConfig.h
+echo "#define TXBundleBuildReference					@\"${buildRef}\"" >> BuildConfig.h
 else
-echo "#define TXBundleBuildReference	@\"${buildRef},nocdsign\"" >> BuildConfig.h
-echo "#define TXBundleBuiltWithoutCodeSigning                   1" >> BuildConfig.h
+echo "#define TXBundleBuildReference					@\"${buildRef},nocdsign\"" >> BuildConfig.h
+echo "#define TXBundleBuiltWithoutCodeSigning           1" >> BuildConfig.h
 fi
