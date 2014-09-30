c6df3d4d7b43a1df11f862e860d73c6bf0c68802
Michael Morris <mikey@codeux.com>
2014-09-30 09:23:26 -0400

    Remove animations for switching between preference panes

--- Classes/Views/TVCAnimatedContentNavigationOutlineView.m
+++ Classes/Views/TVCAnimatedContentNavigationOutlineView.m
@@ -37,12 +37,7 @@
 
 #import "TextualApplication.h"
 
-@interface TVCAnimatedContentNavigationOutlineView ()
-@property (nonatomic, assign) BOOL navgiationTreeIsAnimating;
-@property (nonatomic, assign) NSInteger lastSelectedNavigationItem;
-@property (nonatomic, assign) NSInteger currentSelectedNavigationItem;
-@end
-
+/* This outline view used to be animated. See git history. */
 @implementation TVCAnimatedContentNavigationOutlineView
 
 #pragma mark -
@@ -50,20 +45,12 @@
 
 - (void)awakeFromNib
 {
-	self.navgiationTreeIsAnimating = NO;
-
-	self.lastSelectedNavigationItem = -1;
-	self.currentSelectedNavigationItem = -1;
-	
 	[self setDelegate:self];
 	[self setDataSource:self];
 }
 
 - (void)startAtSelectionIndex:(NSInteger)startingSelection
 {
-	self.lastSelectedNavigationItem = startingSelection;
-	self.currentSelectedNavigationItem = startingSelection;
-	
 	[self selectItemAtIndex:startingSelection];
 }
 
@@ -105,11 +92,7 @@
 
 - (BOOL)outlineView:(NSOutlineView *)outlineView shouldSelectItem:(id)item
 {
-	if (self.navgiationTreeIsAnimating) {
-		return NO;
-	} else {
-		return ([item containsKey:@"children"] == NO);
-	}
+	return ([item containsKey:@"children"] == NO);
 }
 
 - (id)outlineView:(NSOutlineView *)outlineView viewForTableColumn:(NSTableColumn *)tableColumn item:(NSDictionary *)item
@@ -126,11 +109,7 @@
 	NSInteger selectedRow = [self selectedRow];
 	
 	NSDictionary *navItem = [self itemAtRow:selectedRow];
-	
-	self.lastSelectedNavigationItem = self.currentSelectedNavigationItem;
-	
-	self.currentSelectedNavigationItem = selectedRow;
-	
+
 	[self presentView:navItem[@"view"]];
 	
 	id firstResponder = navItem[@"firstResponder"];
@@ -140,141 +119,39 @@
 	}
 }
 
-- (NSRect)currentWindowFrame
-{
-	return [self.parentWindow frame];
-}
-
 - (void)presentView:(NSView *)newView
 {
-	/* Determine direction. */
-	BOOL isGoingDown = NO;
-	
-	if (self.currentSelectedNavigationItem > self.lastSelectedNavigationItem) {
-		isGoingDown = YES;
-	}
-	
-	BOOL invertedScrollingDirection = [RZUserDefaults() boolForKey:@"com.apple.swipescrolldirection"];
-	
-	if (invertedScrollingDirection) {
-		if (isGoingDown) {
-			isGoingDown = NO;
-		} else {
-			isGoingDown = YES;
-		}
-	}
-	
-	self.navgiationTreeIsAnimating = YES;
-	
-	/* Set view frame. */
+	/* Calculate view frame. */
 	NSRect newViewFinalFrame = [newView frame];
-	
+
 	newViewFinalFrame.origin.x = 0;
 	newViewFinalFrame.origin.y = 0;
-	
+
 	newViewFinalFrame.size.width = self.contentViewPreferredWidth;
-	
+
 	if (newViewFinalFrame.size.height < self.contentViewPreferredHeight) {
 		newViewFinalFrame.size.height = self.contentViewPreferredHeight;
 	}
-	
-	/* Set frame animation will start at. */
-	NSRect newViewAnimationFrame = newViewFinalFrame;
-	
-	if (isGoingDown) {
-		newViewAnimationFrame.origin.y += newViewAnimationFrame.size.height;
-	} else {
-		newViewAnimationFrame.origin.y -= newViewAnimationFrame.size.height;
-	}
-	
-	[newView setFrame:newViewAnimationFrame];
-	
-	/* Update window size. */
-	NSRect contentViewFrame = [self.contentView frame];
-	
-	BOOL contentSizeDidntChange =  (fabs(contentViewFrame.size.height) == fabs(newViewFinalFrame.size.height));
-	
-	BOOL windowWillBecomeSmaller = (contentViewFrame.size.height >  newViewFinalFrame.size.height);
-	
-	/* Special condition to allow for smoother animations when going up
-	 with a window which is resizing to a smaller size. */
-	if (isGoingDown && windowWillBecomeSmaller) {
-		isGoingDown = NO;
-	}
-	
-	/* Set window frame. */
-	NSRect windowFrame = [self currentWindowFrame];
-	
-	if (contentSizeDidntChange == NO) {
-		windowFrame.size.height = (self.contentViewPadding + newViewFinalFrame.size.height);
-		
-		windowFrame.origin.y = (NSMaxY([self currentWindowFrame]) - windowFrame.size.height);
-		
-		[self.parentWindow setFrame:windowFrame display:YES animate:YES];
-	}
-	
-	/* Add new frame. */
-	[self.contentView addSubview:newView];
-	
-	/* Update content frame. */
-	if (contentSizeDidntChange == NO) {
-		contentViewFrame.size.height = newViewFinalFrame.size.height;
-	}
-	
-	/* Cancel any previous animation resets. */
-	[NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(timedRemoveFrame:) object:newView];
-	
-	/* Begin animation. */
-	[RZAnimationCurrentContext() setDuration:0.7];
-	
-	/* Find existing views. */
-	/* If count is 0, then that means preferences just launched
-	 and we have not added anything to our window yet. */
-	NSArray *subviews = [self.contentView subviews];
-	
-	NSInteger subviewCount = [subviews count];
-	
-	if (subviewCount > 1) {
-		NSView *oldView = [self.contentView subviews][0];
-		
-		/* If the number of visible views is more than 2 (the one we added and the old),
-		 then we erase the old views because the user could be clicking the navigation
-		 list fast which means the old views would stick until animations complete. */
-		if (subviewCount > 2) {
-			for (NSInteger i = 2; i < subviewCount; i++) {
-				[subviews[i] removeFromSuperview];
-			}
-		}
-		
-		NSRect oldViewAnimationFrame = [oldView frame]; // Set frame animation will end at.
-		
-		if (isGoingDown) {
-			oldViewAnimationFrame.origin.y = -(windowFrame.size.height); // No way anything will be there…
-		} else {
-			oldViewAnimationFrame.origin.y =   windowFrame.size.height; // No way anything will be there…
-		}
-		
-		[oldView.animator setAlphaValue:0.0];
-		[oldView.animator setFrame:oldViewAnimationFrame];
-		
-		[newView.animator setAlphaValue:1.0];
-		[newView.animator setFrame:newViewFinalFrame];
-		
-		[self performSelector:@selector(timedRemoveFrame:) withObject:oldView afterDelay:0.3];
-	} else {
-		[newView setFrame:newViewFinalFrame];
-		
-		self.navgiationTreeIsAnimating = NO;
-	}
-	
-	[self.parentWindow recalculateKeyViewLoop];
-}
 
-- (void)timedRemoveFrame:(NSView *)oldView
-{
-	self.navgiationTreeIsAnimating = NO;
-	
-	[oldView removeFromSuperview];
+	[newView setFrame:newViewFinalFrame];
+
+	/* Update window size. */
+	NSRect windowFrame = [self.parentWindow frame];
+
+	windowFrame.size.height = (self.contentViewPadding + newViewFinalFrame.size.height);
+
+	windowFrame.origin.y = (NSMaxY([self.parentWindow frame]) - windowFrame.size.height);
+
+	[self.parentWindow setFrame:windowFrame display:YES animate:NO];
+
+	/* Add new frame. */
+	if ([[self.contentView subviews] count] == 1) {
+		[[self.contentView subviews][0] removeFromSuperview];
+	}
+
+	[self.contentView addSubview:newView];
+
+	[self.parentWindow recalculateKeyViewLoop];
 }
 
 @end
