59f69d86b3c3781347f0fece1084163e676678ed
Michael Morris <michael@codeux.com>
2015-03-15 22:34:51 -0400

    Do not build blowfish encryption anymore

--- Main Project (Textual).xcodeproj/BuildExtensions.sh
+++ Main Project (Textual).xcodeproj/BuildExtensions.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-cd "${PROJECT_DIR}/Resources/Plugins/Blowfish Key Control"
-xcodebuild -alltargets -configuration "Release" BUNDLE_LOADER="${CODESIGNING_FOLDER_PATH}/Contents/MacOS/${EXECUTABLE_NAME}" CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}"
-
 cd "${PROJECT_DIR}/Resources/Plugins/Brag Spam"
 xcodebuild -alltargets -configuration "Release" BUNDLE_LOADER="${CODESIGNING_FOLDER_PATH}/Contents/MacOS/${EXECUTABLE_NAME}" CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}"
 
