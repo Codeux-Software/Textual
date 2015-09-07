Codeux-Software/Textual

commit a82c2abc583f7281ae1459b6b05e484114703e01

Large plugin infrastructure overhaul
• -didReceiveServerInputOnClient:senderInformation:messageInformation:
is now deprecated. This method will continue to be invoked on plugins
that support it, but it is recommended to eventually move to
-didReceiveServerInput:onClient:

-didReceiveServerInput:onClient: is passed an instance of
THOPluginDidReceiveServerInputConcreteObject which has properly cast
properties.

•
-didPostNewMessageForViewController:messageInfo:isThemeReload:isHistoryR
eload: is now deprecated. This method will continue to be invoked on
plugins that support it, but it is recommended to eventually move to
-didPostNewMessage:forViewController:

-didPostNewMessage:forViewController: is passed an instance of
THOPluginDidPostNewMessageConcreteObject which has properly cast
properties.

• The following methods which were deprecated in version 5.0.0 have
been removed and will no longer be invoked on loaded plugins:

-pluginLoadedIntoMemory:
-pluginUnloadedFromMemory

-pluginSupportsUserInputCommands
-pluginSupportsServerInputCommands

-preferencesView
-preferencesMenuItemName

-messageSentByUser:message:command:
-messageReceivedByServer:sender:message:

Classes/IRC/IRCClient.m
@@ -965,30 +965,25 @@ NSString * const IRCClientConfigurationWasUpdatedNotification = @"IRCClientConfi
 - (BOOL)outputRuleMatchedInMessage:(NSString *)raw inChannel:(IRCChannel *)chan withLineType:(TVCLogLineType)type
 {
 	NSObjectIsEmptyAssertReturn(raw, NO);
-	
+
 	if ([TPCPreferences removeAllFormatting] == NO) {
 		raw = [raw stripIRCEffects];
 	}
 
-	NSArray *rules = [sharedPluginManager() outputRulesForCommand:IRCCommandFromLineType(type)];
+	NSArray *rules = [sharedPluginManager() pluginOutputSuppressionRules];
 
-	for (NSArray *ruleData in rules) {
-		NSString *ruleRegex = ruleData[0];
-
-		if ([XRRegularExpression string:raw isMatchedByRegex:ruleRegex]) {
-			BOOL console = [ruleData boolAtIndex:0];
-			BOOL channel = [ruleData boolAtIndex:1];
-			BOOL queries = [ruleData boolAtIndex:2];
-
-			if ([chan isKindOfClass:[IRCChannel class]]) {
-				if ((chan.isClient && console) ||
-					(chan.isChannel && channel) ||
-					(chan.isPrivateMessage && queries)) {
-
+	for (THOPluginOutputSuppressionRule *ruleData in rules) {
+		if ([XRRegularExpression string:raw isMatchedByRegex:[ruleData match]]) {
+			if (chan) {
+				if (([chan isChannel] && [ruleData restrictChannel]) ||
+					([chan isPrivateMessage] && [ruleData restrictPrivateMessage]))
+				{
 					return YES;
 				}
 			} else {
-				return console;
+				if ([ruleData restrictConsole]) {
+					return YES;
+				}
 			}
 		}
 	}
@@ -3764,7 +3759,7 @@ NSString * const IRCClientConfigurationWasUpdatedNotification = @"IRCClientConfi
 
 - (void)printErrorReply:(IRCMessage *)m channel:(IRCChannel *)channel
 {
-	NSString *text = BLS(1139, [m numericReply], [m sequence]);
+	NSString *text = BLS(1139, [m commandNumeric], [m sequence]);
 
 	[self printToWebView:channel type:TVCLogLineDebugType command:[m command] nickname:nil messageBody:text isEncrypted:NO receivedAt:[m receivedAt] referenceMessage:nil completionBlock:nil];
 }
@@ -4057,8 +4052,8 @@ NSString * const IRCClientConfigurationWasUpdatedNotification = @"IRCClientConfi
 		}
 	}
 
-	if ([m numericReply] > 0) {
 		[self receiveNumericReply:m];
+	if ([m commandNumeric] > 0) {
 	} else {
 		NSInteger switchNumeric = [IRCCommandIndex indexOfIRCommand:[m command] publicSearch:NO];
 
@@ -5510,7 +5505,10 @@ NSString * const IRCClientConfigurationWasUpdatedNotification = @"IRCClientConfi
 - (NSString *)stringValueOfCapacity:(ClientIRCv3SupportedCapacities)capacity
 {
 	NSString *stringValue = nil;
-	
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wswitch"
+
 	switch (capacity) {
 		case ClientIRCv3SupportedCapacityAwayNotify:
 		{
@@ -5589,6 +5587,8 @@ NSString * const IRCClientConfigurationWasUpdatedNotification = @"IRCClientConfi
 			break;
 		}
 	}
+
+#pragma clang diagnostic pop
 	
 	return stringValue;
 }
@@ -6063,7 +6063,7 @@ NSString * const IRCClientConfigurationWasUpdatedNotification = @"IRCClientConfi
 
 - (void)receiveNumericReply:(IRCMessage *)m
 {
-	NSInteger n = [m numericReply];
+	NSInteger n = [m commandNumeric];
 
 	if (400 <= n && n < 600 && (n == 403) == NO && (n == 422) == NO) {
 		return [self receiveErrorNumericReply:m];
@@ -6240,7 +6240,7 @@ NSString * const IRCClientConfigurationWasUpdatedNotification = @"IRCClientConfi
 		case 305: // RPL_UNAWAY
 		case 306: // RPL_NOWAWAY
 		{
-			self.isAway = ([m numericReply] == 306);
+			self.isAway = (n == 306);
 
 			[self printUnknownReply:m];
             
@@ -7250,7 +7250,7 @@ NSString * const IRCClientConfigurationWasUpdatedNotification = @"IRCClientConfi
 
 - (void)receiveErrorNumericReply:(IRCMessage *)m
 {
-	NSInteger n = [m numericReply];
+	NSInteger n = [m commandNumeric];
 
 	switch (n) {
 		case 401: // ERR_NOSUCHNICK

Classes/Dialogs/Preferences/TDCPreferencesController.m
@@ -372,7 +372,7 @@
 				THOPluginItem *plugin = [sharedPluginManager() pluginsWithPreferencePanes][pluginIndex];
 
 				if (plugin) {
-					NSView *prefsView = [plugin pluginPreferenesPaneView];
+					NSView *prefsView = [plugin pluginPreferencesPaneView];
 
 					if (prefsView) {
 						[self firstPane:prefsView selectedItem:_toolbarItemIndexAddons];

Classes/Headers/THOPluginItem.h
@@ -41,35 +41,30 @@
  The code contained by this header file was designed to be used internally for
  Textual and may be dangerous to use otherwise. */
 
-// OldStyle = deprecated class names
-// NewStyle = current class names for THOPluginProtocol
-typedef NS_OPTIONS(NSUInteger, THOPluginItemSupportedFeaturesFlags) {
-	THOPluginItemSupportedFeaturePreferencePaneOldStyleFlag						= 1 << 0,
-	THOPluginItemSupportedFeatureSubscribedUserInputCommandsOldStyleFlag		= 1 << 1,
-	THOPluginItemSupportedFeatureSubscribedServerInputCommandsOldStyleFlag		= 1 << 2,
-	THOPluginItemSupportedFeatureInlineMediaManipulationFlag					= 1 << 3,
-	THOPluginItemSupportedFeatureNewMessagePostedEventFlag						= 1 << 4,
-	THOPluginItemSupportedFeatureWillRenderMessageEventFlag						= 1 << 5,
-	THOPluginItemSupportedFeaturePreferencePaneNewStyleFlag						= 1 << 6,
-	THOPluginItemSupportedFeatureUserInputDataInterceptionFlag					= 1 << 7,
-	THOPluginItemSupportedFeatureServerInputDataInterceptionFlag				= 1 << 8,
-	THOPluginItemSupportedFeatureSubscribedUserInputCommandsNewStyleFlag		= 1 << 9,
-	THOPluginItemSupportedFeatureSubscribedServerInputCommandsNewStyleFlag		= 1 << 10,
-	THOPluginItemSupportedFeatureOutputSuppressionRulesFlag						= 1 << 11
+typedef NS_OPTIONS(NSUInteger, THOPluginItemSupportedFeatures) {
+	THOPluginItemSupportsInlineMediaManipulation			= 1 << 0,
+	THOPluginItemSupportsNewMessagePostedEvent				= 1 << 1,
+	THOPluginItemSupportsOutputSuppressionRules				= 1 << 2,
+	THOPluginItemSupportsPreferencePane						= 1 << 3,
+	THOPluginItemSupportsServerInputDataInterception		= 1 << 4,
+	THOPluginItemSupportsSubscribedServerInputCommands		= 1 << 5,
+	THOPluginItemSupportsSubscribedUserInputCommands		= 1 << 6,
+	THOPluginItemSupportsUserInputDataInterception			= 1 << 7,
+	THOPluginItemSupportsWillRenderMessageEvent				= 1 << 8
 };
 
 @interface THOPluginItem : NSObject
-@property (nonatomic, strong) id primaryClass;
-@property (nonatomic, assign) THOPluginItemSupportedFeaturesFlags supportedFeatures;
-@property (nonatomic, copy) NSArray *supportedUserInputCommands;
-@property (nonatomic, copy) NSArray *supportedServerInputCommands;
-@property (nonatomic, copy) NSDictionary *outputSuppressionRules;
-@property (readonly, strong) NSView *pluginPreferenesPaneView;
+@property (readonly, strong) id primaryClass;
+@property (readonly, assign) THOPluginItemSupportedFeatures supportedFeatures;
+@property (readonly, copy) NSArray *supportedUserInputCommands;
+@property (readonly, copy) NSArray *supportedServerInputCommands;
+@property (readonly, copy) NSArray *outputSuppressionRules;
+@property (readonly, strong) NSView *pluginPreferencesPaneView;
 @property (readonly, copy) NSString *pluginPreferencesPaneMenuItemName;
 
 - (BOOL)loadBundle:(NSBundle *)bundle;
 
-- (BOOL)supportsFeature:(THOPluginItemSupportedFeaturesFlags)feature;
+- (BOOL)supportsFeature:(THOPluginItemSupportedFeatures)feature;
 
 - (void)sendDealloc;
 @end

Classes/Headers/THOPluginManager.h
@@ -59,7 +59,9 @@
 
 @property (readonly, copy) NSArray *pluginsWithPreferencePanes;
 
-- (NSArray *)outputRulesForCommand:(NSString *)command;
+@property (readonly, copy) NSArray *pluginOutputSuppressionRules;
+
+@property (readonly) BOOL atleastOnePluginWantsPostNewMessageEvent;
 
 - (void)findHandlerForOutgoingCommand:(NSString *)command
 						   scriptPath:(NSString **)scriptPath
@@ -82,13 +84,10 @@
 - (void)sendServerInputDataToBundles:(IRCClient *)client message:(IRCMessage *)message;
 - (void)sendUserInputDataToBundles:(IRCClient *)client message:(NSString *)message command:(NSString *)command;
 
+- (void)postNewMessageEventForViewController:(TVCLogController *)viewController withObject:(THOPluginDidPostNewMessageConcreteObject *)messageObject;
+
 - (NSString *)postWillRenderMessageEvent:(NSString *)newMessage
 					   forViewController:(TVCLogController *)viewController
 								lineType:(TVCLogLineType)lineType
 							  memberType:(TVCLogLineMemberType)memberType;
-
-- (void)postNewMessageEventForViewController:(TVCLogController *)logController
-								 messageInfo:(NSDictionary *)messageInfo
-							   isThemeReload:(BOOL)isThemeReload
-							 isHistoryReload:(BOOL)isHistoryReload;
 @end

Classes/Headers/THOPluginProtocolPrivate.h
@@ -5,7 +5,6 @@
                    | |  __/>  <| |_| |_| | (_| | |
                    |_|\___/_/\_\\__|\__,_|\__,_|_|
 
- Copyright (c) 2008 - 2010 Satoshi Nakagawa <psychs AT limechat DOT net>
  Copyright (c) 2010 - 2015 Codeux Software, LLC & respective contributors.
         Please see Acknowledgements.pdf for additional information.
 
@@ -38,31 +37,32 @@
 
 #import "TextualApplication.h"
 
-@interface IRCMessage : NSObject
-@property (nonatomic, copy) IRCPrefix *sender;
-@property (nonatomic, copy) NSString *command;
-@property (nonatomic, assign) NSInteger commandNumeric;
-@property (nonatomic, copy) NSArray *params;
-@property (nonatomic, copy) NSDate *receivedAt;
-@property (nonatomic, assign) BOOL isPrintOnlyMessage; /* The message should be parsed and passed to print: but special actions such as adding/removing user from member list should be ignored. */
-@property (nonatomic, assign) BOOL isHistoric; // Whether a custom @time= was supplied during parsing.
+@interface THOPluginDidPostNewMessageConcreteObject ()
+@property (nonatomic, readwrite, assign) BOOL isProcessedInBulk;
+@property (nonatomic, readwrite, copy) NSString *messageContents;
+@property (nonatomic, readwrite, copy) NSString *lineNumber;
+@property (nonatomic, readwrite, copy) NSString *senderNickname;
+@property (nonatomic, readwrite, assign) TVCLogLineType lineType;
+@property (nonatomic, readwrite, assign) TVCLogLineMemberType memberType;
+@property (nonatomic, readwrite, copy) NSDate *receivedAt;
+@property (nonatomic, readwrite, copy) NSArray *listOfHyperlinks;
+@property (nonatomic, readwrite, copy) NSSet *listOfUsers;
+@property (nonatomic, readwrite, assign) BOOL keywordMatchFound;
+@end
 
-- (instancetype)initWithLine:(NSString *)line;
+#pragma mark -
 
-- (void)parseLine:(NSString *)line;
-- (void)parseLine:(NSString *)line forClient:(IRCClient *)client;
-
-@property (readonly, copy) NSString *senderNickname;
-@property (readonly, copy) NSString *senderUsername;
-@property (readonly, copy) NSString *senderAddress;
-@property (readonly, copy) NSString *senderHostmask;
-
-@property (readonly) BOOL senderIsServer;
-
-@property (readonly) NSInteger paramsCount;
-
-- (NSString *)paramAt:(NSInteger)index;
-
-@property (readonly, copy) NSString *sequence;
-- (NSString *)sequence:(NSInteger)index;
+@interface THOPluginDidReceiveServerInputConcreteObject ()
+@property (nonatomic, readwrite, assign) BOOL senderIsServer;
+@property (nonatomic, readwrite, copy) NSString *senderNickname;
+@property (nonatomic, readwrite, copy) NSString *senderUsername;
+@property (nonatomic, readwrite, copy) NSString *senderAddress;
+@property (nonatomic, readwrite, copy) NSString *senderHostmask;
+@property (nonatomic, readwrite, copy) NSDate *receivedAt;
+@property (nonatomic, readwrite, copy) NSString *messageSequence;
+@property (nonatomic, readwrite, copy) NSArray *messageParamaters;
+@property (nonatomic, readwrite, copy) NSString *messageCommand;
+@property (nonatomic, readwrite, assign) NSInteger messageCommandNumeric;
+@property (nonatomic, readwrite, copy) NSString *networkAddress;
+@property (nonatomic, readwrite, copy) NSString *networkName;
 @end

Classes/Headers/TextualApplication.h
@@ -313,9 +313,9 @@
 	#import "TDChannelPropertiesSheet.h"
 
 	/* Helpers. */
+	#import "THOPluginProtocol.h"
 	#import "THOPluginItem.h"
 	#import "THOPluginManager.h"
-	#import "THOPluginProtocol.h"
 	#import "THOUnicodeHelper.h"
 
 	/* Library. */

Classes/Helpers/Plugin Architecture/THOPluginItem.m
@@ -39,6 +39,16 @@
 
 #import "BuildConfig.h"
 
+@interface THOPluginItem ()
+@property (nonatomic, readwrite, strong) id primaryClass;
+@property (nonatomic, readwrite, assign) THOPluginItemSupportedFeatures supportedFeatures;
+@property (nonatomic, readwrite, copy) NSArray *supportedUserInputCommands;
+@property (nonatomic, readwrite, copy) NSArray *supportedServerInputCommands;
+@property (nonatomic, readwrite, copy) NSArray *outputSuppressionRules;
+@property (nonatomic, readwrite, strong) NSView *pluginPreferencesPaneView;
+@property (nonatomic, readwrite, copy) NSString *pluginPreferencesPaneMenuItemName;
+@end
+
 @implementation THOPluginItem
 
 #define VOCT(o, t)				 [o isKindOfClass:[t class]]
@@ -47,8 +57,8 @@
 - (BOOL)loadBundle:(NSBundle *)bundle
 {
 	/* Only load once. */
-	if ((self.primaryClass == nil) == NO) {
-		NSAssert(NO, @"loadBundle: called a THOPluginItem instance that is already loaded.");
+	if (self.primaryClass != nil) {
+		NSAssert(NO, @"-loadBundle: called a THOPluginItem instance that is already loaded");
 	}
 
 	/* Begin version comparison. */
@@ -98,278 +108,134 @@
 
 	self.primaryClass = [principalClass new];
 
-	self.supportedFeatures = 0;
-
-	/* Say hello! */
-	BOOL supportsOldFeature = [self.primaryClass respondsToSelector:@selector(pluginLoadedIntoMemory:)];
-	BOOL supportsNewFeature = [self.primaryClass respondsToSelector:@selector(pluginLoadedIntoMemory)];
-	
-	if (supportsOldFeature || supportsNewFeature)
-	{
-		if (supportsNewFeature) {
-			[self.primaryClass pluginLoadedIntoMemory];
-		} else {
-TEXTUAL_IGNORE_DEPRECATION_BEGIN
-			[self.primaryClass pluginLoadedIntoMemory:worldController()];
-TEXTUAL_IGNORE_DEPRECATION_END
-
-			LogToConsole(@"DEPRECATED: Primary class %@ uses deprecated -pluginLoadedIntoMemory:", self.primaryClass);
-		}
+	if ([self.primaryClass respondsToSelector:@selector(pluginLoadedIntoMemory)]) {
+		[self.primaryClass pluginLoadedIntoMemory];
 	}
-	
+
+	/* Build list of supported features. */
+	THOPluginItemSupportedFeatures supportedFeatures = 0;
+
 	/* Process server output suppression rules. */
-	if ([self.primaryClass respondsToSelector:@selector(pluginOutputDisplayRules)])
+	if ([self.primaryClass respondsToSelector:@selector(pluginOutputSuppressionRules)])
 	{
-		// Use id, never assume what a 3rd-party might give.
-		id outputRulesO = [self.primaryClass pluginOutputDisplayRules];
+		id outputRules = [self.primaryClass pluginOutputSuppressionRules];
 
-		if (VTAE(outputRulesO, NSDictionary)) {
-			NSMutableDictionary *sharedRules = [NSMutableDictionary dictionary];
+		if (VTAE(outputRules, NSArray)) {
+			NSMutableArray *sharedRules = [NSMutableArray array];
 
-			for (NSString *command in outputRulesO) { // Dictionary keys are always NSString.
-				NSArray *appndix = [self processOutputSuppressionRules:outputRulesO forCommand:command];
-
-				if (NSObjectIsNotEmpty(appndix)) {
-					sharedRules[command] = appndix;
+			for (id outputRule in outputRules) {
+				if (VOCT(outputRule, THOPluginOutputSuppressionRule)) {
+					[sharedRules addObject:outputRule];
 				}
 			}
 
-			[self setOutputSuppressionRules:sharedRules];
+			self.outputSuppressionRules = sharedRules;
 
-			[self enableFeature:THOPluginItemSupportedFeatureOutputSuppressionRulesFlag];
+			supportedFeatures |= THOPluginItemSupportsOutputSuppressionRules;
 		}
 	}
 
 	/* Does the bundle have a preference pane?... */
-	supportsOldFeature = ([self.primaryClass respondsToSelector:@selector(preferencesMenuItemName)] &&
-						  [self.primaryClass respondsToSelector:@selector(preferencesView)]);
-
-	supportsNewFeature = ([self.primaryClass respondsToSelector:@selector(pluginPreferencesPaneMenuItemName)] &&
-						  [self.primaryClass respondsToSelector:@selector(pluginPreferencesPaneView)]);
-	
-	if (supportsOldFeature || supportsNewFeature)
+	if ([self.primaryClass respondsToSelector:@selector(pluginPreferencesPaneMenuItemName)] &&
+		[self.primaryClass respondsToSelector:@selector(pluginPreferencesPaneView)])
 	{
-		id itemView = nil;
-		id itemName = nil;
-		
-		if (supportsNewFeature) {
-			itemView = [self.primaryClass pluginPreferencesPaneView];
-			itemName = [self.primaryClass pluginPreferencesPaneMenuItemName];
-		} else {
-TEXTUAL_IGNORE_DEPRECATION_BEGIN
-			itemView = [self.primaryClass preferencesView];
-			itemName = [self.primaryClass preferencesMenuItemName];
-TEXTUAL_IGNORE_DEPRECATION_END
-			
-			LogToConsole(@"DEPRECATED: Primary class %@ uses deprecated -preferencesMenuItemName", self.primaryClass);
-			LogToConsole(@"DEPRECATED: Primary class %@ uses deprecated -preferencesView", self.primaryClass);
-		}
+		id itemView = [self.primaryClass pluginPreferencesPaneView];
+		id itemName = [self.primaryClass pluginPreferencesPaneMenuItemName];
 
 		if (VTAE(itemName, NSString) && VOCT(itemView, NSView)) {
-			if (supportsNewFeature == NO) {
-				[self enableFeature:THOPluginItemSupportedFeaturePreferencePaneOldStyleFlag];
-			} else {
-				[self enableFeature:THOPluginItemSupportedFeaturePreferencePaneNewStyleFlag];
-			}
+			supportedFeatures |= THOPluginItemSupportsPreferencePane;
 		}
 	}
 
 	/* Process user input commands. */
-	supportsOldFeature = ([self.primaryClass respondsToSelector:@selector(messageSentByUser:message:command:)] &&
-						  [self.primaryClass respondsToSelector:@selector(pluginSupportsUserInputCommands)]);
-	
-	supportsNewFeature = ([self.primaryClass respondsToSelector:@selector(userInputCommandInvokedOnClient:commandString:messageString:)] &&
-						  [self.primaryClass respondsToSelector:@selector(subscribedUserInputCommands)]);
-	
-	if (supportsOldFeature || supportsNewFeature)
+	if ([self.primaryClass respondsToSelector:@selector(subscribedUserInputCommands)] &&
+		[self.primaryClass respondsToSelector:@selector(userInputCommandInvokedOnClient:commandString:messageString:)])
 	{
-		id spdcmds;
+		id subscribedCommands = [self.primaryClass subscribedUserInputCommands];
 		
-		if (supportsNewFeature) {
-			spdcmds = [self.primaryClass subscribedUserInputCommands];
-		} else {
-TEXTUAL_IGNORE_DEPRECATION_BEGIN
-			spdcmds = [self.primaryClass pluginSupportsUserInputCommands];
-TEXTUAL_IGNORE_DEPRECATION_END
-
-			LogToConsole(@"DEPRECATED: Primary class %@ uses deprecated -messageSentByUser:message:command:", self.primaryClass);
-			LogToConsole(@"DEPRECATED: Primary class %@ uses deprecated -pluginSupportsUserInputCommands", self.primaryClass);
-		}
-		
-		if (VTAE(spdcmds, NSArray)) {
+		if (VTAE(subscribedCommands, NSArray)) {
 			NSMutableArray *supportedCommands = [NSMutableArray array];
 
-			for (id command in spdcmds) {
-				if (VOCT(command, NSString))  {
-					if (NSObjectIsNotEmpty(command)) {
-						[supportedCommands addObject:[command lowercaseString]];
-					}
-				}
-			}
-
-			if (supportsNewFeature) {
-				[self enableFeature:THOPluginItemSupportedFeatureSubscribedUserInputCommandsNewStyleFlag];
-			} else {
-				[self enableFeature:THOPluginItemSupportedFeatureSubscribedUserInputCommandsOldStyleFlag];
-			}
-
-			[self setSupportedUserInputCommands:supportedCommands];
-		}
-	}
-
-	/* Process server input commands. */
-	supportsOldFeature = ([self.primaryClass respondsToSelector:@selector(messageReceivedByServer:sender:message:)] &&
-						  [self.primaryClass respondsToSelector:@selector(pluginSupportsServerInputCommands)]);
-	
-	supportsNewFeature = ([self.primaryClass respondsToSelector:@selector(didReceiveServerInputOnClient:senderInformation:messageInformation:)] &&
-						  [self.primaryClass respondsToSelector:@selector(subscribedServerInputCommands)]);
-	
-	if (supportsOldFeature || supportsNewFeature)
-	{
-		id spdcmds;
-		
-		if (supportsNewFeature) {
-			spdcmds = [self.primaryClass subscribedServerInputCommands];
-		} else {
-TEXTUAL_IGNORE_DEPRECATION_BEGIN
-			spdcmds = [self.primaryClass pluginSupportsServerInputCommands];
-TEXTUAL_IGNORE_DEPRECATION_END
-
-			LogToConsole(@"DEPRECATED: Primary class %@ uses deprecated -messageReceivedByServer:sender:message:", self.primaryClass);
-			LogToConsole(@"DEPRECATED: Primary class %@ uses deprecated -pluginSupportsServerInputCommands", self.primaryClass);
-		}
-
-		if (VTAE(spdcmds, NSArray)) {
-			NSMutableArray *supportedCommands = [NSMutableArray array];
-
-			for (id command in spdcmds) {
-				if (VOCT(command, NSString))  {
+			for (id command in subscribedCommands) {
+				if (VTAE(command, NSString))  {
 					[supportedCommands addObject:[command lowercaseString]];
 				}
 			}
 
-			if (supportsNewFeature) {
-				[self enableFeature:THOPluginItemSupportedFeatureSubscribedServerInputCommandsNewStyleFlag];
-			} else {
-				[self enableFeature:THOPluginItemSupportedFeatureSubscribedServerInputCommandsOldStyleFlag];
+			self.supportedUserInputCommands = supportedCommands;
+
+			supportedFeatures |= THOPluginItemSupportsSubscribedUserInputCommands;
+		}
+	}
+
+	/* Process server input commands. */
+	if ( [self.primaryClass respondsToSelector:@selector(subscribedServerInputCommands)] &&
+		([self.primaryClass respondsToSelector:@selector(didReceiveServerInput:onClient:)] ||
+		 [self.primaryClass respondsToSelector:@selector(didReceiveServerInputOnClient:senderInformation:messageInformation:)]))
+	{
+		id subscribedCommands = [self.primaryClass subscribedServerInputCommands];
+
+		if (VTAE(subscribedCommands, NSArray)) {
+			NSMutableArray *supportedCommands = [NSMutableArray array];
+
+			for (id command in subscribedCommands) {
+				if (VTAE(command, NSString))  {
+					[supportedCommands addObject:[command lowercaseString]];
+				}
 			}
 
-			[self setSupportedServerInputCommands:supportedCommands];
+			self.supportedServerInputCommands = supportedCommands;
+
+			supportedFeatures |= THOPluginItemSupportsSubscribedServerInputCommands;
 		}
 	}
 	
 	/* Check whether plugin supports certain evnets so we do not have
 	 to ask if it responds to the responder everytime we call it. */
-	
+
 	/* Renderer events. */
-	if ([self.primaryClass respondsToSelector:@selector(didPostNewMessageForViewController:messageInfo:isThemeReload:isHistoryReload:)]) {
-		[self enableFeature:THOPluginItemSupportedFeatureNewMessagePostedEventFlag];
+	if ([self.primaryClass respondsToSelector:@selector(didPostNewMessage:forViewController:)] ||
+		[self.primaryClass respondsToSelector:@selector(didPostNewMessageForViewController:messageInfo:isThemeReload:isHistoryReload:)])
+	{
+		supportedFeatures |= THOPluginItemSupportsNewMessagePostedEvent;
 	}
 	
 	if ([self.primaryClass respondsToSelector:@selector(willRenderMessage:forViewController:lineType:memberType:)]) {
-		[self enableFeature:THOPluginItemSupportedFeatureWillRenderMessageEventFlag];
+		supportedFeatures |= THOPluginItemSupportsWillRenderMessageEvent;
 	}
 	
 	/* Inline media. */
 	if ([self.primaryClass respondsToSelector:@selector(processInlineMediaContentURL:)]) {
-		[self enableFeature:THOPluginItemSupportedFeatureInlineMediaManipulationFlag];
+		supportedFeatures |= THOPluginItemSupportsInlineMediaManipulation;
 	}
 	
 	/* Data interception. */
 	if ([self.primaryClass respondsToSelector:@selector(interceptServerInput:for:)]) {
-		[self enableFeature:THOPluginItemSupportedFeatureServerInputDataInterceptionFlag];
+		supportedFeatures |= THOPluginItemSupportsServerInputDataInterception;
 	}
 	
 	if ([self.primaryClass respondsToSelector:@selector(interceptUserInput:command:)]) {
-		[self enableFeature:THOPluginItemSupportedFeatureUserInputDataInterceptionFlag];
+		supportedFeatures |= THOPluginItemSupportsUserInputDataInterception;
 	}
+
+	/* Finish up */
+	self.supportedFeatures = supportedFeatures;
 	
 	return YES;
 }
 
 - (void)sendDealloc
 {
-	if ([self.primaryClass respondsToSelector:@selector(pluginUnloadedFromMemory)]) {
-TEXTUAL_IGNORE_DEPRECATION_BEGIN
-		[self.primaryClass pluginUnloadedFromMemory];
-TEXTUAL_IGNORE_DEPRECATION_END
-	} else if ([self.primaryClass respondsToSelector:@selector(pluginWillBeUnloadedFromMemory)]) {
+	if (self.primaryClass == nil) {
+		return; // Send to where... ?
+	}
+
+	if ([self.primaryClass respondsToSelector:@selector(pluginWillBeUnloadedFromMemory)]) {
 		[self.primaryClass pluginWillBeUnloadedFromMemory];
 	}
 }
 
-- (NSArray *)processOutputSuppressionRules:(NSDictionary *)outputRules forCommand:(NSString *)sourceCommand
-{
-	id commandRules = outputRules[sourceCommand];
-	
-	if (VTAE(commandRules, NSArray)) {
-		NSArray *newBosses = nil;
-
-		for (id commandRule in commandRules) {
-			if (VTAE(commandRule, NSArray) && [commandRule count] == 4) {
-				NSString *ruleMatch	= commandRule[0];
-
-				id hideChannel	= commandRule[1];
-				id hideQuery	= commandRule[2];
-				id hideConsole	= commandRule[3];
-
-				NSArray *bossEntry = @[ruleMatch, hideConsole, hideChannel, hideQuery];
-
-				if (NSObjectIsEmpty(newBosses)) {
-					newBosses = @[bossEntry];
-				} else {
-					newBosses = [newBosses arrayByAddingObject:bossEntry];
-				}
-			}
-		}
-
-		return newBosses;
-	}
-
-	return nil;
-}
-
-- (NSView *)pluginPreferenesPaneView
-{
-	if ([self supportsFeature:THOPluginItemSupportedFeaturePreferencePaneNewStyleFlag]) {
-		return [self.primaryClass pluginPreferencesPaneView];
-	} else if ([self supportsFeature:THOPluginItemSupportedFeaturePreferencePaneOldStyleFlag]) {
-TEXTUAL_IGNORE_DEPRECATION_BEGIN
-		return [self.primaryClass preferencesView];
-TEXTUAL_IGNORE_DEPRECATION_END
-	} else {
-		return nil;
-	}
-}
-
-- (NSString *)pluginPreferencesPaneMenuItemName
-{
-	if ([self supportsFeature:THOPluginItemSupportedFeaturePreferencePaneNewStyleFlag]) {
-		return [self.primaryClass pluginPreferencesPaneMenuItemName];
-	} else if ([self supportsFeature:THOPluginItemSupportedFeaturePreferencePaneOldStyleFlag]) {
-TEXTUAL_IGNORE_DEPRECATION_BEGIN
-		return [self.primaryClass preferencesMenuItemName];
-TEXTUAL_IGNORE_DEPRECATION_END
-	} else {
-		return nil;
-	}
-}
-
-- (void)enableFeature:(THOPluginItemSupportedFeaturesFlags)feature
-{
-	if ([self supportsFeature:feature] == NO) {
-		_supportedFeatures |= feature;
-	}
-}
-
-- (void)disableFeature:(THOPluginItemSupportedFeaturesFlags)feature
-{
-	if ([self supportsFeature:feature]) {
-		_supportedFeatures &= ~feature;
-	}
-}
-
-- (BOOL)supportsFeature:(THOPluginItemSupportedFeaturesFlags)feature
+- (BOOL)supportsFeature:(THOPluginItemSupportedFeatures)feature
 {
 	return ((_supportedFeatures & feature) == feature);
 }

commit 9b01aa1969cd49af30c3aff39033ffc0e049b549

Fix regression introduced by a82c2ab

Classes/IRC/IRCClient.m
@@ -4052,8 +4052,8 @@ NSString * const IRCClientConfigurationWasUpdatedNotification = @"IRCClientConfi
 		}
 	}
 
-		[self receiveNumericReply:m];
 	if ([m commandNumeric] > 0) {
+		[self receiveNumericReply:m];
 	} else {
 		NSInteger switchNumeric = [IRCCommandIndex indexOfIRCommand:[m command] publicSearch:NO];
 