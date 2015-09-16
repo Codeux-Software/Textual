24999508f5a9f69f844859e75f93c2567f5f95c4
Michael Morris <michael@codeux.com>
2015-09-16 11:35:44 -0400

    Bundle "Chat Filter" plugin

--- Main Project (Textual).xcodeproj/BuildExtensions.sh
+++ Main Project (Textual).xcodeproj/BuildExtensions.sh
@@ -3,6 +3,9 @@
 cd "${PROJECT_DIR}/Resources/Plugins/Brag Spam"
 xcodebuild -target "BragSpam" -configuration "Release" BUNDLE_LOADER="${CODESIGNING_FOLDER_PATH}/Contents/MacOS/${EXECUTABLE_NAME}" CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}"
 
+cd "${PROJECT_DIR}/Resources/Plugins/Chat Filter"
+xcodebuild -target "ChatFilterExtension" -configuration "Release" BUNDLE_LOADER="${CODESIGNING_FOLDER_PATH}/Contents/MacOS/${EXECUTABLE_NAME}" CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}"
+
 cd "${PROJECT_DIR}/Resources/Plugins/Smiley Converter"
 xcodebuild -target "SmileyConverter" -configuration "Release" BUNDLE_LOADER="${CODESIGNING_FOLDER_PATH}/Contents/MacOS/${EXECUTABLE_NAME}" CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}"
 
