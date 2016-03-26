03144c796dcb2efcbf211c5688f16b8db5a7a705
Michael Morris <michael@codeux.com>
2016-03-26 17:11:09 -0400

    Disable WK2 on OS X Yosemite for now
    When invoking -evaluateJavaScript:completionHandler: on OS X Yosemite,
    WebKit creates a new JavaScript context each time. Textual invoking JS
    hundreds of times a minute (in some rare cases, large playbacks)
    results in hundreds of threads being created which leads to a freeze
    and crash.
    
    OS X El Capitan reuses context which means this is not a problem. See
    this bug: https://bugs.webkit.org/show_bug.cgi?id=146416

--- Classes/Preferences/TPCPreferences.m
+++ Classes/Preferences/TPCPreferences.m
@@ -514,15 +514,6 @@ NSInteger const TPCPreferencesDictionaryVersion		= 100;
 
 	if ([XRSystemInformation isUsingOSXElCapitanOrLater]) {
 		return YES;
-	} else if ([XRSystemInformation isUsingOSXYosemiteOrLater]) {
-		/* WKWebView uses sandboxing... The OS X El Capitan version of the
-		 WebKit framework includes a method to allow access to local files
-		 outside of this sandbox, but the OS X Yosemite version does not.
-		 This means we restrict WebKit2 to bundled styles on this verison. */
-
-		if ([themeController() storageLocation] == TPCThemeControllerStorageBundleLocation) {
-			return YES;
-		}
 	}
 
 	return NO;
