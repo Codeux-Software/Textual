1bcf03104b14c4b95e71e806ec15cd570c9400db
Michael Morris <michael@codeux.com>
2016-06-23 17:51:16 -0400

    HockeyApp framework 4.0.3

--- Frameworks/HockeySDK.framework/Versions/A/Headers/BITHockeyManager.h
+++ Frameworks/HockeySDK.framework/Versions/A/Headers/BITHockeyManager.h
@@ -1,29 +1,6 @@
-// 
-//  Author: Andreas Linde <mail@andreaslinde.de>
-// 
-//  Copyright (c) 2012-2014 HockeyApp, Bit Stadium GmbH. All rights reserved.
-//  See LICENSE.txt for author information.
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE SOFTWARE.
-
 @class BITCrashManager;
 @class BITFeedbackManager;
+@class BITMetricsManager;
 @protocol BITHockeyManagerDelegate;
 
 /**
@@ -203,6 +180,29 @@
  @see feedbackManager
  */
 @property (nonatomic, getter = isFeedbackManagerDisabled) BOOL disableFeedbackManager;
+
+
+/**
+ Reference to the initialized BITMetricsManager module
+ 
+ Returns the BITMetricsManager instance initialized by BITHockeyManager
+ */
+@property (nonatomic, strong, readonly) BITMetricsManager *metricsManager;
+
+/**
+ Flag the determines whether the BITMetricsManager should be disabled
+ 
+ If this flag is enabled, then sending metrics data such as sessions and users
+ will be turned off!
+ 
+ Please note that the BITMetricsManager instance will be initialized anyway!
+ 
+ @warning This property needs to be set before calling `startManager`
+ 
+ *Default*: _NO_
+ @see metricsManager
+ */
+@property (nonatomic, getter = isMetricsManagerDisabled) BOOL disableMetricsManager;
 
 
 ///-----------------------------------------------------------------------------
