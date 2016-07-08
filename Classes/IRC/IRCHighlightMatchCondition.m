8f4df810f96aadb3fe99e27b8a06b1ba55e4e90c
Michael Morris <michael@codeux.com>
2016-07-07 22:46:14 -0400

    This method does not need to be exposed for this class

--- Classes/IRC/IRCHighlightMatchCondition.m
+++ Classes/IRC/IRCHighlightMatchCondition.m
@@ -143,11 +143,6 @@ DESIGNATED_INITIALIZER_EXCEPTION_BODY_END
 @dynamic matchIsExcluded;
 @dynamic matchKeyword;
 
-- (void)populateDictionaryValues:(NSDictionary<NSString *, id> *)dic
-{
-	[super populateDictionaryValues:dic];
-}
-
 - (BOOL)isMutable
 {
 	return YES;
