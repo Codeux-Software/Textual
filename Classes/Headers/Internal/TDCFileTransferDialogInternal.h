a4732637e6dc63504a863cffe28afa4e4e5c0329
Michael Morris <michael@codeux.com>
2016-08-07 18:36:29 -0400

    Fix license header misalignment

--- Classes/Headers/Internal/TDCFileTransferDialogInternal.h
+++ Classes/Headers/Internal/TDCFileTransferDialogInternal.h
@@ -1,25 +1,25 @@
 /* *********************************************************************
- _____         _               _
- |_   _|____  _| |_ _   _  __ _| |
- | |/ _ \ \/ / __| | | |/ _` | |
- | |  __/>  <| |_| |_| | (_| | |
- |_|\___/_/\_\\__|\__,_|\__,_|_|
-
+                  _____         _               _
+                 |_   _|____  _| |_ _   _  __ _| |
+                   | |/ _ \ \/ / __| | | |/ _` | |
+                   | |  __/>  <| |_| |_| | (_| | |
+                   |_|\___/_/\_\\__|\__,_|\__,_|_|
+ 
  Copyright (c) 2010 - 2016 Codeux Software, LLC & respective contributors.
- Please see Acknowledgements.pdf for additional information.
+        Please see Acknowledgements.pdf for additional information.
 
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions
  are met:
 
- * Redistributions of source code must retain the above copyright
- notice, this list of conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright
- notice, this list of conditions and the following disclaimer in the
- documentation and/or other materials provided with the distribution.
- * Neither the name of Textual and/or "Codeux Software, LLC", nor the
- names of its contributors may be used to endorse or promote products
- derived from this software without specific prior written permission.
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of Textual and/or "Codeux Software, LLC", nor the 
+      names of its contributors may be used to endorse or promote products 
+      derived from this software without specific prior written permission.
 
  THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
