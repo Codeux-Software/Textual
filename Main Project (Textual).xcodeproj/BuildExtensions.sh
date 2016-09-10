4957c451c24bf231138418b4a2694459f7f3b536
Michael Morris <michael@codeux.com>
2016-09-10 07:23:07 -0400

    Fix issues with self-signed code signing certificates
    Code Signing Identity.xcconfig now include two new properties. Signing
    with a self-signed certificate requires that the properties
    DEVELOPMENT_TEAM and PROVISIONING_PROFILE_SPECIFIER be set to empty in
    this file.

--- Main Project (Textual).xcodeproj/BuildExtensions.sh
+++ Main Project (Textual).xcodeproj/BuildExtensions.sh
@@ -3,19 +3,49 @@
 set -e
 
 cd "${PROJECT_DIR}/Resources/Plugins/Brag Spam"
-xcodebuild -target "BragSpam" -configuration "Release" BUNDLE_LOADER="${CODESIGNING_FOLDER_PATH}/Contents/MacOS/${EXECUTABLE_NAME}" CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}"
+xcodebuild -target "BragSpam" \
+ -configuration "Release" \
+ BUNDLE_LOADER="${CODESIGNING_FOLDER_PATH}/Contents/MacOS/${EXECUTABLE_NAME}" \
+ CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}" \
+ DEVELOPMENT_TEAM="${DEVELOPMENT_TEAM}" \
+ PROVISIONING_PROFILE_SPECIFIER="${PROVISIONING_PROFILE_SPECIFIER}"
 
 cd "${PROJECT_DIR}/Resources/Plugins/Chat Filter"
-xcodebuild -target "ChatFilterExtension" -configuration "Release" BUNDLE_LOADER="${CODESIGNING_FOLDER_PATH}/Contents/MacOS/${EXECUTABLE_NAME}" CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}"
+xcodebuild -target "ChatFilterExtension" \
+ -configuration "Release" \
+ BUNDLE_LOADER="${CODESIGNING_FOLDER_PATH}/Contents/MacOS/${EXECUTABLE_NAME}" \
+ CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}" \
+ DEVELOPMENT_TEAM="${DEVELOPMENT_TEAM}" \
+ PROVISIONING_PROFILE_SPECIFIER="${PROVISIONING_PROFILE_SPECIFIER}"
 
 cd "${PROJECT_DIR}/Resources/Plugins/Smiley Converter"
-xcodebuild -target "SmileyConverter" -configuration "Release" BUNDLE_LOADER="${CODESIGNING_FOLDER_PATH}/Contents/MacOS/${EXECUTABLE_NAME}" CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}"
+xcodebuild -target "SmileyConverter" \
+ -configuration "Release" \
+ BUNDLE_LOADER="${CODESIGNING_FOLDER_PATH}/Contents/MacOS/${EXECUTABLE_NAME}" \
+ CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}" \
+ DEVELOPMENT_TEAM="${DEVELOPMENT_TEAM}" \
+ PROVISIONING_PROFILE_SPECIFIER="${PROVISIONING_PROFILE_SPECIFIER}"
 
 cd "${PROJECT_DIR}/Resources/Plugins/Spammer Paradise"
-xcodebuild -target "SpammerParadise" -configuration "Release" BUNDLE_LOADER="${CODESIGNING_FOLDER_PATH}/Contents/MacOS/${EXECUTABLE_NAME}" CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}"
+xcodebuild -target "SpammerParadise" \
+ -configuration "Release" \
+ BUNDLE_LOADER="${CODESIGNING_FOLDER_PATH}/Contents/MacOS/${EXECUTABLE_NAME}" \
+ CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}" \
+ DEVELOPMENT_TEAM="${DEVELOPMENT_TEAM}" \
+ PROVISIONING_PROFILE_SPECIFIER="${PROVISIONING_PROFILE_SPECIFIER}"
 
 cd "${PROJECT_DIR}/Resources/Plugins/System Profiler"
-xcodebuild -target "SystemProfiler" -configuration "Release" BUNDLE_LOADER="${CODESIGNING_FOLDER_PATH}/Contents/MacOS/${EXECUTABLE_NAME}" CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}"
+xcodebuild -target "SystemProfiler" \
+ -configuration "Release" \
+ BUNDLE_LOADER="${CODESIGNING_FOLDER_PATH}/Contents/MacOS/${EXECUTABLE_NAME}" \
+ CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}" \
+ DEVELOPMENT_TEAM="${DEVELOPMENT_TEAM}" \
+ PROVISIONING_PROFILE_SPECIFIER="${PROVISIONING_PROFILE_SPECIFIER}"
 
 cd "${PROJECT_DIR}/Resources/Plugins/ZNC Additions"
-xcodebuild -target "ZNCAdditions" -configuration "Release" BUNDLE_LOADER="${CODESIGNING_FOLDER_PATH}/Contents/MacOS/${EXECUTABLE_NAME}" CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}"
+xcodebuild -target "ZNCAdditions" \
+ -configuration "Release" \
+ BUNDLE_LOADER="${CODESIGNING_FOLDER_PATH}/Contents/MacOS/${EXECUTABLE_NAME}" \
+ CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}" \
+ DEVELOPMENT_TEAM="${DEVELOPMENT_TEAM}" \
+ PROVISIONING_PROFILE_SPECIFIER="${PROVISIONING_PROFILE_SPECIFIER}"
--- Main Project (Textual).xcodeproj/BuildFrameworks.sh
+++ Main Project (Textual).xcodeproj/BuildFrameworks.sh
@@ -8,20 +8,41 @@ rm -rf "${BUILD_DESTINATION_FOLDER}"
 
 mkdir -p "${BUILD_DESTINATION_FOLDER}"
 
+# Auto Hyperlinks
 cd "${PROJECT_DIR}/Frameworks/Auto Hyperlinks/"
-xcodebuild -target "AutoHyperlinks.framework" -configuration "Release" CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}"
+
+xcodebuild -target "AutoHyperlinks.framework" \
+ -configuration "Release" \
+ CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}" \
+ DEVELOPMENT_TEAM="${DEVELOPMENT_TEAM}" \
+ PROVISIONING_PROFILE_SPECIFIER="${PROVISIONING_PROFILE_SPECIFIER}"
+
 mv "${PROJECT_DIR}/Frameworks/Auto Hyperlinks/Build Results/Release/AutoHyperlinks.framework" "${BUILD_DESTINATION_FOLDER}"
 rm -rf "${PROJECT_DIR}/Frameworks/Auto Hyperlinks/Build Results/"
 rm -rf "${PROJECT_DIR}/Frameworks/Auto Hyperlinks/.tmp/"
 
+# Encryption Kit
 cd "${PROJECT_DIR}/Frameworks/Encryption Kit/"
-xcodebuild -target "EncryptionKit" -configuration "Release" CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}"
+
+xcodebuild -target "EncryptionKit" \
+ -configuration "Release" \
+ CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}" \
+ DEVELOPMENT_TEAM="${DEVELOPMENT_TEAM}" \
+ PROVISIONING_PROFILE_SPECIFIER="${PROVISIONING_PROFILE_SPECIFIER}"
+
 mv "${PROJECT_DIR}/Frameworks/Encryption Kit/Build Results/Release/EncryptionKit.framework" "${BUILD_DESTINATION_FOLDER}"
 rm -rf "${PROJECT_DIR}/Frameworks/Encryption Kit/Build Results/"
 rm -rf "${PROJECT_DIR}/Frameworks/Encryption Kit/.tmp/"
 
+# Cocoa Extensions
 cd "${PROJECT_DIR}/Frameworks/Cocoa Extensions/"
-xcodebuild -target "CocoaExtensions (OS X)" -configuration "Release" CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}"
+
+xcodebuild -target "CocoaExtensions (OS X)" \
+ -configuration "Release" \
+ CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}" \
+ DEVELOPMENT_TEAM="${DEVELOPMENT_TEAM}" \
+ PROVISIONING_PROFILE_SPECIFIER="${PROVISIONING_PROFILE_SPECIFIER}"
+
 mv "${PROJECT_DIR}/Frameworks/Cocoa Extensions/Build Results/Release/CocoaExtensions.framework" "${BUILD_DESTINATION_FOLDER}"
 rm -rf "${PROJECT_DIR}/Frameworks/Cocoa Extensions/Build Results/"
 rm -rf "${PROJECT_DIR}/Frameworks/Cocoa Extensions/.tmp/"
