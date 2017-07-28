Codeux-Software/Textual

commit 28032dde055a53715a599045af1f31272b688748

Plugins update
Extra plugins no longer build with a code signature. They are signed at the point they are inserted into the installer.

Blowfish Encryption plugin received a version bump because it was upgraded to version 2.5.5 of libressl.

Resources/Plugins/Caffeine/Classes/TPI_Caffeine.m
@@ -178,7 +178,7 @@ NS_ASSUME_NONNULL_BEGIN
 	/* The Textual Extras installer can detect operating system and will not
 	 allow it to be installed on Mountain Lion, but still good to have some
 	 type of sanity type. */
-	if ([XRSystemInformation isUsingOSXMavericksOrLater] == NO) {
+	if (TEXTUAL_RUNNING_ON(10.9, Mavericks)) {
 		return;
 	}
 

Resources/Plugins/Blowfish Encryption/Blowfish Encryption Extension.xcodeproj/project.pbxproj
@@ -226,7 +226,12 @@
 		089C1669FE841209C02AAC07 /* Project object */ = {
 			isa = PBXProject;
 			attributes = {
-				LastUpgradeCheck = 0800;
+				LastUpgradeCheck = 0900;
+				TargetAttributes = {
+					8D57630D048677EA00EA77CD = {
+						ProvisioningStyle = Manual;
+					};
+				};
 			};
 			buildConfigurationList = 1DEB911E08733D790010E9CD /* Build configuration list for PBXProject "Blowfish Encryption Extension" */;
 			compatibilityVersion = "Xcode 3.2";
@@ -283,24 +288,40 @@
 		1DEB911B08733D790010E9CD /* Debug */ = {
 			isa = XCBuildConfiguration;
 			buildSettings = {
+				CODE_SIGN_IDENTITY = "Mac Developer";
+				CODE_SIGN_STYLE = Manual;
 				COMBINE_HIDPI_IMAGES = YES;
+				DEVELOPMENT_TEAM = "";
 				HEADER_SEARCH_PATHS = (
-					"\"${TEXTUAL_PROJECT_ROOT}/Classes/Headers/**\"",
+					"${inherited}",
 					"\"/private/tmp/com.codeux.frameworks.encryptionKit/Library-Build-Results/include\"/**",
 				);
+				LIBRARY_SEARCH_PATHS = (
+					"${inherited}/**",
+					"/private/tmp/com.codeux.frameworks.encryptionKit/Library-Build-Results/lib",
+				);
 				PRODUCT_BUNDLE_IDENTIFIER = "com.codeux.app-extensions.textual-BlowfishEncryption";
+				PROVISIONING_PROFILE_SPECIFIER = "";
 			};
 			name = Debug;
 		};
 		1DEB911C08733D790010E9CD /* Release */ = {
 			isa = XCBuildConfiguration;
 			buildSettings = {
+				CODE_SIGN_IDENTITY = "Mac Developer";
+				CODE_SIGN_STYLE = Manual;
 				COMBINE_HIDPI_IMAGES = YES;
+				DEVELOPMENT_TEAM = "";
 				HEADER_SEARCH_PATHS = (
-					"\"${TEXTUAL_PROJECT_ROOT}/Classes/Headers/**\"",
+					"${inherited}",
 					"\"/private/tmp/com.codeux.frameworks.encryptionKit/Library-Build-Results/include\"/**",
 				);
+				LIBRARY_SEARCH_PATHS = (
+					"${inherited}/**",
+					"/private/tmp/com.codeux.frameworks.encryptionKit/Library-Build-Results/lib",
+				);
 				PRODUCT_BUNDLE_IDENTIFIER = "com.codeux.app-extensions.textual-BlowfishEncryption";
+				PROVISIONING_PROFILE_SPECIFIER = "";
 			};
 			name = Release;
 		};
@@ -308,7 +329,6 @@
 			isa = XCBuildConfiguration;
 			baseConfigurationReference = 4C24DC8A1A93CD350098D1BE /* Shared Configuration for Extensions.xcconfig */;
 			buildSettings = {
-				CODE_SIGN_IDENTITY = "Developer ID Application";
 				INFOPLIST_FILE = Resources/Info.plist;
 				PRODUCT_NAME = BlowfishEncryption;
 			};
@@ -318,7 +338,6 @@
 			isa = XCBuildConfiguration;
 			baseConfigurationReference = 4C24DC8A1A93CD350098D1BE /* Shared Configuration for Extensions.xcconfig */;
 			buildSettings = {
-				CODE_SIGN_IDENTITY = "Developer ID Application";
 				INFOPLIST_FILE = Resources/Info.plist;
 				PRODUCT_NAME = BlowfishEncryption;
 			};

Resources/Plugins/Blowfish Encryption/Blowfish Encryption Extension.xcodeproj/xcshareddata/xcschemes/BlowfishEncryption.xcscheme
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <Scheme
-   LastUpgradeVersion = "0800"
+   LastUpgradeVersion = "0900"
    version = "1.3">
    <BuildAction
       parallelizeBuildables = "YES"

Resources/Plugins/Blowfish Encryption/Resources/Info.plist
@@ -11,7 +11,7 @@
 	<key>CFBundlePackageType</key>
 	<string>BNDL</string>
 	<key>CFBundleVersion</key>
-	<string>1.0.8</string>
+	<string>1.0.9</string>
 	<key>MinimumTextualVersion</key>
 	<string>6.0.0</string>
 	<key>NSPrincipalClass</key>

Resources/Plugins/Caffeine/Caffeine Extension.xcodeproj/project.pbxproj
@@ -151,7 +151,12 @@
 		089C1669FE841209C02AAC07 /* Project object */ = {
 			isa = PBXProject;
 			attributes = {
-				LastUpgradeCheck = 0800;
+				LastUpgradeCheck = 0900;
+				TargetAttributes = {
+					8D57630D048677EA00EA77CD = {
+						ProvisioningStyle = Manual;
+					};
+				};
 			};
 			buildConfigurationList = 1DEB911E08733D790010E9CD /* Build configuration list for PBXProject "Caffeine Extension" */;
 			compatibilityVersion = "Xcode 3.2";
@@ -199,16 +204,22 @@
 		1DEB911B08733D790010E9CD /* Debug */ = {
 			isa = XCBuildConfiguration;
 			buildSettings = {
+				CODE_SIGN_STYLE = Manual;
 				COMBINE_HIDPI_IMAGES = YES;
+				DEVELOPMENT_TEAM = "";
 				PRODUCT_BUNDLE_IDENTIFIER = "com.codeux.app-extensions.textual-Caffeine";
+				PROVISIONING_PROFILE_SPECIFIER = "";
 			};
 			name = Debug;
 		};
 		1DEB911C08733D790010E9CD /* Release */ = {
 			isa = XCBuildConfiguration;
 			buildSettings = {
+				CODE_SIGN_STYLE = Manual;
 				COMBINE_HIDPI_IMAGES = YES;
+				DEVELOPMENT_TEAM = "";
 				PRODUCT_BUNDLE_IDENTIFIER = "com.codeux.app-extensions.textual-Caffeine";
+				PROVISIONING_PROFILE_SPECIFIER = "";
 			};
 			name = Release;
 		};
@@ -216,7 +227,6 @@
 			isa = XCBuildConfiguration;
 			baseConfigurationReference = 4C24DC8C1A93CD540098D1BE /* Shared Configuration for Extensions.xcconfig */;
 			buildSettings = {
-				CODE_SIGN_IDENTITY = "Developer ID Application";
 				INFOPLIST_FILE = Resources/Info.plist;
 				PRODUCT_NAME = Caffeine;
 			};
@@ -226,7 +236,6 @@
 			isa = XCBuildConfiguration;
 			baseConfigurationReference = 4C24DC8C1A93CD540098D1BE /* Shared Configuration for Extensions.xcconfig */;
 			buildSettings = {
-				CODE_SIGN_IDENTITY = "Developer ID Application";
 				INFOPLIST_FILE = Resources/Info.plist;
 				PRODUCT_NAME = Caffeine;
 			};

Resources/Plugins/Caffeine/Caffeine Extension.xcodeproj/xcshareddata/xcschemes/Caffeine.xcscheme
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <Scheme
-   LastUpgradeVersion = "0800"
+   LastUpgradeVersion = "0900"
    version = "1.3">
    <BuildAction
       parallelizeBuildables = "YES"

Resources/Plugins/Wiki Link Parser/Wiki-style Link Parser Extension.xcodeproj/project.pbxproj
@@ -151,7 +151,12 @@
 		089C1669FE841209C02AAC07 /* Project object */ = {
 			isa = PBXProject;
 			attributes = {
-				LastUpgradeCheck = 0800;
+				LastUpgradeCheck = 0900;
+				TargetAttributes = {
+					8D57630D048677EA00EA77CD = {
+						ProvisioningStyle = Manual;
+					};
+				};
 			};
 			buildConfigurationList = 1DEB911E08733D790010E9CD /* Build configuration list for PBXProject "Wiki-style Link Parser Extension" */;
 			compatibilityVersion = "Xcode 3.2";
@@ -210,16 +215,22 @@
 		1DEB911B08733D790010E9CD /* Debug */ = {
 			isa = XCBuildConfiguration;
 			buildSettings = {
+				CODE_SIGN_STYLE = Manual;
 				COMBINE_HIDPI_IMAGES = YES;
+				DEVELOPMENT_TEAM = "";
 				PRODUCT_BUNDLE_IDENTIFIER = "com.codeux.app-extensions.textual-WikiStyleLinkParser";
+				PROVISIONING_PROFILE_SPECIFIER = "";
 			};
 			name = Debug;
 		};
 		1DEB911C08733D790010E9CD /* Release */ = {
 			isa = XCBuildConfiguration;
 			buildSettings = {
+				CODE_SIGN_STYLE = Manual;
 				COMBINE_HIDPI_IMAGES = YES;
+				DEVELOPMENT_TEAM = "";
 				PRODUCT_BUNDLE_IDENTIFIER = "com.codeux.app-extensions.textual-WikiStyleLinkParser";
+				PROVISIONING_PROFILE_SPECIFIER = "";
 			};
 			name = Release;
 		};
@@ -227,7 +238,6 @@
 			isa = XCBuildConfiguration;
 			baseConfigurationReference = 4C1187671A950809001E8654 /* Shared Configuration for Extensions.xcconfig */;
 			buildSettings = {
-				CODE_SIGN_IDENTITY = "Developer ID Application";
 				INFOPLIST_FILE = Resources/Info.plist;
 				PRODUCT_NAME = WikiStyleLinkParser;
 			};
@@ -237,7 +247,6 @@
 			isa = XCBuildConfiguration;
 			baseConfigurationReference = 4C1187671A950809001E8654 /* Shared Configuration for Extensions.xcconfig */;
 			buildSettings = {
-				CODE_SIGN_IDENTITY = "Developer ID Application";
 				INFOPLIST_FILE = Resources/Info.plist;
 				PRODUCT_NAME = WikiStyleLinkParser;
 			};

Resources/Plugins/Wiki Link Parser/Wiki-style Link Parser Extension.xcodeproj/xcshareddata/xcschemes/WikiStyleLinkParser.xcscheme
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <Scheme
-   LastUpgradeVersion = "0800"
+   LastUpgradeVersion = "0900"
    version = "1.3">
    <BuildAction
       parallelizeBuildables = "YES"

commit 7dc437b4e564c680d0f86d2823479454080b98b5

Fix incorrect condition introduced in recent commit
Introduced in 28032dde055a53715a599045af1f31272b688748

I am tired

Resources/Plugins/Caffeine/Classes/TPI_Caffeine.m
@@ -178,7 +178,7 @@ NS_ASSUME_NONNULL_BEGIN
 	/* The Textual Extras installer can detect operating system and will not
 	 allow it to be installed on Mountain Lion, but still good to have some
 	 type of sanity type. */
-	if (TEXTUAL_RUNNING_ON(10.9, Mavericks)) {
+	if (TEXTUAL_RUNNING_ON(10.9, Mavericks) == NO) {
 		return;
 	}
 