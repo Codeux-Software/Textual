Codeux-Software/Textual

commit c4eb3f6f5c25cdffe5c7ed022eb9bb379001237c

Update menu controller
• Updated all tags for menu items in Help menu for greater uniformity
• Updated menu validation to disable all menu items until masterController().applicationIsLaunched is YES.
• Added action to menu controller for accessing in-app purchase window.

Classes/Controllers/TXMenuController.m
@@ -172,6 +172,10 @@ NS_ASSUME_NONNULL_BEGIN
 		return NO;
 	}
 
+#if TEXTUAL_BUILT_FOR_APP_STORE_DISTRIBUTION == 1
+	BOOL appFinishedLaunching = masterController().applicationIsLaunched;
+#endif
+
 	BOOL isMainWindowMain = mainWindow().mainWindow;
 
 	BOOL mainWindowHasSheet = (mainWindow().attachedSheet != nil);
@@ -252,21 +256,43 @@ NS_ASSUME_NONNULL_BEGIN
 				}
 			}
 
-			/* If trial is expired, default everything to disabled. */
+			/* If trial is expired, or app has not finished launching,
+			 then default everything to disabled. */
 			BOOL isTrialExpired = NO;
 
+#if TEXTUAL_BUILT_WITH_LICENSE_MANAGER == 1 || TEXTUAL_BUILT_FOR_APP_STORE_DISTRIBUTION == 1
+			if (
+
 #if TEXTUAL_BUILT_WITH_LICENSE_MANAGER == 1
-			if (TLOLicenseManagerTextualIsRegistered() == NO && TLOLicenseManagerIsTrialExpired()) {
-				/* Disable everything by default except tag 900 through 916. These are various
-				 help menu links. See TXMenuController.h for complete list of tags. */
-				if (tag < 900 || (tag >= 917 && tag <= 930)) {
+				(TLOLicenseManagerTextualIsRegistered() == NO && TLOLicenseManagerIsTrialExpired())
+#elif TEXTUAL_BUILT_FOR_APP_STORE_DISTRIBUTION == 1
+				appFinishedLaunching == NO
+#endif
+
+				)
+			{
+				/* Disable everything by default except for items that are considred
+				 helpful. See TXMenuController.h for complete list of tags. */
+				if (tag < 900 ||
+					(tag >= 900 && tag <= 902) ||
+					(tag >= 930 && tag <= 944))
+				{
 					returnValue = NO;
 				}
 
 				/* Enable specific items after it has been disabled. */
 				/* Other always-required items are enabled further 
 				 below this switch statement. */
-				if (tag == 102) { // "Manage license…"
+				if (
+
+#if TEXTUAL_BUILT_WITH_LICENSE_MANAGER == 1
+					tag == 102 // "Manage license…"
+#elif TEXTUAL_BUILT_FOR_APP_STORE_DISTRIBUTION == 1
+					tag == 110 // "In-app Purchase…"
+#endif
+
+					)
+				{
 					menuItem.hidden = NO;
 
 					returnValue = YES;
@@ -283,6 +309,7 @@ NS_ASSUME_NONNULL_BEGIN
 					case 100: // "About Textual"
 					case 101: // "Preferences…"
 					case 102: // "Manage license…"
+					case 110: // "In-app Purchase…"
 					case 109: // "Check for updates…"
 					{
 						returnValue = YES;
@@ -318,7 +345,7 @@ NS_ASSUME_NONNULL_BEGIN
 					case 801: // "Zoom"
 					case 808: // "Main Window"
 					case 814: // "Bring All to Front"
-					case 924: // "Export Preferences"
+					case 963: // "Export Preferences"
 					{
 						returnValue = YES;
 					} // case
@@ -390,7 +417,15 @@ NS_ASSUME_NONNULL_BEGIN
 
 			return YES;
 		}
-		case 922: // "Reset 'Don't Ask Me' Warnings"
+		case 110: // "In-app Purchase…"
+		{
+#if TEXTUAL_BUILT_FOR_APP_STORE_DISTRIBUTION == 1
+			menuItem.hidden = YES;
+#endif
+
+			return YES;
+		}
+		case 965: // "Reset 'Don't Ask Me' Warnings"
 		{
 			BOOL condition = [TPCPreferences developerModeEnabled];
 
@@ -729,7 +764,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 			return [TPCPreferences logHighlights];
 		}
-        case 920: // Developer Mode
+        case 961: // Developer Mode
         {
 			if ([TPCPreferences developerModeEnabled]) {
                 menuItem.state = NSOnState;
@@ -3161,21 +3196,21 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)openHelpMenuItem:(id)sender
 {
 	NSDictionary *_helpMenuLinks = @{
-	   @(901) : @"https://help.codeux.com/textual/Privacy-Policy.kb",
+	   @(901) : @"https://help.codeux.com/textual/End-User-License-Agreement.kb",
+	   @(902) : @"https://help.codeux.com/textual/Privacy-Policy.kb",
 	   @(902) : @"https://help.codeux.com/textual/Frequently-Asked-Questions.kb",
-	   @(905) : @"https://help.codeux.com/textual/home.kb",
-	   @(906) : @"https://help.codeux.com/textual/iCloud-Syncing.kb",
-	   @(907) : @"https://help.codeux.com/textual/Off-the-Record-Messaging.kb",
-	   @(908) : @"https://help.codeux.com/textual/Command-Reference.kb",
-	   @(909) : @"https://help.codeux.com/textual/Support.kb",
-	   @(910) : @"https://help.codeux.com/textual/Keyboard-Shortcuts.kb",
-	   @(911) : @"https://help.codeux.com/textual/Memory-Management.kb",
-	   @(912) : @"https://help.codeux.com/textual/Text-Formatting.kb",
-	   @(913) : @"https://help.codeux.com/textual/Styles.kb",
-	   @(914) : @"https://help.codeux.com/textual/Using-CertFP.kb",
-	   @(915) : @"https://help.codeux.com/textual/Connecting-to-ZNC-Bouncer.kb",
-	   @(916) : @"https://help.codeux.com/textual/DCC-File-Transfer-Information.kb",
-	   @(927) : @"https://help.codeux.com/textual/End-User-License-Agreement.kb"
+	   @(933) : @"https://help.codeux.com/textual/home.kb",
+	   @(934) : @"https://help.codeux.com/textual/iCloud-Syncing.kb",
+	   @(935) : @"https://help.codeux.com/textual/Off-the-Record-Messaging.kb",
+	   @(936) : @"https://help.codeux.com/textual/Command-Reference.kb",
+	   @(937) : @"https://help.codeux.com/textual/Support.kb",
+	   @(938) : @"https://help.codeux.com/textual/Keyboard-Shortcuts.kb",
+	   @(939) : @"https://help.codeux.com/textual/Memory-Management.kb",
+	   @(940) : @"https://help.codeux.com/textual/Text-Formatting.kb",
+	   @(941) : @"https://help.codeux.com/textual/Styles.kb",
+	   @(942) : @"https://help.codeux.com/textual/Using-CertFP.kb",
+	   @(943) : @"https://help.codeux.com/textual/Connecting-to-ZNC-Bouncer.kb",
+	   @(944) : @"https://help.codeux.com/textual/DCC-File-Transfer-Information.kb"
 	};
 	
 	NSString *link = _helpMenuLinks[@([sender tag])];
@@ -3554,6 +3589,16 @@ NS_ASSUME_NONNULL_BEGIN
 	}
 }
 #endif
+
+#pragma mark -
+#pragma mark In-app Purchase
+
+- (void)manageInAppPurchase:(id)sender
+{
+#if TEXTUAL_BUILT_FOR_APP_STORE_DISTRIBUTION == 1
+	[[TXSharedApplication sharedInAppPurchaseDialog] show];
+#endif
+}
 
 #pragma mark -
 #pragma mark Developer Tools

Classes/Headers/TXMenuController.h
@@ -58,6 +58,7 @@ NS_ASSUME_NONNULL_BEGIN
 	100: "About Textual"
 	101: "Preferences…"
 	102: "Manage license…"
+    110: "In-app Purchase…"
 	103: "Updates"
 	109: "Check for updates…"
 	104: "Services"
@@ -172,32 +173,31 @@ NS_ASSUME_NONNULL_BEGIN
 	814: "Bring All to Front"
 
 	900: "Acknowledgements"
-	901: "Privacy Policy"
-	902: "Frequently Asked Questions"
-	903: "Introduction to Writing Scripts"
-	904: "Other Helpful Resources"
-	905: "Knowledge Base Home"
-	906: "Using iCloud with Textual"
-	907: "Chat Encryption"
-	908: "Command Reference"
-	909: "Feature Requests"
-	910: "Keyboard Shortcuts"
-	911: "Memory Management"
-	912: "Text Formatting"
-	913: "Styling Information"
-	914: "Connecting with Certificate"
-	915: "Connecting to a ZNC Bouncer"
-	916: "DCC File Transfer Information"
-	917: "Connect to Help Channel"
-	918: "Connect to Testing Channel"
-	919: "Developer Resources"
-	920: "Enable Developer Mode"
-	922: "Reset 'Don't Ask Me' Warnings"
-	923: "Simulate a Crash"
-	924: "Export Preferences"
-	925: "Import Preferences"
-	927: "End User License Agreement"
-	930: "Hidden Preferences"
+    901: "End User License Agreement"
+	902: "Privacy Policy"
+    903: "Connect to Help Channel"
+    904: "Connect to Testing Channel"
+	930: "Frequently Asked Questions"
+	931: "Introduction to Writing Scripts"
+	932: "Other Helpful Resources"
+	933: "Knowledge Base Home"
+	934: "Using iCloud with Textual"
+	935: "Chat Encryption"
+	936: "Command Reference"
+	937: "Feature Requests"
+	938: "Keyboard Shortcuts"
+	939: "Memory Management"
+	940: "Text Formatting"
+	941: "Styling Information"
+	942: "Connecting with Certificate"
+	943: "Connecting to a ZNC Bouncer"
+	944: "DCC File Transfer Information"
+	960: "Advanced"
+	961: "Enable Developer Mode"
+    962: "Hidden Preferences"
+	963: "Export Preferences"
+	964: "Import Preferences"
+    965: "Reset 'Don't Ask Me' Warnings"
  
 	The following tags apply to the "Join Channel" menu:
 	1000: "Join Channel"
@@ -436,6 +436,7 @@ NS_ASSUME_NONNULL_BEGIN
 - (IBAction)toggleMuteOnNotificationSounds:(id)sender;
 
 - (IBAction)manageLicense:(id)sender;
+- (IBAction)manageInAppPurchase:(id)sender;
 
 - (IBAction)simulateCrash:(id)sender;
 

Resources/User Interface/English.lproj/TXCMainMenu.xib
@@ -36,6 +36,12 @@
                                     <action selector="manageLicense:" target="267" id="9xt-Qu-8UP"/>
                                 </connections>
                             </menuItem>
+                            <menuItem title="In-app Purchase…" tag="110" id="CES-gF-e9x">
+                                <modifierMask key="keyEquivalentModifierMask"/>
+                                <connections>
+                                    <action selector="manageInAppPurchase:" target="267" id="c2m-fH-Kci"/>
+                                </connections>
+                            </menuItem>
                             <menuItem title="Check for updates…" tag="109" id="SKm-yR-9hI">
                                 <modifierMask key="keyEquivalentModifierMask"/>
                                 <connections>
@@ -858,107 +864,107 @@ CQ
                                     <action selector="showAcknowledgements:" target="267" id="2556"/>
                                 </connections>
                             </menuItem>
-                            <menuItem title="License Agreement" tag="927" id="BrC-PS-U3X">
+                            <menuItem title="License Agreement" tag="901" id="BrC-PS-U3X">
                                 <modifierMask key="keyEquivalentModifierMask"/>
                                 <connections>
                                     <action selector="openHelpMenuItem:" target="267" id="ell-Ij-llA"/>
                                 </connections>
                             </menuItem>
-                            <menuItem title="Privacy Policy" tag="901" id="1710">
+                            <menuItem title="Privacy Policy" tag="902" id="1710">
                                 <modifierMask key="keyEquivalentModifierMask"/>
                                 <connections>
                                     <action selector="openHelpMenuItem:" target="267" id="7cF-3R-5pW"/>
                                 </connections>
                             </menuItem>
                             <menuItem isSeparatorItem="YES" id="1661"/>
-                            <menuItem title="Frequently Asked Questions" tag="902" id="gkX-Of-Y5Q">
+                            <menuItem title="Frequently Asked Questions" tag="930" id="gkX-Of-Y5Q">
                                 <modifierMask key="keyEquivalentModifierMask"/>
                                 <connections>
                                     <action selector="openHelpMenuItem:" target="267" id="G7o-2j-dES"/>
                                 </connections>
                             </menuItem>
-                            <menuItem title="Introduction to Writing Scripts" tag="903" id="3130">
+                            <menuItem title="Introduction to Writing Scripts" tag="931" id="3130">
                                 <modifierMask key="keyEquivalentModifierMask"/>
                                 <connections>
                                     <action selector="showScriptingDocumentation:" target="267" id="YZ0-Ui-lxb"/>
                                 </connections>
                             </menuItem>
                             <menuItem isSeparatorItem="YES" id="fIj-9Q-u6p"/>
-                            <menuItem title="Other Helpful Resources" tag="904" id="1686">
+                            <menuItem title="Other Helpful Resources" tag="932" id="1686">
                                 <modifierMask key="keyEquivalentModifierMask"/>
                                 <menu key="submenu" title="Other Helpful Resources" id="1687">
                                     <items>
-                                        <menuItem title="Knowledge Base Home" tag="905" id="01c-Ny-YrB">
+                                        <menuItem title="Knowledge Base Home" tag="933" id="01c-Ny-YrB">
                                             <modifierMask key="keyEquivalentModifierMask"/>
                                             <connections>
                                                 <action selector="openHelpMenuItem:" target="267" id="ujf-rz-QKO"/>
                                             </connections>
                                         </menuItem>
                                         <menuItem isSeparatorItem="YES" id="VTu-dA-PTE"/>
-                                        <menuItem title="Using iCloud with Textual" tag="906" id="aO8-3y-WHh">
+                                        <menuItem title="Using iCloud with Textual" tag="934" id="aO8-3y-WHh">
                                             <modifierMask key="keyEquivalentModifierMask"/>
                                             <connections>
                                                 <action selector="openHelpMenuItem:" target="267" id="woi-33-SkS"/>
                                             </connections>
                                         </menuItem>
                                         <menuItem isSeparatorItem="YES" id="Lpo-5n-kp3"/>
-                                        <menuItem title="Chat Encryption" tag="907" id="w1H-u0-OYW">
+                                        <menuItem title="Chat Encryption" tag="935" id="w1H-u0-OYW">
                                             <modifierMask key="keyEquivalentModifierMask"/>
                                             <connections>
                                                 <action selector="openHelpMenuItem:" target="267" id="Ttg-Hf-X1o"/>
                                             </connections>
                                         </menuItem>
-                                        <menuItem title="Command Reference" tag="908" id="1667">
+                                        <menuItem title="Command Reference" tag="936" id="1667">
                                             <modifierMask key="keyEquivalentModifierMask"/>
                                             <connections>
                                                 <action selector="openHelpMenuItem:" target="267" id="fkh-AP-nEH"/>
                                             </connections>
                                         </menuItem>
-                                        <menuItem title="Feature Requests" tag="909" id="1689">
+                                        <menuItem title="Feature Requests" tag="937" id="1689">
                                             <modifierMask key="keyEquivalentModifierMask"/>
                                             <connections>
                                                 <action selector="openHelpMenuItem:" target="267" id="kbI-Gd-aBG"/>
                                             </connections>
                                         </menuItem>
-                                        <menuItem title="Keyboard Shortcuts" tag="910" id="3080">
+                                        <menuItem title="Keyboard Shortcuts" tag="938" id="3080">
                                             <modifierMask key="keyEquivalentModifierMask"/>
                                             <connections>
                                                 <action selector="openHelpMenuItem:" target="267" id="EcI-bN-kg9"/>
                                             </connections>
                                         </menuItem>
-                                        <menuItem title="Memory Management" tag="911" id="1691">
+                                        <menuItem title="Memory Management" tag="939" id="1691">
                                             <modifierMask key="keyEquivalentModifierMask"/>
                                             <connections>
                                                 <action selector="openHelpMenuItem:" target="267" id="yKU-Kd-2sX"/>
                                             </connections>
                                         </menuItem>
-                                        <menuItem title="Text Formatting" tag="912" id="1693">
+                                        <menuItem title="Text Formatting" tag="940" id="1693">
                                             <modifierMask key="keyEquivalentModifierMask"/>
                                             <connections>
                                                 <action selector="openHelpMenuItem:" target="267" id="VqO-jK-WsS"/>
                                             </connections>
                                         </menuItem>
-                                        <menuItem title="Styling Information" tag="913" id="1695">
+                                        <menuItem title="Styling Information" tag="941" id="1695">
                                             <modifierMask key="keyEquivalentModifierMask"/>
                                             <connections>
                                                 <action selector="openHelpMenuItem:" target="267" id="F9h-mt-chQ"/>
                                             </connections>
                                         </menuItem>
                                         <menuItem isSeparatorItem="YES" id="3079"/>
-                                        <menuItem title="Connecting with a Certificate" tag="914" id="o9J-hu-dKA">
+                                        <menuItem title="Connecting with a Certificate" tag="942" id="o9J-hu-dKA">
                                             <modifierMask key="keyEquivalentModifierMask"/>
                                             <connections>
                                                 <action selector="openHelpMenuItem:" target="267" id="6Ax-wH-9h7"/>
                                             </connections>
                                         </menuItem>
-                                        <menuItem title="Connecting to a ZNC Bouncer" tag="915" id="VQN-Rg-JZ0">
+                                        <menuItem title="Connecting to a ZNC Bouncer" tag="943" id="VQN-Rg-JZ0">
                                             <modifierMask key="keyEquivalentModifierMask"/>
                                             <connections>
                                                 <action selector="openHelpMenuItem:" target="267" id="QOa-Cc-r8n"/>
                                             </connections>
                                         </menuItem>
                                         <menuItem isSeparatorItem="YES" id="e0i-mT-GT6"/>
-                                        <menuItem title="DCC File Transfer Information" tag="916" id="3064">
+                                        <menuItem title="DCC File Transfer Information" tag="944" id="3064">
                                             <modifierMask key="keyEquivalentModifierMask"/>
                                             <connections>
                                                 <action selector="openHelpMenuItem:" target="267" id="qfF-bN-57R"/>
@@ -968,13 +974,13 @@ CQ
                                 </menu>
                             </menuItem>
                             <menuItem isSeparatorItem="YES" id="Ws1-vl-bOl"/>
-                            <menuItem title="Connect to Help Channel" tag="917" id="1392">
+                            <menuItem title="Connect to Help Channel" tag="903" id="1392">
                                 <modifierMask key="keyEquivalentModifierMask"/>
                                 <connections>
                                     <action selector="connectToTextualHelpChannel:" target="267" id="2568"/>
                                 </connections>
                             </menuItem>
-                            <menuItem title="Connect to Testing Channel" tag="918" id="2706">
+                            <menuItem title="Connect to Testing Channel" tag="904" id="2706">
                                 <modifierMask key="keyEquivalentModifierMask"/>
                                 <connections>
                                     <action selector="connectToTextualTestingChannel:" target="267" id="2708"/>

commit 548c5b54a1a3fbe08639d2af9e60f9e1d2e168b7

Fix incorrect menu validation logic introduced in c4eb3f6f5c25cdffe5c7ed022eb9bb379001237c

Classes/Controllers/TXMenuController.m
@@ -275,7 +275,7 @@ NS_ASSUME_NONNULL_BEGIN
 				 helpful. See TXMenuController.h for complete list of tags. */
 				if (tag < 900 ||
 					(tag >= 900 && tag <= 902) ||
-					(tag >= 930 && tag <= 944))
+					(tag >= 960 && tag <= 965))
 				{
 					returnValue = NO;
 				}