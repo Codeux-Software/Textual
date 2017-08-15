14f03e7a98326c02ca8269af14ff4c64913bf5f3
Michael Morris <michael@codeux.com>
2017-08-15 00:29:50 -0400

    Fix line in license that was somehow corrupted

--- Classes/Headers/Private/TLOLicenseManagerDownloaderPrivate.h
+++ Classes/Headers/Private/TLOLicenseManagerDownloaderPrivate.h
@@ -18,7 +18,7 @@
       notice, this list of conditions and the following disclaimer in the
       documentation and/or other materials provided with the distribution.
     * Neither the name of Textual and/or "Codeux Software, LLC", nor the 
-      names of its contributors may be usedr promote products
+      names of its contributors may be used to endorse or promote products
       derived from this software without specific prior written permission.
 
  THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
