ee881780c3eb3c1319bdbf78a967942ca6aed499
Michael Morris <michael@codeux.com>
2017-08-28 00:35:53 -0400

    Extend TVCLogControllerHistoricLogFile
    • Added -fetchEntriesForItem:beforeUniqueIdentifier:fetchLimit:limitToDate:withCompletionBlock:
    
    • Added -fetchEntriesForItem:afterUniqueIdentifier:fetchLimit:limitToDate:withCompletionBlock:
    
    Return results that are before/after unique identifier (line number) (not including unique identifier) up to fetchLimit

--- Classes/Headers/Private/TVCLogControllerHistoricLogFilePrivate.h
+++ Classes/Headers/Private/TVCLogControllerHistoricLogFilePrivate.h
@@ -57,6 +57,18 @@ NS_ASSUME_NONNULL_BEGIN
 				 fetchLimit:(NSUInteger)fetchLimit
 				limitToDate:(nullable NSDate *)limitToDate
 		withCompletionBlock:(void (^)(NSArray<TVCLogLine *> *entries))completionBlock;
+
+- (void)fetchEntriesForItem:(IRCTreeItem *)item
+	 beforeUniqueIdentifier:(NSString *)uniqueId
+				 fetchLimit:(NSUInteger)fetchLimit
+				limitToDate:(nullable NSDate *)limitToDate
+		withCompletionBlock:(void (^)(NSArray<TVCLogLine *> *entries))completionBlock;
+
+- (void)fetchEntriesForItem:(IRCTreeItem *)item
+	  afterUniqueIdentifier:(NSString *)uniqueId
+				 fetchLimit:(NSUInteger)fetchLimit
+				limitToDate:(nullable NSDate *)limitToDate
+		withCompletionBlock:(void (^)(NSArray<TVCLogLine *> *entries))completionBlock;
 @end
 
 NS_ASSUME_NONNULL_END
