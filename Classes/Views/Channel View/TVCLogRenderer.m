Codeux-Software/Textual

commit fbd2e84c91ecb53f3fdc5e47fda7dddcf89045f0

Modify TVCLogRenderer
Allow an instance of RMustacheTemplate to be passed for template rendering.

Classes/Views/Channel View/TVCLogRenderer.m
@@ -699,13 +699,13 @@ NSString * const TVCLogRendererResultsOriginalBodyWithoutEffectsAttribute = @"TV
 
 		templateTokens[@"anchorTitle"] = [TVCLogRenderer escapeString:fragment];
 
-		html = [TVCLogRenderer renderTemplate:@"renderedStandardAnchorLinkResource" attributes:templateTokens];
+		html = [TVCLogRenderer renderTemplateNamed:@"renderedStandardAnchorLinkResource" attributes:templateTokens];
 	}
 	else if ([stringAttributes containsKey:TVCLogRendererFormattingChannelNameAttribute])
 	{
 		templateTokens[@"channelName"] = [TVCLogRenderer escapeString:fragment];
 
-		html = [TVCLogRenderer renderTemplate:@"renderedChannelNameLinkResource" attributes:templateTokens];
+		html = [TVCLogRenderer renderTemplateNamed:@"renderedChannelNameLinkResource" attributes:templateTokens];
 	}
 	else if ([stringAttributes containsKey:TVCLogRendererFormattingConversationTrackingAttribute])
 	{
@@ -883,7 +883,7 @@ NSString * const TVCLogRendererResultsOriginalBodyWithoutEffectsAttribute = @"TV
 
 	templateTokens[@"messageFragment"] = html;
 
-	return [TVCLogRenderer renderTemplate:@"formattedMessageFragment" attributes:templateTokens];
+	return [TVCLogRenderer renderTemplateNamed:@"formattedMessageFragment" attributes:templateTokens];
 }
 
 #pragma mark -
@@ -1040,12 +1040,12 @@ NSString * const TVCLogRendererResultsOriginalBodyWithoutEffectsAttribute = @"TV
 
 #pragma mark -
 
-+ (nullable NSString *)renderTemplate:(NSString *)templateName
++ (nullable NSString *)renderTemplateNamed:(NSString *)templateName
 {
 	return [TVCLogRenderer renderTemplate:templateName attributes:nil];
 }
 
-+ (nullable NSString *)renderTemplate:(NSString *)templateName attributes:(nullable NSDictionary<NSString *, id> *)templateToken
++ (nullable NSString *)renderTemplateNamed:(NSString *)templateName attributes:(nullable NSDictionary<NSString *, id> *)templateTokens
 {
 	NSParameterAssert(templateName != nil);
 
@@ -1055,7 +1055,19 @@ NSString * const TVCLogRendererResultsOriginalBodyWithoutEffectsAttribute = @"TV
 		return nil;
 	}
 
-	NSString *templateRender = [template renderObject:templateToken error:NULL];
+	return [self renderTemplate:template attributes:templateTokens];
+}
+
++ (nullable NSString *)renderTemplate:(GRMustacheTemplate *)template
+{
+	return [self renderTemplate:template attributes:nil];
+}
+
++ (nullable NSString *)renderTemplate:(GRMustacheTemplate *)template attributes:(nullable NSDictionary<NSString *, id> *)templateTokens
+{
+	NSParameterAssert(template != nil);
+
+	NSString *templateRender = [template renderObject:templateTokens error:NULL];
 
 	if (templateRender == nil) {
 		return nil;

Classes/Headers/TVCLogRenderer.h
@@ -68,8 +68,11 @@ TEXTUAL_EXTERN NSString * const TVCLogRendererResultsOriginalBodyWithoutEffectsA
 + (nullable NSColor *)mapColor:(id)color;
 + (NSColor *)mapColorCode:(NSUInteger)colorCode;
 
-+ (nullable NSString *)renderTemplate:(NSString *)templateName;
-+ (nullable NSString *)renderTemplate:(NSString *)templateName attributes:(nullable NSDictionary<NSString *, id> *)templateToken;
++ (nullable NSString *)renderTemplateNamed:(NSString *)templateName;
++ (nullable NSString *)renderTemplateNamed:(NSString *)templateName attributes:(nullable NSDictionary<NSString *, id> *)templateTokens;
+
++ (nullable NSString *)renderTemplate:(GRMustacheTemplate *)template;
++ (nullable NSString *)renderTemplate:(GRMustacheTemplate *)template attributes:(nullable NSDictionary<NSString *, id> *)templateTokens;
 
 + (NSAttributedString *)renderBodyAsAttributedString:(NSString *)body withAttributes:(NSDictionary<NSString *, id> *)inputDictionary;
 

Classes/Views/Channel View/TVCLogController.m
@@ -419,7 +419,7 @@ ClassWithDesignatedInitializerInitMethod
 - (void)mark
 {
 	TVCLogControllerPrintingBlock operationBlock = ^(id operation) {
-		NSString *markTemplate = [TVCLogRenderer renderTemplate:@"historyIndicator"];
+		NSString *markTemplate = [TVCLogRenderer renderTemplateNamed:@"historyIndicator"];
 
 		[self _evaluateFunction:@"Textual.historyIndicatorAdd" withArguments:@[markTemplate]];
 	};
@@ -637,7 +637,7 @@ ClassWithDesignatedInitializerInitMethod
 		@"message" : message
 	};
 
-	return [TVCLogRenderer renderTemplate:@"messageBufferSessionIndicator" attributes:templateAttributes];
+	return [TVCLogRenderer renderTemplateNamed:@"messageBufferSessionIndicator" attributes:templateAttributes];
 }
 
 #pragma mark -
@@ -1393,7 +1393,7 @@ ClassWithDesignatedInitializerInitMethod
 		templateAttributes[@"isEncrypted"] = @(YES);
 
 		templateAttributes[@"encryptedMessageLockTemplate"] =
-		[TVCLogRenderer renderTemplate:@"encryptedMessageLock" attributes:pathAttributes];
+		[TVCLogRenderer renderTemplateNamed:@"encryptedMessageLock" attributes:pathAttributes];
 	}
 
 	// ---- //
@@ -1446,9 +1446,9 @@ ClassWithDesignatedInitializerInitMethod
 
 	// ************************************************************************** /
 
-	NSString *templateName = [themeSettings() templateNameWithLineType:lineType];
+	GRMustacheTemplate *template = [themeSettings() templateWithLineType:lineType];
 
-	NSString *html = [TVCLogRenderer renderTemplate:templateName attributes:templateAttributes];
+	NSString *html = [TVCLogRenderer renderTemplate:template attributes:templateAttributes];
 
 	return html;
 }
@@ -1521,7 +1521,7 @@ ClassWithDesignatedInitializerInitMethod
 		templateTokens[@"textDirectionToken"] = @"ltr";
 	}
 
-	return [TVCLogRenderer renderTemplate:@"baseLayout" attributes:templateTokens];
+	return [TVCLogRenderer renderTemplateNamed:@"baseLayout" attributes:templateTokens];
 }
 
 - (NSMutableDictionary<NSString *, id> *)generateOverrideStyle

Classes/Views/Channel View/TVCLogScriptEventSink.m
@@ -1031,7 +1031,7 @@ ClassWithDesignatedInitializerInitMethod
 
 	NSDictionary *templateAttributes = [TVCLogScriptEventSink objectValueToCommon:arguments[1]];
 
-	NSString *renderedTemplate = [TVCLogRenderer renderTemplate:templateName attributes:templateAttributes];
+	NSString *renderedTemplate = [TVCLogRenderer renderTemplateNamed:templateName attributes:templateAttributes];
 
 	context.completionBlock( renderedTemplate );
 }

commit 0c084226d59740f90f502ac1b0b16d9b0f53c4ba

Correct call to parent method
Broke by fbd2e84c91ecb53f3fdc5e47fda7dddcf89045f0

Classes/Views/Channel View/TVCLogRenderer.m
@@ -1042,7 +1042,7 @@ NSString * const TVCLogRendererResultsOriginalBodyWithoutEffectsAttribute = @"TV
 
 + (nullable NSString *)renderTemplateNamed:(NSString *)templateName
 {
-	return [TVCLogRenderer renderTemplate:templateName attributes:nil];
+	return [TVCLogRenderer renderTemplateNamed:templateName attributes:nil];
 }
 
 + (nullable NSString *)renderTemplateNamed:(NSString *)templateName attributes:(nullable NSDictionary<NSString *, id> *)templateTokens