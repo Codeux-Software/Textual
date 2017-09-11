Codeux-Software/Textual

commit ed38564c669b5b958f0a978fb76663e21c879e1a

Substantial JavaScript refactoring
Functions that are private to Textual and sould never be touched by a style are now assigned to the _Textual object.

Same applies to other objects. There is now _MessageBuffer and _Textual

The visibility of each function is also declared in source, at the end of each declaration, to aid in easier understanding of their scope.

Resources/Styles/JavaScript/API/private/core/scrollTo.js
@@ -35,6 +35,8 @@
 
  *********************************************************************** */
 
+"use strict";
+
 /* ************************************************** */
 /*                                                    */
 /* DO NOT OVERRIDE ANYTHING BELOW THIS LINE           */
@@ -42,26 +44,30 @@
 /* ************************************************** */
 
 /* Scrolling */
-Textual.scrollToBottomOfView = function(fireNotification)
+Textual.scrollToBottomOfView = function(fireNotification) /* PUBLIC */
 {
 	document.body.scrollToBottom();
 	
-	Textual.viewPositionMovedToBottom();
+	if (fireNotification) {
+		Textual.viewPositionMovedToBottom();
+	}
 };
 
-Textual.scrollToTopOfView = function(fireNotification)
+Textual.scrollToTopOfView = function(fireNotification) /* PUBLIC */
 {
 	document.body.scrollToTop();
 
-	Textual.viewPositionMovedToTop();
+	if (fireNotification) {
+		Textual.viewPositionMovedToTop();
+	}
 };
 
-Textual.scrollToLine = function(lineNumber)
+Textual.scrollToLine = function(lineNumber) /* PUBLIC */
 {
 	Textual.jumpToLine(lineNumber);
 };
 
-Textual.jumpToLine = function(lineNumber)
+Textual.jumpToLine = function(lineNumber) /* PUBLIC */
 {
 	MessageBuffer.jumpToLine(
 		lineNumber, 
@@ -76,18 +82,12 @@ Textual.jumpToLine = function(lineNumber)
 	);
 };
 
-Textual.scrollToElement = function(elementName)
+Textual.scrollToElement = function(elementName) /* PUBLIC */
 {
 	var element = document.getElementById(elementName);
 
 	if (element) {
-		var elementRect = element.getBoundingClientRect();
-		var elementTop = (elementRect.top + window.scrollY);
-		var elementCenter = (elementTop - (window.innerHeight / 2));
-	
-		console.log("Scrolling to " + elementCenter);
-	
-		window.scrollTo(0, elementCenter);
+		TextualScroller.scrollElementToCenter(element);
 	
 		return true;
 	}
@@ -95,7 +95,7 @@ Textual.scrollToElement = function(elementName)
 	return false;
 };
 
-Textual.scrollToHistoryIndicator = function()
+Textual.scrollToHistoryIndicator = function() /* PUBLIC */
 {
 	if (Textual.scrollToElement("mark")) {
 		Textual.viewPositionModToHistoryIndicator();

Classes/Views/Channel View/TVCLogController.m
@@ -421,7 +421,7 @@ ClassWithDesignatedInitializerInitMethod
 	TVCLogControllerPrintingBlock operationBlock = ^(id operation) {
 		NSString *markTemplate = [TVCLogRenderer renderTemplateNamed:@"historyIndicator"];
 
-		[self _evaluateFunction:@"Textual.historyIndicatorAdd" withArguments:@[markTemplate]];
+		[self _evaluateFunction:@"_Textual.historyIndicatorAdd" withArguments:@[markTemplate]];
 	};
 
 	_enqueueBlock(operationBlock);
@@ -429,7 +429,7 @@ ClassWithDesignatedInitializerInitMethod
 
 - (void)unmark
 {
-	[self _evaluateFunction:@"Textual.historyIndicatorRemove" withArguments:nil];
+	[self _evaluateFunction:@"_Textual.historyIndicatorRemove" withArguments:nil];
 }
 
 - (void)goToMark
@@ -444,7 +444,7 @@ ClassWithDesignatedInitializerInitMethod
 {
 	NSParameterAssert(html != nil);
 
-	[self _evaluateFunction:@"Textual.documentBodyAppendHistoric" withArguments:@[html, lineNumbers, @(isReload)]];
+	[self _evaluateFunction:@"_Textual.documentBodyAppendHistoric" withArguments:@[html, lineNumbers, @(isReload)]];
 }
 
 /* reloadOldLines: is supposed to be called from inside a queue. */
@@ -668,7 +668,7 @@ ClassWithDesignatedInitializerInitMethod
 
 - (void)notifyDidBecomeVisible /* When the view is switched to */
 {
-	[self _evaluateFunction:@"Textual.notifyDidBecomeVisible" withArguments:nil];
+	[self _evaluateFunction:@"_Textual.notifyDidBecomeVisible" withArguments:nil];
 
 	[self maybeReloadHistory];
 
@@ -681,12 +681,12 @@ ClassWithDesignatedInitializerInitMethod
 
 - (void)notifySelectionChanged
 {
-	[self _evaluateFunction:@"Textual.notifySelectionChanged" withArguments:@[@(self.selected)]];
+	[self _evaluateFunction:@"_Textual.notifySelectionChanged" withArguments:@[@(self.selected)]];
 }
 
 - (void)notifyDidBecomeHidden
 {
-	[self _evaluateFunction:@"Textual.notifyDidBecomeHidden" withArguments:nil];
+	[self _evaluateFunction:@"_Textual.notifyDidBecomeHidden" withArguments:nil];
 
 	[self.backingView saveScrollerPosition];
 
@@ -695,7 +695,7 @@ ClassWithDesignatedInitializerInitMethod
 
 - (void)notifyViewFinishedLoadingHistory
 {
-	[self _evaluateFunction:@"Textual.viewFinishedLoadingHistoryInt" withArguments:nil];
+	[self _evaluateFunction:@"_Textual.viewFinishedLoadingHistory" withArguments:nil];
 }
 
 - (void)changeTextSize:(BOOL)bigger
@@ -711,7 +711,7 @@ ClassWithDesignatedInitializerInitMethod
 {
 	NSUInteger scrollbackLimit = [TPCPreferences scrollbackVisibleLimit];
 
-	[self _evaluateFunction:@"MessageBuffer.setBufferLimit" withArguments:@[@(scrollbackLimit)]];
+	[self _evaluateFunction:@"_MessageBuffer.setBufferLimit" withArguments:@[@(scrollbackLimit)]];
 }
 
 #pragma mark -
@@ -1438,7 +1438,7 @@ ClassWithDesignatedInitializerInitMethod
 
 - (void)isSafeToPresentImageWithId:(NSString *)uniqueId
 {
-	[self _evaluateFunction:@"Textual.toggleInlineImageVisibility" withArguments:@[uniqueId]];
+	[self _evaluateFunction:@"_Textual.toggleInlineImageVisibility" withArguments:@[uniqueId]];
 }
 
 - (void)isNotSafeToPresentImageWithId:(NSString *)uniqueId
@@ -1581,7 +1581,7 @@ ClassWithDesignatedInitializerInitMethod
 
 	NSUInteger scrollbackLimit = [TPCPreferences scrollbackVisibleLimit];
 
-	[self _evaluateFunction:@"Textual.viewFinishedLoadingInt"
+	[self _evaluateFunction:@"_Textual.viewFinishedLoading"
 			  withArguments:
 	 @[
 		  @{

Resources/Language Files/English.lproj/IRC.strings
@@ -174,7 +174,7 @@
 
 /* Software trial (Mac App Store) */
 "IRC[1125]" = "Disconnected from server due to trial period limit. Please unlock the Standard Edition of Textual 7, or simply reconnect.";
-"IRC[1126]" = "You will be disconnected in %.0f minutes due to trial period limit. You can remove this limitation by unlocking the Standard Edition of Textual 7. <a href='#' onclick='app.showInAppPurchaseWindow(); return false;'>Click here</a> to do this.";
+"IRC[1126]" = "You will be disconnected in %.0f minutes due to trial period limit. You can remove this limitation by unlocking the Standard Edition of Textual 7. <a href='#' onclick='appPrivate.showInAppPurchaseWindow(); return false;'>Click here</a> to do this.";
 
 /* Specific IRC related, app-specific errors */
 "IRC[1121]" = "\002Error:\002 Messages cannot be sent to this window.";

Resources/Styles/JavaScript/API/core.js
@@ -35,6 +35,8 @@
 
  *********************************************************************** */
 
+"use strict";
+
 var Textual = {};
 
 /* *********************************************************************** */

Resources/Styles/JavaScript/API/corePrivate.js
@@ -35,11 +35,16 @@
 
  *********************************************************************** */
 
+"use strict";
+
 /* ************************************************** */
 /*                                                    */
 /* DO NOT OVERRIDE ANYTHING BELOW THIS LINE           */
 /*                                                    */
 /* ************************************************** */
+
+/* Private objects */
+var _Textual = {};
 
 /* Resource management */
 Textual.initializeCore = function(resourcesPath)
@@ -50,7 +55,6 @@ Textual.initializeCore = function(resourcesPath)
 	Textual.includeScriptResourceFile(resourcesPath + "/JavaScript/API/private/core/inlineMedia.js");
 	Textual.includeScriptResourceFile(resourcesPath + "/JavaScript/API/private/core/messageBuffer.js");
 	Textual.includeScriptResourceFile(resourcesPath + "/JavaScript/API/private/core/scrollTo.js");
-	Textual.includeScriptResourceFile(resourcesPath + "/JavaScript/API/private/core/scroll.js");
 	Textual.includeScriptResourceFile(resourcesPath + "/JavaScript/API/private/scroller/state.js");
 	
 	/* Only load auto scroller if we believe this is WebKit2 */

Resources/Styles/JavaScript/API/private/conversationTracking.js
@@ -35,6 +35,8 @@
 
  *********************************************************************** */
 
+"use strict";
+
 /* ************************************************** */
 /*                                                    */
 /* DO NOT OVERRIDE ANYTHING BELOW THIS LINE           */

Resources/Styles/JavaScript/API/private/core/clickMenuSelection.js
@@ -35,52 +35,51 @@
 
  *********************************************************************** */
 
+"use strict";
+
 /* ************************************************** */
 /*                                                    */
 /* DO NOT OVERRIDE ANYTHING BELOW THIS LINE           */
 /*                                                    */
 /* ************************************************** */
 
-/* Internal state */
-Textual.nicknameDoubleClickTimer = null;
-
 /* Selection */
-Textual.clearSelection = function()
+Textual.currentSelection = function() /* PUBLIC */
+{
+	return window.getSelection().toString();
+};
+
+Textual.clearSelection = function() /* PUBLIC */
 {
 	window.getSelection().empty();
 };
 
-Textual.clearSelectionAndPreventDefault = function()
+_Textual.clearSelectionAndPreventDefault = function() /* PRIVATE */
 {
 	Textual.clearSelection();
 
 	event.preventDefault();
 };
 
-Textual.currentSelection = function()
-{
-	return window.getSelection().toString();
-};
-
-Textual.recordSelection = function()
+_Textual.recordSelection = function() /* PRIVATE */
 {
 	var selectedText = Textual.currentSelection();
 
-	app.setSelection(selectedText);
+	appPrivate.setSelection(selectedText);
 };
 
-Textual.selectionChangedCallback = function()
+_Textual.selectionChangedCallback = function() /* PRIVATE */
 {
-	Textual.recordSelection();
+	_Textual.recordSelection();
 };
 
-Textual.copySelectionOnMouseUpEvent = function()
+_Textual.copySelectionOnMouseUpEvent = function() /* PRIVATE */
 {
 	if (window.event.metaKey || window.event.altKey) {
 		return;
 	}
 
-	app.copySelectionWhenPermitted(
+	appPrivate.copySelectionWhenPermitted(
 	   function(returnValue) {
 			if (returnValue) {
 				Textual.clearSelection();
@@ -90,9 +89,9 @@ Textual.copySelectionOnMouseUpEvent = function()
 };
 
 /* Contextual menu management */
-Textual.usesCustomMenuConstructor = function()
+_Textual.usesCustomMenuConstructor = function() /* PRIVATE */
 {
-	if (appInternal.isWebKit2() === false) {
+	if (app.isWebKit2() === false) {
 		return false;
 	}
 	
@@ -106,147 +105,149 @@ Textual.usesCustomMenuConstructor = function()
 	}
 };
 
-Textual.openGenericContextualMenu = function()
+_Textual.openGenericContextualMenu = function() /* PRIVATE */
 {
 	/* Do not block if target element already has a callback. */
 	if (event.target.oncontextmenu !== null) {
 		return;
 	}
 
-	if (Textual.usesCustomMenuConstructor()) {
+	if (_Textual.usesCustomMenuConstructor()) {
 		event.preventDefault();
 		
-		Textual.recordSelection();
+		_Textual.recordSelection();
 
-		app.displayContextMenu();
+		appPrivate.displayContextMenu();
 	}
 };
 
-Textual.openChannelNameContextualMenu = function()
+Textual.openChannelNameContextualMenu = function() /* PUBLIC */
 {
-	Textual.setPolicyChannelName();
+	_Textual.setPolicyChannelName();
 
-	if (Textual.usesCustomMenuConstructor()) {
-		Textual.clearSelectionAndPreventDefault();
+	if (_Textual.usesCustomMenuConstructor()) {
+		_Textual.clearSelectionAndPreventDefault();
 
-		app.displayContextMenu();
+		appPrivate.displayContextMenu();
 	}
 };
 
-Textual.openURLManagementContextualMenu = function()
+Textual.openURLManagementContextualMenu = function() /* PUBLIC */
 {
-	Textual.setPolicyURLAddress();
+	_Textual.setPolicyURLAddress();
 
-	if (Textual.usesCustomMenuConstructor()) {
-		Textual.clearSelectionAndPreventDefault();
+	if (_Textual.usesCustomMenuConstructor()) {
+		_Textual.clearSelectionAndPreventDefault();
 
-		app.displayContextMenu();
+		appPrivate.displayContextMenu();
 	}
 };
 
-Textual.openStandardNicknameContextualMenu = function()
+Textual.openStandardNicknameContextualMenu = function() /* PUBLIC */
 {
-	Textual.setPolicyStandardNickname();
+	_Textual.setPolicyStandardNickname();
 
-	if (Textual.usesCustomMenuConstructor()) {
-		Textual.clearSelectionAndPreventDefault();
+	if (_Textual.usesCustomMenuConstructor()) {
+		_Textual.clearSelectionAndPreventDefault();
 
-		app.displayContextMenu();
+		appPrivate.displayContextMenu();
 	}
 };
 
-Textual.openInlineNicknameContextualMenu = function()
+Textual.openInlineNicknameContextualMenu = function() /* PUBLIC */
 {
-	Textual.setPolicyInlineNickname();
+	_Textual.setPolicyInlineNickname();
 
-	if (Textual.usesCustomMenuConstructor()) {
-		Textual.clearSelectionAndPreventDefault();
+	if (_Textual.usesCustomMenuConstructor()) {
+		_Textual.clearSelectionAndPreventDefault();
 
-		app.displayContextMenu();
+		appPrivate.displayContextMenu();
 	}
 };
 
-Textual.setPolicyStandardNickname = function()
+_Textual.setPolicyStandardNickname = function() /* PRIVATE */
 {
 	var userNickname = event.target.getAttribute("nickname");
 
-	app.setNickname(userNickname);
+	appPrivate.setNickname(userNickname);
 };
 
-Textual.setPolicyInlineNickname = function()
+_Textual.setPolicyInlineNickname = function() /* PRIVATE */
 {
 	var userNickname = event.target.textContent;
 
 	var userMode = event.target.getAttribute("mode");
 
 	if (userMode && userMode.length > 0 && userNickname.indexOf(userMode) === 0) {
-		app.setNickname(userNickname.substring(1));
+		appPrivate.setNickname(userNickname.substring(1));
 	} else {
-		app.setNickname(userNickname);
+		appPrivate.setNickname(userNickname);
 	}
 };
 
-Textual.setPolicyURLAddress = function()
+_Textual.setPolicyURLAddress = function() /* PRIVATE */
 {
-	app.setURLAddress(event.target.getAttribute("href"));
+	appPrivate.setURLAddress(event.target.getAttribute("href"));
 };
 
-Textual.setPolicyChannelName = function()
+_Textual.setPolicyChannelName = function() /* PRIVATE */
 {
-	app.setChannelName(event.target.textContent);
+	appPrivate.setChannelName(event.target.textContent);
 };
 
 /* Double click actions */
-Textual.nicknameMaybeWasDoubleClicked = function(e)
-{
-	if (Textual.nicknameDoubleClickTimer) {
-		clearTimeout(Textual.nicknameDoubleClickTimer);
+Textual._nicknameDoubleClickTimer = null;
 
-		Textual.nicknameDoubleClickTimer = null;
+Textual.nicknameMaybeWasDoubleClicked = function(e) /* PUBLIC */
+{
+	if (Textual._nicknameDoubleClickTimer) {
+		clearTimeout(Textual._nicknameDoubleClickTimer);
+
+		Textual._nicknameDoubleClickTimer = null;
 
 		Textual.nicknameDoubleClicked(e);
 	} else {
-		Textual.nicknameDoubleClickTimer = setTimeout(function() {
-			Textual.nicknameDoubleClickTimer = null;
+		Textual._nicknameDoubleClickTimer = setTimeout(function() {
+			Textual._nicknameDoubleClickTimer = null;
 
 			Textual.nicknameSingleClicked(e);
 		}, 250);
 	}
 };
 
-Textual.nicknameSingleClicked = function(e)
+Textual.nicknameSingleClicked = function(e) /* PUBLIC */
 {
 	// API does not handle this action by default...
 };
 
-Textual.channelNameDoubleClicked = function()
+Textual.channelNameDoubleClicked = function() /* PUBLIC */
 {
-	Textual.clearSelectionAndPreventDefault();
+	_Textual.clearSelectionAndPreventDefault();
 
-	Textual.setPolicyChannelName();
+	_Textual.setPolicyChannelName();
 
-	app.channelNameDoubleClicked();
+	appPrivate.channelNameDoubleClicked();
 };
 
-Textual.nicknameDoubleClicked = function()
+Textual.nicknameDoubleClicked = function() /* PUBLIC */
 {
-	Textual.clearSelectionAndPreventDefault();
+	_Textual.clearSelectionAndPreventDefault();
 
-	Textual.setPolicyStandardNickname();
+	_Textual.setPolicyStandardNickname();
 
-	app.nicknameDoubleClicked();
+	appPrivate.nicknameDoubleClicked();
 };
 
-Textual.inlineNicknameDoubleClicked = function()
+Textual.inlineNicknameDoubleClicked = function() /* PUBLIC */
 {
-	Textual.clearSelectionAndPreventDefault();
+	_Textual.clearSelectionAndPreventDefault();
 
-	Textual.setPolicyInlineNickname();
+	_Textual.setPolicyInlineNickname();
 
-	app.nicknameDoubleClicked();
+	appPrivate.nicknameDoubleClicked();
 };
 
 /* Bind to events */
-document.addEventListener("contextmenu", Textual.openGenericContextualMenu, false);
+document.addEventListener("contextmenu", _Textual.openGenericContextualMenu, false);
 
-document.addEventListener("selectionchange", Textual.selectionChangedCallback, false);
+document.addEventListener("selectionchange", _Textual.selectionChangedCallback, false);

Resources/Styles/JavaScript/API/private/core/documentBody.js
@@ -35,29 +35,28 @@
 
  *********************************************************************** */
 
+"use strict";
+
 /* ************************************************** */
 /*                                                    */
 /* DO NOT OVERRIDE ANYTHING BELOW THIS LINE           */
 /*                                                    */
 /* ************************************************** */
 
-Textual.documentBodyElementReference = null;
-Textual.topicBarElementReference = null;
-
 /* Loading screen */
-Textual.loadingScreenElement = function()
+Textual.loadingScreenElement = function() /* PUBLIC */
 {
 	return document.getElementById("loading_screen");
 };
 
-Textual.fadeInLoadingScreen = function(bodyOp, topicOp)
+Textual.fadeInLoadingScreen = function(bodyOp, topicOp) /* PUBLIC */
 {
 	console.warn("Deprecated function. Use Textual.fadeOutLoadingScreen() instead.");
 
 	Textual.fadeOutLoadingScreen(bodyOp, topicOp);
 };
 
-Textual.fadeOutLoadingScreen = function(bodyOp, topicOp)
+Textual.fadeOutLoadingScreen = function(bodyOp, topicOp) /* PUBLIC */
 {
 	var documentBody = Textual.documentBodyElement();
 
@@ -86,16 +85,18 @@ Textual.fadeOutLoadingScreen = function(bodyOp, topicOp)
 };
 
 /* Topic bar */
-Textual.topicBarElement = function()
+Textual._topicBarElementReference = null; /* PRIVATE */
+
+Textual.topicBarElement = function() /* PUBLIC */
 {
-	if (Textual.topicBarElementReference === null) {
-		Textual.topicBarElementReference = document.getElementById("topic_bar");
+	if (Textual._topicBarElementReference === null) {
+		Textual._topicBarElementReference = document.getElementById("topic_bar");
 	}
 
-	return Textual.topicBarElementReference;
+	return Textual._topicBarElementReference;
 };
 
-Textual.topicBarValue = function(asText)
+Textual.topicBarValue = function(asText) /* PUBLIC */
 {
 	var topicBar = Textual.topicBarElement();
 
@@ -105,12 +106,12 @@ Textual.topicBarValue = function(asText)
 		} else {
 			return topicBar.innerHTML;
 		}
-	} else {
-		return null;
 	}
+		
+	return null;
 };
 
-Textual.setTopicBarValue = function(topicValue, topicValueHTML)
+Textual.setTopicBarValue = function(topicValue, topicValueHTML) /* PUBLIC */
 {
 	var topicBar = Textual.topicBarElement();
 
@@ -120,12 +121,12 @@ Textual.setTopicBarValue = function(topicValue, topicValueHTML)
 		Textual.topicBarValueChanged(topicValue);
 
 		return true;
-	} else {
-		return false;
 	}
+	
+	return false;
 };
 
-Textual.setTopicBarVisible = function(isVisible)
+Textual.setTopicBarVisible = function(isVisible) /* PUBLIC */
 {
 	var topicBar = Textual.topicBarElement();
 
@@ -138,43 +139,45 @@ Textual.setTopicBarVisible = function(isVisible)
 	}
 };
 
-Textual.topicBarDoubleClicked = function()
+Textual.topicBarDoubleClicked = function() /* PUBLIC */
 {
-	app.topicBarDoubleClicked();
+	appPrivate.topicBarDoubleClicked();
 };
 
 /* History indicator */
-Textual.historyIndicatorAdd = function(templateHTML)
+_Textual.historyIndicatorAdd = function(templateHTML) /* PRIVATE */
 {
-	Textual.historyIndicatorRemove();
+	_Textual.historyIndicatorRemove();
 
 	MessageBuffer.bufferElementAppend(templateHTML);
 
 	Textual.historyIndicatorAddedToView();
 };
 
-Textual.historyIndicatorRemove = function()
+_Textual.historyIndicatorRemove = function() /* PRIVATE */
 {
 	var e = document.getElementById("mark");
 
 	if (e) {
-		e.parentNode.removeChild(e);
+		e.remove();
 
 		Textual.historyIndicatorRemovedFromView();
 	}
 };
 
 /* Document body */
-Textual.documentBodyElement = function()
+Textual._documentBodyElementReference = null; /* PRIVATE */
+
+Textual.documentBodyElement = function() /* PUBLIC */
 {
-	if (Textual.documentBodyElementReference === null) {
-		Textual.documentBodyElementReference = document.getElementById("body_home");
+	if (Textual._documentBodyElementReference === null) {
+		Textual._documentBodyElementReference = document.getElementById("body_home");
 	}
 	
-	return Textual.documentBodyElementReference;
+	return Textual._documentBodyElementReference;
 };
 
-Textual.setDocumentBodyPointerEventsEnabled = function(enablePointerEvents)
+Textual.setDocumentBodyPointerEventsEnabled = function(enablePointerEvents) /* PUBLIC */
 {
 	var documentBody = Textual.documentBodyElement();
 
@@ -187,13 +190,13 @@ Textual.setDocumentBodyPointerEventsEnabled = function(enablePointerEvents)
 	}
 };
 
-Textual.documentHTML = function()
+Textual.documentHTML = function() /* PUBLIC */
 {
 	return document.documentElement.innerHTML;
 };
 
 /* History */
-Textual.documentBodyAppendHistoric = function(templateHTML, lineNumbers, isReload)
+_Textual.documentBodyAppendHistoric = function(templateHTML, lineNumbers, isReload) /* PRIVATE */
 {
 	var atBottom = TextualScroller.isScrolledToBottom();
 
@@ -211,7 +214,7 @@ Textual.documentBodyAppendHistoric = function(templateHTML, lineNumbers, isReloa
 };
 
 /* Text */
-Textual.changeTextSizeMultiplier = function(sizeMultiplier)
+Textual.changeTextSizeMultiplier = function(sizeMultiplier) /* PUBLIC */
 {
 	if (sizeMultiplier === 1.0) {
 		document.body.style.fontSize = "";
@@ -221,7 +224,7 @@ Textual.changeTextSizeMultiplier = function(sizeMultiplier)
 }
 
 /* Line numbers */
-Textual.lineNumberStandardize = function(lineNumber)
+Textual.lineNumberStandardize = function(lineNumber) /* PUBLIC */
 {
 	if (lineNumber.indexOf("line-") !== 0) {
 		lineNumber = ("line-" + lineNumber);
@@ -230,7 +233,7 @@ Textual.lineNumberStandardize = function(lineNumber)
 	return lineNumber;
 };
 
-Textual.lineNumberContents = function(lineNumber)
+Textual.lineNumberContents = function(lineNumber) /* PUBLIC */
 {
 	if (lineNumber.indexOf("line-") !== 0) {
 		return lineNumber;

commit 6b2aef009a6b5707e6985e4db620076c0cd54362

Call out to new function object
Broke by ed38564c669b5b958f0a978fb76663e21c879e1a

Resources/Styles/JavaScript/API/private/core/scrollTo.js
@@ -77,7 +77,7 @@ Textual.jumpToLine = function(lineNumber) /* PUBLIC */
 				Textual.viewPositionMovedToLine(lineNumber);
 			}
 
-			app.notifyJumpToLineCallback(lineNumber, success);
+			appPrivate.notifyJumpToLineCallback(lineNumber, success);
 		})
 	);
 };