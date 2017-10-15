6d80a24c067717e139aaa14d574980207cc43316
Michael Morris <michael@codeux.com>
2017-10-14 22:28:37 -0400

    Add -applicationTemporaryProcessSpecific

--- Classes/Headers/TPCPathInfo.h
+++ Classes/Headers/TPCPathInfo.h
@@ -70,6 +70,9 @@ NS_ASSUME_NONNULL_BEGIN
 @property (readonly, class) NSString *applicationTemporary;
 @property (readonly, class) NSURL *applicationTemporaryURL;
 
+@property (readonly, class) NSString *applicationTemporaryProcessSpecific;
+@property (readonly, class) NSURL *applicationTemporaryProcessSpecificURL;
+
 @property (readonly, class) NSString *bundledExtensions;
 @property (readonly, class) NSURL *bundledExtensionsURL;
 
