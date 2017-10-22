4cdc65601f7e9da3a40aeb9f38bc331d5875f3e9
Michael Morris <michael@codeux.com>
2017-10-22 00:26:09 -0400

    Fix ICMInlineHTML not passing -classAttribute value

--- XPC Services/Inline Content Loader/Classes/Modules/Root Classes/ICMInlineHTML.m
+++ XPC Services/Inline Content Loader/Classes/Modules/Root Classes/ICMInlineHTML.m
@@ -47,6 +47,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 	NSDictionary *templateAttributes =
 	@{
+		@"classAttribute" : self.classAttribute,
 		@"unescapedHTML" : unescapedHTML,
 		@"uniqueIdentifier" : payload.uniqueIdentifier
 	};
