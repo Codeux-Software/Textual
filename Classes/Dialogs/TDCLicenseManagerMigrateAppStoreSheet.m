Codeux-Software/Textual

commit 0ffbb2be5662fee73d36d72943e00f3f32965f3a

Remove/stop using silly helpers

Classes/Dialogs/TDCLicenseManagerMigrateAppStoreSheet.m
@@ -310,8 +310,9 @@ NS_ASSUME_NONNULL_BEGIN
 	 value that we are expecting from an older copy which we cannot change. */
 	NSString *applicationBundleID = [applicationBundle objectForInfoDictionaryKey:@"CFBundleIdentifier"];
 
-	if (NSObjectsAreEqual(applicationBundleID, @"com.codeux.irc.textual5") == NO &&
-		NSObjectsAreEqual(applicationBundleID, @"com.codeux.apps.textual-mas") == NO)
+	if (applicationBundleID == nil ||
+		[applicationBundleID isEqualToString:@"com.codeux.irc.textual5"] == NO ||
+		[applicationBundleID isEqualToString:@"com.codeux.apps.textual-mas"] == NO)
 	{
 		return NO;
 	}

Classes/Controllers/TXMenuController.m
@@ -3408,7 +3408,7 @@ NS_ASSUME_NONNULL_BEGIN
 			return [name1 compare:name2];
 		}];
 
-		if (NSObjectsAreEqual(u.channelList, channelList)) {
+		if ([channelList isEqualToArray:u.channelList]) {
 			continue;
 		}
 

Classes/Dialogs/File Transfers/TDCFileTransferDialog.m
@@ -148,7 +148,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 	TDCFileTransferDialogTransferController *fileTransfer =
 	[self fileTransferMatchingCondition:^BOOL(TDCFileTransferDialogTransferController *controller) {
-		return NSObjectsAreEqual(identifier, controller.uniqueIdentifier);
+		return [identifier isEqualToString:controller.uniqueIdentifier];
 	}];
 
 	return fileTransfer;
@@ -160,7 +160,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 	TDCFileTransferDialogTransferController *fileTransfer =
 	[self fileTransferMatchingCondition:^BOOL(TDCFileTransferDialogTransferController *controller) {
-		return NSObjectsAreEqual(transferToken, controller.transferToken);
+		return [transferToken isEqualToString:controller.transferToken];
 	}];
 
 	return (fileTransfer != nil);
@@ -172,7 +172,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 	TDCFileTransferDialogTransferController *fileTransfer =
 	[self fileTransferMatchingCondition:^BOOL(TDCFileTransferDialogTransferController *controller) {
-		return (NSObjectsAreEqual(transferToken, controller.transferToken) && controller.isSender);
+		return ([transferToken isEqualToString:controller.transferToken] && controller.isSender);
 	}];
 
 	return fileTransfer;
@@ -184,7 +184,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 	TDCFileTransferDialogTransferController *fileTransfer =
 	[self fileTransferMatchingCondition:^BOOL(TDCFileTransferDialogTransferController *controller) {
-		return (NSObjectsAreEqual(transferToken, controller.transferToken) && controller.isSender == NO);
+		return ([transferToken isEqualToString:controller.transferToken] && controller.isSender == NO);
 	}];
 
 	return fileTransfer;

Classes/Dialogs/File Transfers/TDCFileTransferDialogTransferController.m
@@ -712,13 +712,15 @@ ClassWithDesignatedInitializerInitMethod
 
 - (void)peerNicknameChanged:(NSNotification *)notification
 {
-	NSString *oldNickname = notification.userInfo[@"oldNickname"];
+	NSDictionary *userInfo = notification.userInfo;
 
-	if (NSObjectsAreEqual(oldNickname, self.peerNickname) == NO) {
+	NSString *oldNickname = userInfo[@"oldNickname"];
+
+	if ([self.peerNickname isEqualToString:oldNickname] == NO) {
 		return;
 	}
 
-	self.peerNickname = notification.userInfo[@"newNickname"];
+	self.peerNickname = userInfo[@"newNickname"];
 }
 
 - (void)clientDisconnected:(NSNotification *)notification

Classes/Dialogs/Preferences/TDCPreferencesController.m
@@ -1048,7 +1048,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 	NSString *currentTheme = [TPCPreferences themeName];
 
-	if ([currentTheme isEqual:newTheme]) {
+	if ([currentTheme isEqualToString:newTheme]) {
 		return;
 	}
 
@@ -1269,8 +1269,8 @@ NS_ASSUME_NONNULL_BEGIN
 		NSUInteger applicationIndex =
 		[RZWorkspace().runningApplications indexOfObjectPassingTest:^BOOL(NSRunningApplication *application, NSUInteger index, BOOL *stop) {
 			return
-			(NSObjectsAreEqual(application.localizedName, @"TorBrowser") ||
-			 NSObjectsAreEqual(application.localizedName, @"Tor Browser"));
+			([application.localizedName isEqualToString:@"TorBrowser"] ||
+			 [application.localizedName isEqualToString:@"Tor Browser"]);
 		}];
 
 		presentDialog = (applicationIndex != NSNotFound);

Classes/Dialogs/TDCHighlightEntrySheet.m
@@ -114,7 +114,7 @@ ClassWithDesignatedInitializerInitMethod
 
 		[self.matchChannelPopupButton addItemWithTitle:channelName];
 
-		if (NSObjectsAreEqual(channel.uniqueIdentifier, matchChannelId)) {
+		if ([channel.uniqueIdentifier isEqualToString:matchChannelId]) {
 			[self.matchChannelPopupButton selectItemWithTitle:channelName];
 		}
 
@@ -142,7 +142,7 @@ ClassWithDesignatedInitializerInitMethod
 		NSString *selectedChannelName = self.matchChannelPopupButton.titleOfSelectedItem;
 
 		for (IRCChannelConfig *c in self.channelList) {
-			if (NSObjectsAreEqual(c.channelName, selectedChannelName)) {
+			if ([c.channelName isEqualToString:selectedChannelName]) {
 				self.config.matchChannelId = c.uniqueIdentifier;
 
 				break;

Classes/Dialogs/TDCInAppPurchaseUpgradeEligibilitySheet.m
@@ -195,7 +195,7 @@ NS_ASSUME_NONNULL_BEGIN
 	 value that we are expecting from an older copy which we cannot change. */
 	NSString *applicationBundleID = [applicationBundle objectForInfoDictionaryKey:@"CFBundleIdentifier"];
 
-	if (NSObjectsAreEqual(applicationBundleID, @"com.codeux.irc.textual5") == NO) {
+	if (applicationBundleID == nil || [applicationBundleID isEqualToString:@"com.codeux.irc.textual5"] == NO) {
 		return NO;
 	}
 

Classes/Dialogs/TDCServerPropertiesSheet.m
@@ -1461,7 +1461,7 @@ NS_ASSUME_NONNULL_BEGIN
 						  sha1Fingerprint:&sha1Fingerprint
 						   md5Fingerprint:&md5Fingerprint];
 
-	BOOL hasNoCertificate = NSObjectIsEmpty(commonName);
+	BOOL hasNoCertificate = (commonName.length == 0);
 
 	if (hasNoCertificate) {
 		self.clientCertificateCommonNameField.stringValue = TXTLS(@"TDCServerPropertiesSheet[1008]");
@@ -1980,7 +1980,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 			if (matchChannelId) {
 				for (IRCChannelConfig *channel in self.channelList) {
-					if (NSObjectsAreEqual(channel.uniqueIdentifier, matchChannelId) == NO) {
+					if ([channel.uniqueIdentifier isEqualToString:matchChannelId] == NO) {
 						continue;
 					}
 

commit 423fedf57875f73faa19bcd7e978ad88854a3d20

Fix condition
Incorrect condition introduced by 0ffbb2b

Classes/Dialogs/TDCLicenseManagerMigrateAppStoreSheet.m
@@ -311,8 +311,8 @@ NS_ASSUME_NONNULL_BEGIN
 	NSString *applicationBundleID = [applicationBundle objectForInfoDictionaryKey:@"CFBundleIdentifier"];
 
 	if (applicationBundleID == nil ||
-		[applicationBundleID isEqualToString:@"com.codeux.irc.textual5"] == NO ||
-		[applicationBundleID isEqualToString:@"com.codeux.apps.textual-mas"] == NO)
+		([applicationBundleID isEqualToString:@"com.codeux.irc.textual5"] == NO &&
+		 [applicationBundleID isEqualToString:@"com.codeux.apps.textual-mas"] == NO))
 	{
 		return NO;
 	}