2bd8b8db4efea4de96c6b6d6921cbea4b71c2bd3
Michael <michael@codeux.com>
2018-06-05 20:37:08 -0400

    Modify the build system
    • Create "Standard Release Sandboxed" build scheme with matching build configuration files and entitlement file.
    • Entitlement files are no longer copied in a pre-build script. Each target now has a static file.
    • Shared build schemes for plugins were removed because they didn't serve much purpose.
    • The CleanUpResources.sh script was removed.

--- Main Project (Textual).xcodeproj/BuildFrameworks.sh
+++ Main Project (Textual).xcodeproj/BuildFrameworks.sh
@@ -14,10 +14,10 @@ if [ "${TEXTUAL_BUILD_SCHEME_TOKEN}" == "appstore" ]; then
 cd "${PROJECT_DIR}/Frameworks/Apple Receipt Loader/"
 
 xcodebuild -target "libreceipt" \
--configuration "${TEXTUAL_FRAMEWORK_BUILD_SCHEME}" \
-CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}" \
-DEVELOPMENT_TEAM="${DEVELOPMENT_TEAM}" \
-PROVISIONING_PROFILE_SPECIFIER=""
+ -configuration "${TEXTUAL_FRAMEWORK_BUILD_SCHEME}" \
+ CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}" \
+ DEVELOPMENT_TEAM="${DEVELOPMENT_TEAM}" \
+ PROVISIONING_PROFILE_SPECIFIER=""
 
 mv "${PROJECT_DIR}/Frameworks/Apple Receipt Loader/Build Results/${TEXTUAL_FRAMEWORK_BUILD_SCHEME}/libreceipt.a" "${BUILD_DESTINATION_FOLDER}"
 rm -rf "${PROJECT_DIR}/Frameworks/Apple Receipt Loader/Build Results/"
@@ -62,3 +62,5 @@ xcodebuild -target "CocoaExtensions (OS X)" \
 mv "${PROJECT_DIR}/Frameworks/Cocoa Extensions/Build Results/${TEXTUAL_FRAMEWORK_BUILD_SCHEME}/CocoaExtensions.framework" "${BUILD_DESTINATION_FOLDER}"
 rm -rf "${PROJECT_DIR}/Frameworks/Cocoa Extensions/Build Results/"
 rm -rf "${PROJECT_DIR}/Frameworks/Cocoa Extensions/.tmp/"
+
+exit 0;
