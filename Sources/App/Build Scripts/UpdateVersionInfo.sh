a7a117f2f5f365407269a1c4490c8f25ee1cd8ee
Michael <michael@codeux.com>
2018-07-04 12:36:00 -0400

    Modify update version build script
    Don't keep making changes to files when we don't need to. Only make changes when something changes.

--- Sources/App/Build Scripts/UpdateVersionInfo.sh
+++ Sources/App/Build Scripts/UpdateVersionInfo.sh
@@ -2,64 +2,83 @@
 
 set -e
 
-mkdir -p "${TEXTUAL_WORKSPACE_TEMP_DIR}/"
+cd "${TEXTUAL_WORKSPACE_TEMP_DIR}/"
 
 # Make a copy of the Info.plist file in the .tmp folder
 # This will be the Info.plist file manipulated with the version
 # information that is generated below.
 
-if [ -f "${TEXTUAL_WORKSPACE_TEMP_DIR}/Info.plist" ]; then
-	rm -f "${TEXTUAL_WORKSPACE_TEMP_DIR}/Info.plist"
-fi
-
 if [ "${TEXTUAL_BUILD_SCHEME_TOKEN}" == "appstore" ]; then
-	cp "${PROJECT_DIR}/Resources/Property Lists/Application Properties/InfoAppStore.plist" "${TEXTUAL_WORKSPACE_TEMP_DIR}/Info.plist"
+	infoPlistSource="${PROJECT_DIR}/Resources/Property Lists/Application Properties/InfoAppStore.plist"
 else
-	cp "${PROJECT_DIR}/Resources/Property Lists/Application Properties/Info.plist" "${TEXTUAL_WORKSPACE_TEMP_DIR}/Info.plist"
+	infoPlistSource="${PROJECT_DIR}/Resources/Property Lists/Application Properties/Info.plist"
 fi
 
-cd "${TEXTUAL_WORKSPACE_TEMP_DIR}/"
+infoPlistTarget="${TEXTUAL_WORKSPACE_TEMP_DIR}/Info.plist"
 
-# Write the version information to the Info.plist file
-# The build version is the date of the last commit in git
+if [ ! -f "${infoPlistTarget}" ] || [ "${infoPlistTarget}" -ot "${infoPlistSource}" ]; then
+	echo "Step 1: Info.plist file doesn't exist and/or is oudated. Performing copy."
+
+	# Copy with -p flag to preserve modification time
+	cp -p "${infoPlistSource}" "${infoPlistTarget}"
+else
+	echo "Step 1: Info.plist file hasn't changed."
+fi
+
+# Write the version information to the Info.plist file.
+# The build version is the date of the last commit in git.
 gitBundle=`which git`
 
 if [ -z "${gitBundle}" ]; then
-bundleVersion="000000.00"
+	bundleVersionNew="000000.00"
 else 
-gitDateOfLastCommit=`"${gitBundle}" log -n1 --format="%at"`
+	gitDateOfLastCommit=`"${gitBundle}" log -n1 --format="%at"`
 
-bundleVersion=`/bin/date -u -r "${gitDateOfLastCommit}" "+%y%m%d.%H"`
+	bundleVersionNew=`/bin/date -u -r "${gitDateOfLastCommit}" "+%y%m%d.%H"`
 fi;
 
-/usr/libexec/PlistBuddy -c "Set \"CFBundleVersion\" \"${bundleVersion}\"" Info.plist
+bundleVersionOld=$(/usr/libexec/PlistBuddy -c "Print \"CFBundleVersion\"" Info.plist)
+
+if [ "${bundleVersionOld}" != "${bundleVersionNew}" ]; then
+	echo "Step 2: Writing version: New ('${bundleVersionNew}'), Old ('${bundleVersionOld}')"
+
+	/usr/libexec/PlistBuddy -c "Set \"CFBundleVersion\" \"${bundleVersionNew}\"" Info.plist
+else
+	echo "Step 2: The version hasn't changed."
+fi
+
+# ------ #
 
 # Gather the information necessary for building Textual's BuildConfig.h
 # header. This header file gives various section of the code base version
 # information so it does not need to constantly access the Info.plist file.
 bundleVersionShort=$(/usr/libexec/PlistBuddy -c "Print \"CFBundleShortVersionString\"" Info.plist)
 
-# ------ #
+echo "
+/* ANY CHANGES TO THIS FILE WILL NOT BE SAVED AND WILL NOT BE COMMITTED */
 
-echo "/* ANY CHANGES TO THIS FILE WILL NOT BE SAVED AND WILL NOT BE COMMITTED */" > BuildConfig.h
-
-echo "" >> BuildConfig.h
-
-echo "#define TXBundleBuildProductName						@\"${PRODUCT_NAME}\"" >> BuildConfig.h
-echo "#define TXBundleBuildProductIdentifier				@\"${PRODUCT_BUNDLE_IDENTIFIER}\"" >> BuildConfig.h
-echo "#define TXBundleBuildProductIdentifierCString			\"${PRODUCT_BUNDLE_IDENTIFIER}\"" >> BuildConfig.h
-
-echo "#define TXBundleBuildGroupContainerIdentifier			@\"${TEXTUAL_GROUP_CONTAINER_IDENTIFIER}\"" >> BuildConfig.h
-
-echo "#define TXBundleBuildVersion							@\"${bundleVersion}\"" >> BuildConfig.h
-echo "#define TXBundleBuildVersionShort						@\"${bundleVersionShort}\"" >> BuildConfig.h
-
-echo "#define TXBundleBuildScheme							@\"${TEXTUAL_BUILD_SCHEME_TOKEN}\"" >> BuildConfig.h
-
-echo "#define TXBundleBuildDate								@\"$(date +%s)\"" >> BuildConfig.h
+#define TXBundleBuildProductName					@\"${PRODUCT_NAME}\"
+#define TXBundleBuildProductIdentifier				@\"${PRODUCT_BUNDLE_IDENTIFIER}\"
+#define TXBundleBuildProductIdentifierCString		 \"${PRODUCT_BUNDLE_IDENTIFIER}\"
+#define TXBundleBuildGroupContainerIdentifier		@\"${TEXTUAL_GROUP_CONTAINER_IDENTIFIER}\"
+#define TXBundleBuildVersion						@\"${bundleVersionNew}\"
+#define TXBundleBuildVersionShort					@\"${bundleVersionShort}\"
+#define TXBundleBuildScheme							@\"${TEXTUAL_BUILD_SCHEME_TOKEN}\"
+" > _BuildConfig.h
 
 if [ -z "$CODE_SIGN_IDENTITY" ]; then
-echo "#define TXBundleBuiltWithoutCodeSigning				1" >> BuildConfig.h
+echo "#define TXBundleBuiltWithoutCodeSigning		1" >> _BuildConfig.h
+fi
+
+if cmp -s "BuildConfig.h" "_BuildConfig.h"; then
+	echo "Step 3: The build configuration file hasn't changed. Not deploying."
+
+	rm "_BuildConfig.h"
+else
+	# Force flag is used on rm to avoid error for missing file
+	rm -f "BuildConfig.h"
+
+	mv "_BuildConfig.h" "BuildConfig.h"
 fi
 
 exit 0;
