Codeux-Software/Textual

commit 05730ff8734c8afdc23ba3aa94e5db57b4e9d088

Add auto join delay timer preference
On a certain server, even though we received message from NickServ that identification was successful, joining channels failed because of lag between NickServ and the IRCd. This commit adds a new preference to the Flood Control section of Preferences which controls delay before auto join is performed. It defaults to 0.0 (no delay). It can be configured up to 10.0 seconds.

To accommodate this new feature, the current auto join timer has been renamed to auto join next join timer.

Sources/App/Classes/IRC/IRCClient.m
@@ -159,7 +159,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-#define _autojoinDelayedWarningInterval		90
+#define _autojoinDelayedWarningInterval		90 // max delay after identification is 10 so keep this above that
 #define _autojoinDelayedWarningMaxCount		3
 
 #define _isonCheckInterval			30
@@ -219,6 +219,7 @@ NSString * const IRCClientUserNicknameChangedNotification = @"IRCClientUserNickn
 @property (nonatomic, strong) IRCMessageBatchMessageContainer *batchMessages;
 @property (nonatomic, strong, nullable) TLOFileLogger *logFile;
 @property (nonatomic, strong) TLOTimer *autojoinTimer;
+@property (nonatomic, strong) TLOTimer *autojoinNextJoinTimer;
 @property (nonatomic, strong) TLOTimer *autojoinDelayedWarningTimer;
 @property (nonatomic, strong) TLOTimer *isonTimer;
 @property (nonatomic, strong) TLOTimer *pongTimer;
@@ -335,6 +336,11 @@ DESIGNATED_INITIALIZER_EXCEPTION_BODY_END
 		[self onAutojoinTimer];
 	}];
 
+	self.autojoinNextJoinTimer =
+	[TLOTimer timerWithActionBlock:^(TLOTimer *sender) {
+		[self onAutojoinNextJoinTimer];
+	}];
+
 	self.autojoinDelayedWarningTimer =
 	[TLOTimer timerWithActionBlock:^(TLOTimer *sender) {
 		[self onAutojoinDelayedWarningTimer];
@@ -389,6 +395,7 @@ DESIGNATED_INITIALIZER_EXCEPTION_BODY_END
 #endif
 
 	[self.autojoinTimer stop];
+	[self.autojoinNextJoinTimer stop];
 	[self.autojoinDelayedWarningTimer stop];
 	[self.isonTimer	stop];
 	[self.pongTimer	stop];
@@ -397,6 +404,7 @@ DESIGNATED_INITIALIZER_EXCEPTION_BODY_END
 	[self.whoTimer stop];
 
 	self.autojoinTimer = nil;
+	self.autojoinNextJoinTimer = nil;
 	self.autojoinDelayedWarningTimer = nil;
 	self.isonTimer = nil;
 	self.pongTimer = nil;
@@ -5790,6 +5798,7 @@ DESIGNATED_INITIALIZER_EXCEPTION_BODY_END
 	[self removeRequestedCommands];
 
 	[self stopAutojoinTimer];
+	[self stopAutojoinNextJoinTimer];
 	[self stopAutojoinDelayedWarningTimer];
 	[self stopISONTimer];
 	[self stopPongTimer];
@@ -10749,6 +10758,37 @@ DESIGNATED_INITIALIZER_EXCEPTION_BODY_END
 #pragma mark -
 #pragma mark Autojoin
 
+- (void)startAutojoinTimer
+{
+	if (self.autojoinTimer.timerIsActive) {
+		return;
+	}
+
+	NSTimeInterval interval = [TPCPreferences autojoinDelayAfterIdentification];
+
+	if (CGFloatAreEqual(interval, 0.0)) {
+		[self onAutojoinTimer];
+
+		return;
+	}
+
+	[self.autojoinTimer start:interval onRepeat:NO];
+}
+
+- (void)stopAutojoinTimer
+{
+	if (self.autojoinTimer.timerIsActive == NO) {
+		return;
+	}
+
+	[self.autojoinTimer stop];
+}
+
+- (void)onAutojoinTimer
+{
+	[self onAutojoinNextJoinTimer];
+}
+
 - (void)startAutojoinDelayedWarningTimer
 {
 	if (self.autojoinDelayedWarningTimer.timerIsActive) {
@@ -10793,29 +10833,29 @@ DESIGNATED_INITIALIZER_EXCEPTION_BODY_END
 	}
 }
 
-- (void)startAutojoinTimer
+- (void)startAutojoinNextJoinTimer
 {
-	if (self.autojoinTimer.timerIsActive) {
+	if (self.autojoinNextJoinTimer.timerIsActive) {
 		return;
 	}
 
 	NSTimeInterval interval = [TPCPreferences autojoinDelayBetweenChannelJoins];
 
-	[self.autojoinTimer start:interval onRepeat:YES];
+	[self.autojoinNextJoinTimer start:interval onRepeat:YES];
 }
 
-- (void)stopAutojoinTimer
+- (void)stopAutojoinNextJoinTimer
 {
-	if (self.autojoinTimer.timerIsActive == NO) {
+	if (self.autojoinNextJoinTimer.timerIsActive == NO) {
 		return;
 	}
 
-	[self.autojoinTimer stop];
+	[self.autojoinNextJoinTimer stop];
 
 	self.channelsToAutojoin = nil;
 }
 
-- (void)onAutojoinTimer
+- (void)onAutojoinNextJoinTimer
 {
 	[self autojoinNextChannel];
 }
@@ -10852,7 +10892,7 @@ DESIGNATED_INITIALIZER_EXCEPTION_BODY_END
 
 			self.isAutojoined = YES;
 
-			[self stopAutojoinTimer];
+			[self stopAutojoinNextJoinTimer];
 		}
 	}
 }
@@ -10925,8 +10965,6 @@ DESIGNATED_INITIALIZER_EXCEPTION_BODY_END
 	}
 
 	[self startAutojoinTimer];
-
-	[self onAutojoinTimer];
 }
 
 #pragma mark -

Sources/App/Classes/Headers/TPCPreferencesLocal.h
@@ -236,6 +236,7 @@ typedef NS_ENUM(NSUInteger, TXPreferredAppearance) {
 
 + (NSUInteger)autojoinMaximumChannelJoins;
 + (NSTimeInterval)autojoinDelayBetweenChannelJoins;
++ (NSTimeInterval)autojoinDelayAfterIdentification;
 
 + (TXUserDoubleClickAction)userDoubleClickOption;
 

Sources/App/Classes/Preferences/TPCPreferencesLocal.m
@@ -116,6 +116,11 @@ NSUInteger const TPCPreferencesDictionaryVersion = 602;
 	return [RZUserDefaults() doubleForKey:@"AutojoinDelayBetweenChannelJoins"];
 }
 
++ (NSTimeInterval)autojoinDelayAfterIdentification
+{
+	return [RZUserDefaults() doubleForKey:@"AutojoinDelayAfterIdentification"];
+}
+
 + (NSString *)defaultKickMessage
 {
 	return [RZUserDefaults() objectForKey:@"ChannelOperatorDefaultLocalization -> Kick Reason"];

Sources/App/Resources/Property Lists/RegisteredUserDefaultsInContainer.plist
@@ -16,6 +16,8 @@
 	<false/>
 	<key>AutojoinDelayBetweenChannelJoins</key>
 	<real>2.2</real>
+	<key>AutojoinDelayAfterIdentification</key>
+	<real>0</real>
 	<key>AutojoinMaximumChannelJoinCount</key>
 	<integer>2</integer>
 	<key>AutojoinWaitsForNickservIdentification</key>

commit 3d8f993b1963c09a86ee999e33c9a752e768c0ef

Modify auto join logic
05730ff8734c8afdc23ba3aa94e5db57b4e9d088 introduced bug because i did not test it thoroughly.

When the auto join delay timer fired, it called out to join the next channel, bu tit never started the timer to continue after doing that. I only tested the change on one channel whichis why I never caught this.

Sources/App/Classes/IRC/IRCClient.m
@@ -10784,7 +10784,7 @@ DESIGNATED_INITIALIZER_EXCEPTION_BODY_END
 
 - (void)onAutojoinTimer
 {
-	[self onAutojoinNextJoinTimer];
+	[self startAutojoinNextJoinTimer];
 }
 
 - (void)startAutojoinDelayedWarningTimer
@@ -10840,6 +10840,9 @@ DESIGNATED_INITIALIZER_EXCEPTION_BODY_END
 	NSTimeInterval interval = [TPCPreferences autojoinDelayBetweenChannelJoins];
 
 	[self.autojoinNextJoinTimer start:interval onRepeat:YES];
+
+	/* Fake first event */
+	[self onAutojoinNextJoinTimer];
 }
 
 - (void)stopAutojoinNextJoinTimer