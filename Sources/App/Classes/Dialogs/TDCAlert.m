Codeux-Software/Textual

commit 89ac7278dbba9c8a67e13220ab61ad7c90b6088a

Move from HockeyApp to AppCenter

Sources/App/Classes/Dialogs/TDCAlert.m
@@ -134,13 +134,55 @@ NSString * const TDCAlertSuppressionPrefix = @"Text Input Prompt Suppression -> 
 				accessoryView:(nullable NSView *)accessoryView
 		  suppressionResponse:(nullable BOOL *)suppressionResponse
 {
+	TDCAlertResponse response =
+	[self modalAlertWithMessage:bodyText
+						  title:titleText
+				  defaultButton:buttonDefault
+				alternateButton:buttonAlternate
+					otherButton:nil
+				 suppressionKey:suppressKey
+				suppressionText:suppressText
+				  accessoryView:nil
+			suppressionResponse:nil];
+
+	return (response == TDCAlertResponseDefault);
+}
+
++ (TDCAlertResponse)modalAlertWithMessage:(NSString *)bodyText
+									title:(NSString *)titleText
+							defaultButton:(NSString *)buttonDefault
+						  alternateButton:(nullable NSString *)buttonAlternate
+							  otherButton:(nullable NSString *)otherButton
+{
+	return
+	[self modalAlertWithMessage:bodyText
+						  title:titleText
+				  defaultButton:buttonDefault
+				alternateButton:buttonAlternate
+					otherButton:otherButton
+				 suppressionKey:nil
+				suppressionText:nil
+				  accessoryView:nil
+			suppressionResponse:nil];
+}
+
++ (TDCAlertResponse)modalAlertWithMessage:(NSString *)bodyText
+									title:(NSString *)titleText
+							defaultButton:(NSString *)buttonDefault
+						  alternateButton:(nullable NSString *)buttonAlternate
+							  otherButton:(nullable NSString *)otherButton
+						   suppressionKey:(nullable NSString *)suppressKey
+						  suppressionText:(nullable NSString *)suppressText
+							accessoryView:(nullable NSView *)accessoryView
+					  suppressionResponse:(nullable BOOL *)suppressionResponse
+{
 	NSParameterAssert(bodyText != nil);
 	NSParameterAssert(titleText != nil);
 	NSParameterAssert(buttonDefault != nil);
 
 	/* Require main thread */
 	if ([NSThread isMainThread] == NO) {
-		__block BOOL result = NO;
+		__block TDCAlertResponse result = TDCAlertResponseAlternate;
 
 		[self performBlockOnMainThread:^{
 			result =
@@ -148,6 +190,7 @@ NSString * const TDCAlertSuppressionPrefix = @"Text Input Prompt Suppression -> 
 								  title:titleText
 						  defaultButton:buttonDefault
 						alternateButton:buttonAlternate
+							otherButton:otherButton
 						 suppressionKey:suppressKey
 						suppressionText:suppressText
 						  accessoryView:accessoryView
@@ -183,6 +226,10 @@ NSString * const TDCAlertSuppressionPrefix = @"Text Input Prompt Suppression -> 
 		[alert addButtonWithTitle:buttonAlternate];
 	}
 
+	if (otherButton) {
+		[alert addButtonWithTitle:otherButton];
+	}
+
 	if (suppressKey || suppressText) {
 		alert.showsSuppressionButton = YES;
 
@@ -204,7 +251,7 @@ NSString * const TDCAlertSuppressionPrefix = @"Text Input Prompt Suppression -> 
 		  suppressionKey:suppressKey
 	 suppressionResponse:suppressionResponse];
 
-	return (response == TDCAlertResponseDefault);
+	return response;
 }
 
 #pragma mark -

Configurations/Build/App Store Release/Enabled Features.xcconfig
@@ -7,4 +7,4 @@
 // the inclusion of certain segments of code. Toggling one of these flags does not
 // guarantee that the toggled feature will work. These only toggle the code.
 // Not add additional resource files that the code may be dependent on.
-GCC_PREPROCESSOR_DEFINITIONS = TEXTUAL_BUILT_INSIDE_SANDBOX=1 TEXTUAL_BUILT_WITH_ADVANCED_ENCRYPTION=1 TEXTUAL_BUILT_WITH_HOCKEYAPP_SDK_ENABLED=1 TEXTUAL_BUILT_WITH_ICLOUD_SUPPORT=1 TEXTUAL_BUILT_FOR_APP_STORE_DISTRIBUTION=1
+GCC_PREPROCESSOR_DEFINITIONS = TEXTUAL_BUILT_INSIDE_SANDBOX=1 TEXTUAL_BUILT_WITH_ADVANCED_ENCRYPTION=1 TEXTUAL_BUILT_WITH_APPCENTER_SDK_ENABLED=1 TEXTUAL_BUILT_WITH_ICLOUD_SUPPORT=1 TEXTUAL_BUILT_FOR_APP_STORE_DISTRIBUTION=1

Configurations/Build/Standard Release Sandboxed/Enabled Features.xcconfig
@@ -7,4 +7,4 @@
 // the inclusion of certain segments of code. Toggling one of these flags DOES NOT
 // guarantee that the toggled feature will work. These only toggle the code.
 // Not add additional resource files that the code may be dependent on.
-GCC_PREPROCESSOR_DEFINITIONS = TEXTUAL_BUILT_INSIDE_SANDBOX=1 TEXTUAL_BUILT_WITH_ADVANCED_ENCRYPTION=1 TEXTUAL_BUILT_WITH_LICENSE_MANAGER=1 TEXTUAL_BUILT_WITH_HOCKEYAPP_SDK_ENABLED=1 TEXTUAL_BUILT_WITH_SPARKLE_ENABLED=0
+GCC_PREPROCESSOR_DEFINITIONS = TEXTUAL_BUILT_INSIDE_SANDBOX=1 TEXTUAL_BUILT_WITH_ADVANCED_ENCRYPTION=1 TEXTUAL_BUILT_WITH_LICENSE_MANAGER=1 TEXTUAL_BUILT_WITH_APPCENTER_SDK_ENABLED=1 TEXTUAL_BUILT_WITH_SPARKLE_ENABLED=0

Configurations/Build/Standard Release/Enabled Features.xcconfig
@@ -7,4 +7,4 @@
 // the inclusion of certain segments of code. Toggling one of these flags does not
 // guarantee that the toggled feature will work. These only toggle the code.
 // Not add additional resource files that the code may be dependent on.
-GCC_PREPROCESSOR_DEFINITIONS = TEXTUAL_BUILT_INSIDE_SANDBOX=0 TEXTUAL_BUILT_WITH_ADVANCED_ENCRYPTION=1 TEXTUAL_BUILT_WITH_LICENSE_MANAGER=1 TEXTUAL_BUILT_WITH_HOCKEYAPP_SDK_ENABLED=1 TEXTUAL_BUILT_WITH_SPARKLE_ENABLED=1
+GCC_PREPROCESSOR_DEFINITIONS = TEXTUAL_BUILT_INSIDE_SANDBOX=0 TEXTUAL_BUILT_WITH_ADVANCED_ENCRYPTION=1 TEXTUAL_BUILT_WITH_LICENSE_MANAGER=1 TEXTUAL_BUILT_WITH_APPCENTER_SDK_ENABLED=1 TEXTUAL_BUILT_WITH_SPARKLE_ENABLED=1

Sources/App/Classes/Controllers/TXMasterController.m
@@ -67,8 +67,9 @@
 #import "TXMasterControllerPrivate.h"
 #import "IRCClient.h"
 
-#if TEXTUAL_BUILT_WITH_HOCKEYAPP_SDK_ENABLED == 1
-#import <HockeySDK/HockeySDK.h>
+#if TEXTUAL_BUILT_WITH_APPCENTER_SDK_ENABLED == 1
+#import <AppCenter/AppCenter.h>
+#import <AppCenterCrashes/AppCenterCrashes.h>
 #endif
 
 #if TEXTUAL_BUILT_WITH_SPARKLE_ENABLED == 1
@@ -254,18 +255,46 @@ NS_ASSUME_NONNULL_BEGIN
 #pragma mark -
 #pragma mark Services
 
-- (void)prepareThirdPartyServiceHockeyAppFramework
+- (void)prepareThirdPartyServiceAppCenterFramework
 {
-#if TEXTUAL_BUILT_WITH_HOCKEYAPP_SDK_ENABLED == 1
-	NSDictionary *hockeyAppData = [TPCResourceManager loadContentsOfPropertyListInResources:@"3rdPartyStaticStoreHockeyAppFramework"];
+#if TEXTUAL_BUILT_WITH_APPCENTER_SDK_ENABLED == 1
+	NSDictionary *acData = [TPCResourceManager loadContentsOfPropertyListInResources:@"3rdPartyStaticStoreAppCenterFramework"];
 
-	NSString *applicationIdentifier = hockeyAppData[@"Application Identifier"];
+	NSString *applicationIdentifier = acData[@"Application Identifier"];
 
-	[[BITHockeyManager sharedHockeyManager] configureWithIdentifier:applicationIdentifier delegate:(id)self];
+	[MSAppCenter start:applicationIdentifier withServices:@[[MSCrashes class]]];
 
-	[BITHockeyManager sharedHockeyManager].disableMetricsManager = YES;
+	[MSCrashes setUserConfirmationHandler:^BOOL(NSArray<MSErrorReport *> *errorReports) {
+		TDCAlertResponse response =
+		[TDCAlert modalAlertWithMessage:TXTLS(@"Prompts[oe3-i3]")
+								  title:TXTLS(@"Prompts[h49-h5]")
+						  defaultButton:TXTLS(@"Prompts[arr-yb]")
+						alternateButton:TXTLS(@"Prompts[98a-kj]")
+							otherButton:TXTLS(@"Prompts[6j7-hw]")];
 
-	[[BITHockeyManager sharedHockeyManager] startManager];
+		switch (response) {
+			case TDCAlertResponseDefault:
+			{
+				[MSCrashes notifyWithUserConfirmation:MSUserConfirmationSend];
+
+				break;
+			}
+			case TDCAlertResponseAlternate:
+			{
+				[MSCrashes notifyWithUserConfirmation:MSUserConfirmationDontSend];
+
+				break;
+			}
+			case TDCAlertResponseOther:
+			{
+				[MSCrashes notifyWithUserConfirmation:MSUserConfirmationAlways];
+
+				break;
+			}
+		} // switch
+
+		return YES;
+	}];
 #endif
 }
 
@@ -298,7 +327,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (void)prepareThirdPartyServices
 {
-	[self prepareThirdPartyServiceHockeyAppFramework];
+	[self prepareThirdPartyServiceAppCenterFramework];
 
 	[self prepareThirdPartyServiceSparkleFramework];
 }
@@ -406,6 +435,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (BOOL)queryTerminate
 {
+	[MSCrashes generateTestCrash];
+
 	if (self.applicationIsTerminating) {
 		return YES;
 	}

Sources/App/Classes/Controllers/TXMenuController.m
@@ -87,10 +87,6 @@
 #import "TXWindowControllerPrivate.h"
 #import "TXMenuControllerPrivate.h"
 
-#if TEXTUAL_BUILT_WITH_HOCKEYAPP_SDK_ENABLED == 1
-#import <HockeySDK/HockeySDK.h>
-#endif
-
 #if TEXTUAL_BUILT_WITH_SPARKLE_ENABLED == 1
 #import <Sparkle/Sparkle.h>
 #endif
@@ -3054,13 +3050,6 @@ NS_ASSUME_NONNULL_BEGIN
 
 #pragma mark -
 #pragma mark Developer
-
-- (void)simulateCrash:(id)sender
-{
-#if TEXTUAL_BUILT_WITH_HOCKEYAPP_SDK_ENABLED == 1
-	[[BITHockeyManager sharedHockeyManager].crashManager generateTestCrash];
-#endif
-}
 
 - (void)toggleDeveloperMode:(id)sender
 {

Sources/App/Classes/Headers/TDCAlert.h
@@ -95,6 +95,22 @@ typedef void (^TDCAlertCompletionBlock)(TDCAlertResponse buttonClicked, BOOL sup
 				accessoryView:(nullable NSView *)accessoryView
 		  suppressionResponse:(nullable BOOL *)suppressionResponse;
 
++ (TDCAlertResponse)modalAlertWithMessage:(NSString *)bodyText
+									title:(NSString *)titleText
+							defaultButton:(NSString *)buttonDefault
+						  alternateButton:(nullable NSString *)buttonAlternate
+							  otherButton:(nullable NSString *)otherButton;
+
++ (TDCAlertResponse)modalAlertWithMessage:(NSString *)bodyText
+									title:(NSString *)titleText
+							defaultButton:(NSString *)buttonDefault
+						  alternateButton:(nullable NSString *)buttonAlternate
+							  otherButton:(nullable NSString *)otherButton
+						   suppressionKey:(nullable NSString *)suppressKey
+						  suppressionText:(nullable NSString *)suppressText
+							accessoryView:(nullable NSView *)accessoryView
+					  suppressionResponse:(nullable BOOL *)suppressionResponse;
+
 #pragma mark -
 #pragma mark Non-blocking Alerts (Panel)
 

Sources/App/Classes/Headers/TXMenuController.h
@@ -505,8 +505,6 @@ enum
 - (IBAction)manageLicense:(id)sender;
 - (IBAction)manageInAppPurchase:(id)sender;
 
-- (IBAction)simulateCrash:(id)sender;
-
 #if TEXTUAL_BUILT_WITH_ADVANCED_ENCRYPTION == 1
 - (IBAction)encryptionWhatIsThisInformation:(id)sender;
 - (IBAction)encryptionStartPrivateConversation:(id)sender;

commit e4a25322542a2c10265bf9e597c4edbe93d949f1

Fix regression in TDCAlert introduced by 89ac727
I modified method to return TDCAlertResponse type instead of a boolean. This is enum with three values: 1000, 1001, 1002. It is declared as an integer. Compiler also treats YES (objective-c equivalent of TRUE) as an integer (1) so compiler did not notify me I was returning an incorrect value when I forgot to modify one return since it was in fact returning an integer. But because YES (1) did not equal one of the expected results, it threw off the logic.

Sources/App/Classes/Dialogs/TDCAlert.m
@@ -206,7 +206,7 @@ NSString * const TDCAlertSuppressionPrefix = @"Text Input Prompt Suppression -> 
 
 		/* Exit if suppressed */
 		if ([RZUserDefaults() boolForKey:suppressKey]) {
-			return YES;
+			return TDCAlertResponseDefault;
 		}
 	}
 