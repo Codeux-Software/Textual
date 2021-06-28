f43d8be3d3d50402b0b20a3c9206bf0c760ce0e4
Michael <michael@codeux.com>
2021-06-28 02:50:56 -0400

    Archive utility improvements

--- Sources/App/Build Scripts/ExportArchive.sh
+++ Sources/App/Build Scripts/ExportArchive.sh
@@ -2,51 +2,90 @@
 
 set -e
 
-EXPORT_PATH="${TEXTUAL_WORKSPACE_TEMP_DIR}/ArchiveTan"
+WORKING_PATH="${TEXTUAL_WORKSPACE_TEMP_DIR}/ArchiveTan"
 
-mkdir -p "${EXPORT_PATH}"
+mkdir -p "${WORKING_PATH}"
+
+cd "${WORKING_PATH}"
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
 xcodebuild -exportArchive \
 -exportOptionsPlist "${TEXTUAL_WORKSPACE_DIR}/Configurations/ExportArchiveConfiguration.plist" \
 -archivePath "${ARCHIVE_PATH}" \
--exportPath "${EXPORT_PATH}"
+-exportPath "${WORKING_PATH}"
 
-cd "${EXPORT_PATH}"
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
+# Call `git` after `cd` into working path to make
+# sure we are in a directory of a git respository.
 GIT_COMMIT_HASH=`git rev-parse --short HEAD`
 
+EXPORT_PATH="${HOME}/Desktop/Textual-${GIT_COMMIT_HASH}"
+
+mkdir -p "${EXPORT_PATH}"
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
 if [ "${TEXTUAL_BUILT_AS_UNIVERSAL_BINARY}" == "1" ]; then
-	ZIP_FILE_NAME="Textual-${GIT_COMMIT_HASH}-universal.zip"
+	ARCHSPEC_PATH="${EXPORT_PATH}/universal"
 else
-	ZIP_FILE_NAME="Textual-${GIT_COMMIT_HASH}.zip"
+	ARCHSPEC_PATH="${EXPORT_PATH}/intel"
 fi
 
-zip -y -r -X "./${ZIP_FILE_NAME}" "./${FULL_PRODUCT_NAME}/"
+mkdir -p "${ARCHSPEC_PATH}"
 
-mv -f "./${ZIP_FILE_NAME}" ~/Desktop
+ZIP_EXPORT_PATH="${ARCHSPEC_PATH}/Textual.zip"
+
+zip -y -r -X "${ZIP_EXPORT_PATH}" "./${FULL_PRODUCT_NAME}/"
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
 BUNDLE_VERSION_LONG=$(/usr/libexec/PlistBuddy -c "Print \"CFBundleVersion\"" "./${FULL_PRODUCT_NAME}/Contents/Info.plist")
 BUNDLE_VERSION_SHORT=$(/usr/libexec/PlistBuddy -c "Print \"CFBundleShortVersionString\"" "./${FULL_PRODUCT_NAME}/Contents/Info.plist")
 
-touch "./buildInfo.txt"
+echo "<?php
 
-echo "	\$current_release_version_short = \"${BUNDLE_VERSION_SHORT}\";" >> ./buildInfo.txt
-echo "	\$current_release_version_long = \"${BUNDLE_VERSION_LONG}\";" >> ./buildInfo.txt
-echo "	\$current_release_version_signature = \"${GIT_COMMIT_HASH}\";" >> ./buildInfo.txt
+	/* Generated on: $(date) */
+" > ./buildInfo.php
 
-mv -f "./buildInfo.txt" ~/Desktop
+echo "	\$current_release_version_short = \"${BUNDLE_VERSION_SHORT}\";" >> ./buildInfo.php
+echo "	\$current_release_version_long = \"${BUNDLE_VERSION_LONG}\";" >> ./buildInfo.php
+echo "	\$current_release_version_signature = \"${GIT_COMMIT_HASH}\";" >> ./buildInfo.php
 
-touch "./buildLog.txt"
+mv "./buildInfo.php" "${EXPORT_PATH}"
 
-echo "<h2>Changes in ${BUNDLE_VERSION_LONG}</h2>" >> buildLog.txt
-echo "<ul>" >> buildLog.txt
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
-git log --since='48 hours ago' --pretty=format:'<li>%s</li>' >> buildLog.txt
+echo "<section class=\"main\">
+	<section class=\"header\">Release Notes for ${BUNDLE_VERSION_LONG}</section>
 
-echo "</ul>" >> buildLog.txt
+	<section class=\"body\">
 
-mv -f "./buildLog.txt" ~/Desktop
+		<ul>" > ./buildLog.txt
 
-rm -rf "${EXPORT_PATH}"
+git log --since='48 hours ago' --pretty=format:'			<li>%s</li>' >> ./buildLog.txt
+
+echo "
+		</ul>
+
+	</section>
+</section>
+
+<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->" >> ./buildLog.txt
+
+mv "./buildLog.txt" "${EXPORT_PATH}"
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+cd "${DWARF_DSYM_FOLDER_PATH}"
+
+DYSM_EXPORT_PATH="${ARCHSPEC_PATH}/Debug symbols.zip"
+
+zip -y -r -X "${DYSM_EXPORT_PATH}" "./${DWARF_DSYM_FILE_NAME}"
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+rm -rf "${WORKING_PATH}"
 
 exit 0;
